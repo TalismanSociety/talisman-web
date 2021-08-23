abe2b146372577998d69330d1a7a45a4975c0bce
Chid Gilovitz <chid.gilovitz@gmail.com>
2021-08-23 15:26:43 +0800

    - Fixed types on Panel and PanelSection; - Fixed formatting; - Removed unused imports and arguments

--- src/archetypes/Crowdloan/Indexx.tsx
+++ src/archetypes/Crowdloan/Indexx.tsx
@@ -1,175 +1,133 @@
-import styled from 'styled-components'
-import { 
-  Field,
-  NoResults,
-  Await,
-  Grid
-} from '@components'
 import { Crowdloan } from '@archetypes'
-import { ReactComponent as Loader } from '@icons/loader.svg'
+import { Await, Field, Grid, NoResults } from '@components'
+import styled from 'styled-components'
 
 const FilterBar = styled(
   ({
-    search='',
-    order='',
-    status=null,
-    setSearch=()=>{}, 
-    setOrder=()=>{},
-    setStatus=()=>{},
-    orderOptions={},
-    statusOptions={},
-    hasFilter=false,
+    search = '',
+    order = '',
+    status = null,
+    setSearch = () => {},
+    setOrder = () => {},
+    setStatus = () => {},
+    orderOptions = {},
+    statusOptions = {},
+    hasFilter = false,
     reset,
     count,
     className,
     ...rest
-  }) => 
-    <div 
-      className={`${className} filterbar`}
-      {...rest}
-      >
-      <span 
-        className="left"
-        >
-        <Field.Search
-          value={search}
-          placeholder='Search Crowdloans'
-          onChange={setSearch}
-        />
-        <Field.RadioGroup
-          value={status}
-          onChange={setStatus}
-          options={statusOptions}
-          small
-        />
+  }) => (
+    <div className={`${className} filterbar`} {...rest}>
+      <span className="left">
+        <Field.Search value={search} placeholder="Search Crowdloans" onChange={setSearch} />
+        <Field.RadioGroup value={status} onChange={setStatus} options={statusOptions} small />
       </span>
-      <span 
-        className="right"
-        >
-        <Field.Select
-          onChange={setOrder}
-          options={orderOptions}
-        />
+      <span className="right">
+        <Field.Select onChange={setOrder} options={orderOptions} />
       </span>
     </div>
   )
-  `
+)`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2.4rem;
+  margin: 2.4rem 0;
+
+  > span {
     display: flex;
-    width: 100%;
-    justify-content: space-between;
     align-items: center;
-    padding: 0 2.4rem;
-    margin: 2.4rem 0;
+    position: relative;
 
-    >span{
-      display: flex;
-      align-items: center;
-      position: relative;
+    &.left {
+      justify-content: flex-start;
+    }
+    &.right {
+      justify-content: flex-end;
+    }
 
-      &.left{ justify-content: flex-start }
-      &.right{ justify-content: flex-end }
+    &.right {
+      > * + * {
+        margin-left: 1em;
+      }
 
-      &.right{
-        >* + *{
-          margin-left: 1em;
-        }
-
-        .field-toggle[data-on='true'] .toggle:after{
-          background: rgb(${({theme}) => theme.primary});
-        }
+      .field-toggle[data-on='true'] .toggle:after {
+        background: rgb(${({ theme }) => theme.primary});
       }
     }
+  }
 
-    .field-search{
-      width: 23vw;
-    }
+  .field-search {
+    width: 23vw;
+  }
 
-    .field-radiogroup{
-      margin-left: 2.3rem
-    }
+  .field-radiogroup {
+    margin-left: 2.3rem;
+  }
 
-    .field-select .children{
-      font-size: 0.9em;
-      box-shadow: none;
-      color: rgb(${({theme}) => theme.primary});
-    }
+  .field-select .children {
+    font-size: 0.9em;
+    box-shadow: none;
+    color: rgb(${({ theme }) => theme.primary});
+  }
 
-    @media only screen and (max-width: 700px) {
-      display: inline-block;
-      margin-bottom: 0;
-      >span{
-        display: inline;
+  @media only screen and (max-width: 700px) {
+    display: inline-block;
+    margin-bottom: 0;
+    > span {
+      display: inline;
+      width: 100%;
+
+      .field-search {
+        display: block;
         width: 100%;
-
-        .field-search{
-          display: block;
-          width: 100%;
-        }
-
-        .field-radiogroup{
-          margin: 0;
-          width: 40%;
-          display: inline-block;
-          margin-top: 1em;
-        }
-
-        .field-select{
-          display: inline-block;
-          margin: 0;
-          float: right;
-          margin-top: 0.5em;
-        }
-      } 
-    };
-  `
-
-const Index = styled(
-  ({
-    withFilter,
-    className
-  }) => {
-    const { 
-      items,
-      count,
-      status,
-      loading,
-      filterProps
-    } = Crowdloan.useFilter()
-
-    return <div
-      className={`crowdloan-index ${className}`}
-      >
-      {withFilter &&
-        <FilterBar
-          {...filterProps}
-          count={count}
-        />
       }
-      <Await
-        until={!loading}
-        >
+
+      .field-radiogroup {
+        margin: 0;
+        width: 40%;
+        display: inline-block;
+        margin-top: 1em;
+      }
+
+      .field-select {
+        display: inline-block;
+        margin: 0;
+        float: right;
+        margin-top: 0.5em;
+      }
+    }
+  } ;
+`
+
+const Index = styled(({ withFilter, className }) => {
+  const { items, count, loading, filterProps } = Crowdloan.useFilter()
+
+  return (
+    <div className={`crowdloan-index ${className}`}>
+      {withFilter && <FilterBar {...filterProps} count={count} />}
+      <Await until={!loading}>
         <NoResults
           require={count?.filtered > 0}
-          title='Vamoosh'
-          subtitle='Talisman cannot summon what you wish for.'
-          text='Better luck next time.'
-          >
+          title="Vamoosh"
+          subtitle="Talisman cannot summon what you wish for."
+          text="Better luck next time."
+        >
           <Grid>
-            {items.map(({paraId}) =>               
-              <Crowdloan.Teaser 
-                key={paraId} 
-                id={paraId}
-              />
-            )}
+            {items.map(({ paraId }) => (
+              <Crowdloan.Teaser key={paraId} id={paraId} />
+            ))}
           </Grid>
         </NoResults>
       </Await>
     </div>
-  })
-  `
-    .await{
-      font-size: var(--font-size-xxlarge)
-    }
-  `
+  )
+})`
+  .await {
+    font-size: var(--font-size-xxlarge);
+  }
+`
 
 export default Index
