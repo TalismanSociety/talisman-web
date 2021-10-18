ebf362cfb8caf04de80fd7fc201186ab45cd5836
Julius Carvajal <julius@talisman.xyz>
2021-10-19 09:58:51 +1100

    feat(buy): buy route cwith coming soon banner (#11)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,40 +1,28 @@
-import {
-	Route, 
-	Switch,
-	Redirect
-} from "react-router-dom";
-import CrowdloanIndex from './Crowdloan.Index.tsx'
+import { Redirect, Route, Switch } from 'react-router-dom'
+
+import Buy from './Buy'
 import CrowdloanDetail from './Crowdloan.Detail.tsx'
+import CrowdloanIndex from './Crowdloan.Index.tsx'
 import Wallet from './Wallet.tsx'
 
-const Routes = () => 
-	<Switch>
-		<Route 
-			exact 
-			path="/"
-			>
-			<Wallet/>
-		</Route>
-		<Route 
-			exact 
-			path="/crowdloans"
-			>
-			<CrowdloanIndex/>
-		</Route>
-		<Route 
-			exact 
-			path="/crowdloans/:slug"
-			>
-			<CrowdloanDetail/>
-		</Route>
-		<Route 
-			path="*"
-			>
-			<Redirect 
-				to="/"
-			/>
-		</Route>
-	</Switch>
-
+const Routes = () => (
+  <Switch>
+    <Route exact path="/">
+      <Wallet />
+    </Route>
+    <Route exact path="/crowdloans">
+      <CrowdloanIndex />
+    </Route>
+    <Route exact path="/crowdloans/:slug">
+      <CrowdloanDetail />
+    </Route>
+    <Route exact path="/buy">
+      <Buy />
+    </Route>
+    <Route path="*">
+      <Redirect to="/" />
+    </Route>
+  </Switch>
+)
 
 export default Routes
