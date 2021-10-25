5696f43db3b0a4419ecc2d3dba25a62c874bc3c1
alecdwm <alec@owls.io>
2021-10-26 09:38:35 +1000

    fix(portfolio) show running total while portfolio is loading

--- src/components/TextLoader.tsx
+++ src/components/TextLoader.tsx
@@ -5,17 +5,17 @@ const TextLoader = styled.div`
 
   background: linear-gradient(
     90deg,
-    rgba(255, 255, 255, 0) 25%,
-    rgba(255, 255, 255, 0.4) 40%,
-    rgba(255, 255, 255, 0.7) 60%,
-    rgba(255, 255, 255, 0) 75%
+    rgba(213, 255, 92, 0) 25%,
+    rgba(213, 255, 92, 0.4) 40%,
+    rgba(213, 255, 92, 0.7) 60%,
+    rgba(213, 255, 92, 0) 75%
   );
   background-size: 400% 100%;
   background-clip: text;
   -webkit-background-clip: text;
   background-repeat: no-repeat;
   background-position: 0 0;
-  background-color: var(--color-primary);
+  background-color: var(--color-text);
   animation: shimmer 2s infinite;
 
   &&& {
