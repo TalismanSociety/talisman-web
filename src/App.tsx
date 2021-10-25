b69c330506b4453bce91edb7022ca6591d993016
Julius Carvajal <julius@talisman.xyz>
2021-10-25 14:06:56 +1100

    feat(i18n): basic i18n on header and welcome screen (#18)
    * feat(i18n): basic i18n on header and welcome screen
    
    * feat(i18n): more translations
    
    * feat(i18n): language dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,32 @@ import * as Portfolio from '@libs/portfolio'
 import TalismanProvider from '@libs/talisman'
 import * as Tokenprices from '@libs/tokenprices'
 import Routes from '@routes'
+import { Suspense } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import ThemeProvider from './App.Theme'
 import Layout from './layout'
 
 const App: React.FC = () => (
-  <Portfolio.Provider>
-    <Tokenprices.Provider>
-      <TalismanProvider>
-        <Crowdloans.Provider uri="https://api.subquery.network/sq/TalismanSociety/kusama-crowdloans">
-          <Router>
-            <ThemeProvider>
-              <ModalProvider>
-                <Layout>
-                  <Routes />
-                </Layout>
-              </ModalProvider>
-            </ThemeProvider>
-          </Router>
-        </Crowdloans.Provider>
-      </TalismanProvider>
-    </Tokenprices.Provider>
-  </Portfolio.Provider>
+  <Suspense fallback="loading">
+    <Portfolio.Provider>
+      <Tokenprices.Provider>
+        <TalismanProvider>
+          <Crowdloans.Provider uri="https://api.subquery.network/sq/TalismanSociety/kusama-crowdloans">
+            <Router>
+              <ThemeProvider>
+                <ModalProvider>
+                  <Layout>
+                    <Routes />
+                  </Layout>
+                </ModalProvider>
+              </ThemeProvider>
+            </Router>
+          </Crowdloans.Provider>
+        </TalismanProvider>
+      </Tokenprices.Provider>
+    </Portfolio.Provider>
+  </Suspense>
 )
 
 export default App
