4d73dec590044afb02aedd46fcd55fe892eb0187
alecdwm <alec@owls.io>
2021-10-26 12:04:50 +1000

    fix(portfolio): fix customRpcs ignored when changing route from home to portfolio

--- src/archetypes/Wallet/Assets.tsx
+++ src/archetypes/Wallet/Assets.tsx
@@ -12,28 +12,11 @@ import {
   useChain,
 } from '@talismn/api-react-hooks'
 import { addBigNumbers, useFuncMemo } from '@talismn/util'
+import customRpcs from '@util/customRpcs'
 import { formatCommas, formatCurrency } from '@util/helpers'
 import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
-
-// TODO: Move these to a global config object
-const customRpcs = {
-  '0': [], // ['wss://polkadot.api.onfinality.io/ws?apikey=e1b2f3ea-f003-42f5-adf6-d2e6aa3ecfe4'], // Polkadot Relay
-  '2': [], // ['wss://kusama.api.onfinality.io/ws?apikey=e1b2f3ea-f003-42f5-adf6-d2e6aa3ecfe4'], // Kusama Relay
-  '1000': [], // ['wss://statemine.api.onfinality.io/ws?apikey=e1b2f3ea-f003-42f5-adf6-d2e6aa3ecfe4'], // Statemine
-  '2000': [], // ['wss://karura.api.onfinality.io/ws?apikey=e1b2f3ea-f003-42f5-adf6-d2e6aa3ecfe4'], // Karura
-  '2001': [], // ['wss://bifrost-parachain.api.onfinality.io/ws?apikey=e1b2f3ea-f003-42f5-adf6-d2e6aa3ecfe4'], // Bifrost
-  '2004': [], // ['wss://khala.api.onfinality.io/ws?apikey=e1b2f3ea-f003-42f5-adf6-d2e6aa3ecfe4'], // Khala
-  '2007': [], // ['wss://shiden.api.onfinality.io/ws?apikey=e1b2f3ea-f003-42f5-adf6-d2e6aa3ecfe4'], // Shiden
-  '2023': [], // ['wss://moonriver.api.onfinality.io/ws?apikey=e1b2f3ea-f003-42f5-adf6-d2e6aa3ecfe4'], // Moonriver
-  '2084': [], // Calamari
-  '2086': [], // KILT Spiritnet
-  '2090': [
-    'wss://basilisk.api.onfinality.io/ws?apikey=e1b2f3ea-f003-42f5-adf6-d2e6aa3ecfe4',
-    'wss://rpc-01.basilisk.hydradx.io',
-  ], // Basilisk
-}
 
 const AssetItem = styled(({ id, balances, addresses, className }) => {
   const chain = useChain(id)
