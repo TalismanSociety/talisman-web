222c755ba5effb72bf49494e80d8f0f8079566f4
Julius Carvajal <julius@talisman.xyz>
2021-10-29 00:24:52 +1100

    feat(crowdloans): unlock the talisman banner (#52)

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,5 @@
-import styled from 'styled-components'
 import { sortBy } from 'lodash'
+import styled from 'styled-components'
 
 const defaultColumns = 4
 
@@ -11,42 +11,31 @@ const defaultBreakpoints = {
   700: { columns: 1 },
 }
 
-const Grid = styled(
-  ({
-    className,
-    children,
-  }) => 
-    <div 
-      className={`grid ${className}`}
-      >
-      {children}
-    </div>
-  )
-  `
-    display: grid;
-    grid-gap: 2.4rem;
-    width: 100%;
-    grid-template-columns: repeat(${({columns=defaultColumns}) => columns}, 1fr);
-    padding: 0 ${({gap=defaultGap}) => gap};
-    margin: ${({gap=defaultGap}) => gap} 0;
+const Grid = styled(({ className, children }) => <div className={`grid ${className}`}>{children}</div>)`
+  display: grid;
+  grid-gap: 2.4rem;
+  width: 100%;
+  grid-template-columns: repeat(${({ columns = defaultColumns }) => columns}, 1fr);
+  margin: ${({ gap = defaultGap }) => gap} 0;
 
-    >*{
-      ${({itemHeight}) => !!itemHeight && `height: ${itemHeight}`}
-    }
+  > * {
+    ${({ itemHeight }) => !!itemHeight && `height: ${itemHeight}`}
+  }
 
-    ${({breakpoints=defaultBreakpoints, columns=defaultColumns, itemHeight}) => 
-      sortBy(Object.entries(breakpoints), bp => parseInt(bp))
-        .reverse()
-        .map((key) => 
+  ${({ breakpoints = defaultBreakpoints, columns = defaultColumns, itemHeight }) =>
+    sortBy(Object.entries(breakpoints), bp => parseInt(bp))
+      .reverse()
+      .map(
+        key =>
           `
             @media only screen and (max-width: ${key[0]}px) {
-              grid-template-columns: repeat(${key[1]?.columns||columns}, 1fr);
+              grid-template-columns: repeat(${key[1]?.columns || columns}, 1fr);
               >*{
-                height: ${key[1]?.itemHeight||itemHeight||'inherit'};
+                height: ${key[1]?.itemHeight || itemHeight || 'inherit'};
               }
             };
           `
-    )}
-  `
-  
+      )}
+`
+
 export default Grid
