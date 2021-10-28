192285c7e1632d6f14def26573644eb19adde754
Julius Carvajal <julius@talisman.xyz>
2021-10-28 21:21:44 +1100

    chore: ramp api key (#49)

--- src/util/fiatOnRamp.ts
+++ src/util/fiatOnRamp.ts
@@ -4,5 +4,6 @@ export function buyNow() {
   new RampInstantSDK({
     hostAppName: process.env.REACT_APP_APPLICATION_NAME || 'Talisman',
     hostLogoUrl: 'https://pbs.twimg.com/profile_images/1433018747762085891/ZATzx-HG_400x400.jpg',
+    hostApiKey: process.env.RAMP_API_KEY,
   }).show()
 }
