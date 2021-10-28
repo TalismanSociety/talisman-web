4b271b3c8ff8247c4fb7f9062a47bd2377a9a718
arnobase <arnaud.gardies@gmail.com>
2021-10-28 12:30:54 +0200

    feat(i18n): FR localization
    update welcome.json
    add FR fallback into src/libs/i18n/i18n.ts

--- src/libs/i18n/i18n.ts
+++ src/libs/i18n/i18n.ts
@@ -15,7 +15,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: ['en', 'cn'],
+    fallbackLng: ['en', 'cn', 'fr'],
     debug: true,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
