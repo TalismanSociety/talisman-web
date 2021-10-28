c9be1ed2083333bf8817f0509429d1580102e608
Julius Carvajal <julius@talisman.xyz>
2021-10-28 12:24:26 +1100

    fix(crowdloans): links use text instead of icons (#41)

--- src/archetypes/Parachain/Links.tsx
+++ src/archetypes/Parachain/Links.tsx
@@ -1,45 +1,6 @@
-import { ReactComponent as DiscordIcon } from '@assets/icons/discord.svg'
-import { ReactComponent as ElementIcon } from '@assets/icons/element.svg'
-import { ReactComponent as GithubIcon } from '@assets/icons/github.svg'
-import { ReactComponent as LinkedinIcon } from '@assets/icons/linkedin.svg'
-import { ReactComponent as MediumIcon } from '@assets/icons/medium.svg'
-import { ReactComponent as WebsiteIcon } from '@assets/icons/monitor.svg'
-import { ReactComponent as RedditIcon } from '@assets/icons/reddit.svg'
-import { ReactComponent as TelegramIcon } from '@assets/icons/send.svg'
-import { ReactComponent as TwitterIcon } from '@assets/icons/twitter.svg'
-import { ReactComponent as YoutubeIcon } from '@assets/icons/youtube.svg'
 import { Pill } from '@components'
 import { useParachainDetailsById } from '@libs/talisman'
 import styled from 'styled-components'
-
-const Icon = ({ type }) => {
-  switch (type.toLowerCase()) {
-    case 'website':
-      return <WebsiteIcon />
-    case 'twitter':
-      return <TwitterIcon />
-    case 'telegram':
-      return <TelegramIcon />
-    case 'github':
-      return <GithubIcon />
-    case 'medium':
-      return <MediumIcon />
-    case 'discord':
-      return <DiscordIcon />
-    case 'reddit':
-      return <RedditIcon />
-    case 'element':
-      return <ElementIcon />
-    case 'linkedin':
-      return <LinkedinIcon />
-    case 'youtube':
-      return <YoutubeIcon />
-    default:
-      break
-  }
-
-  return type
-}
 
 const Links = styled(({ id, className }) => {
   const { parachainDetails: { links = {} } = {} } = useParachainDetailsById(id)
@@ -49,7 +10,7 @@ const Links = styled(({ id, className }) => {
       {Object.keys(links).map((name, index) => (
         <a key={index} href={links[name]} target="_blank" rel="noreferrer">
           <Pill primary onClick={() => null}>
-            <Icon type={name} />
+            {name}
           </Pill>
         </a>
       ))}
@@ -57,14 +18,9 @@ const Links = styled(({ id, className }) => {
   )
 })`
   display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
   align-items: center;
-
-  > a {
-    display: block;
-    & + a {
-      margin-left: 0.2em;
-    }
-  }
 `
 
 export default Links
