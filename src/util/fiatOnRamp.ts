19476fb5fc3c0ac490837aa0410d7c8ec8d634db
Julius Carvajal <julius@talisman.xyz>
2021-10-30 23:35:14 +1100

    fix(onramp): fiat set default asset (#70)

--- src/util/fiatOnRamp.ts
+++ src/util/fiatOnRamp.ts
@@ -5,5 +5,6 @@ export function buyNow() {
     hostAppName: process.env.REACT_APP_APPLICATION_NAME || 'Talisman',
     hostLogoUrl: 'https://pbs.twimg.com/profile_images/1433018747762085891/ZATzx-HG_400x400.jpg',
     hostApiKey: process.env.RAMP_API_KEY,
+    defaultAsset: 'DOT',
   }).show()
 }
