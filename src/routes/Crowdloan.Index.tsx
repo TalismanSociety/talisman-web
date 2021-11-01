6b4960361fe5b07948fb7bee487ea7aa274c4dbe
juliuscarvajal <julius@talisman.xyz>
2021-11-01 11:21:54 +1100

    fix(crowdloans): full width image banner

--- src/routes/Crowdloan.Index.tsx
+++ src/routes/Crowdloan.Index.tsx
@@ -1,9 +1,13 @@
 import { Crowdloan } from '@archetypes'
+import styled from 'styled-components'
 
-const CrowdloanIndex = ({ className }) => (
+const CrowdloanIndex = styled(({ className }) => (
   <div className={className}>
     <Crowdloan.Index withFilter />
   </div>
-)
+))`
+  max-width: 1280px;
+  margin: 0 auto;
+`
 
 export default CrowdloanIndex
