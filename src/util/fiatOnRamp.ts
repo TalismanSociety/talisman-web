6fa91412e1d5b5c226a18c8a1aaf198f8375418a
juliuscarvajal <julius@talisman.xyz>
2021-11-02 15:32:25 +1100

    chore(ci): fix

--- src/util/fiatOnRamp.ts
+++ src/util/fiatOnRamp.ts
@@ -1,12 +1,16 @@
 import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk'
 
+const REACT_APP_APPLICATION_NAME = process.env.REACT_APP_APPLICATION_NAME
+const RAMP_API_KEY = process.env.RAMP_API_KEY
+const RAMP_URL = process.env.RAMP_URL
+
 export function buyNow() {
   const widget = new RampInstantSDK({
-    hostAppName: process.env.REACT_APP_APPLICATION_NAME || 'Talisman Default Name',
+    hostAppName: REACT_APP_APPLICATION_NAME || 'Talisman Default Name',
     hostLogoUrl: 'https://pbs.twimg.com/profile_images/1433018747762085891/ZATzx-HG_400x400.jpg',
-    hostApiKey: process.env.RAMP_API_KEY,
+    hostApiKey: RAMP_API_KEY,
     defaultAsset: 'DOT',
-    url: process.env.RAMP_URL,
+    url: RAMP_URL,
   })
 
   if (widget) {
