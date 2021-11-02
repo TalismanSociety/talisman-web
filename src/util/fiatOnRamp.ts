d049654dc920f2ec2323c549b3ef34a4d78f193a
Julius Carvajal <julius@talisman.xyz>
2021-11-02 17:34:43 +1100

    chore(ci): fix env vars (#83)

--- src/util/fiatOnRamp.ts
+++ src/util/fiatOnRamp.ts
@@ -1,16 +1,16 @@
 import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk'
 
 const REACT_APP_APPLICATION_NAME = process.env.REACT_APP_APPLICATION_NAME
-const RAMP_API_KEY = process.env.RAMP_API_KEY
-const RAMP_URL = process.env.RAMP_URL
+const REACT_APP_RAMP_API_KEY = process.env.REACT_APP_RAMP_API_KEY
+const REACT_APP_RAMP_URL = process.env.REACT_APP_RAMP_URL
 
 export function buyNow() {
   const widget = new RampInstantSDK({
     hostAppName: REACT_APP_APPLICATION_NAME || 'Talisman Default Name',
     hostLogoUrl: 'https://pbs.twimg.com/profile_images/1433018747762085891/ZATzx-HG_400x400.jpg',
-    hostApiKey: RAMP_API_KEY,
+    hostApiKey: REACT_APP_RAMP_API_KEY,
     defaultAsset: 'DOT',
-    url: RAMP_URL,
+    url: REACT_APP_RAMP_URL,
   })
 
   if (widget) {
