50d42e59a6af803826be2bb1dd99fa75ac25ceba
alecdwm <alec@owls.io>
2021-11-02 12:08:46 +1000

    fix(portfolio): added missing altair parachain

--- src/util/customRpcs.tsx
+++ src/util/customRpcs.tsx
@@ -8,11 +8,14 @@ const customRpcs = {
   '2007': [], // ['wss://shiden.api.onfinality.io/ws?apikey=e1b2f3ea-f003-42f5-adf6-d2e6aa3ecfe4'], // Shiden
   '2023': [], // ['wss://moonriver.api.onfinality.io/ws?apikey=e1b2f3ea-f003-42f5-adf6-d2e6aa3ecfe4'], // Moonriver
   '2084': [], // Calamari
+  // '2085': [], // Parallel Heiko
   '2086': [], // KILT Spiritnet
+  '2088': [], // Altair
   '2090': [
     'wss://basilisk.api.onfinality.io/ws?apikey=e1b2f3ea-f003-42f5-adf6-d2e6aa3ecfe4',
     'wss://rpc-01.basilisk.hydradx.io',
   ], // Basilisk
+  // '2092': [], // Kintsugi BTC
 }
 
 export default customRpcs
