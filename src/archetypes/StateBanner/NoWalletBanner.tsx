6a16ce71b413bab7008ed1ae8f9385b52bc196b7
Julius Carvajal <julius@talisman.xyz>
2021-11-04 12:46:16 +1100

    fix(banners): no wallet messaging (#97)

--- src/archetypes/StateBanner/NoWalletBanner.tsx
+++ src/archetypes/StateBanner/NoWalletBanner.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@components'
 import { Banner } from '@components/Banner'
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
+import { NavLink } from 'react-router-dom'
 
 export const NoWalletBanner = () => {
   const { t, ready } = useTranslation('banners')
@@ -11,12 +12,25 @@ export const NoWalletBanner = () => {
     <Banner>
       <div className="description">
         <h1>{t('noWallet.header')}</h1>
-        <p>{t('noWallet.description')}</p>
+        <p>
+          <Trans i18nKey="noWallet.description" ns="banners">
+            It doesn't look like you’ve got a wallet extension installed. We recommend downloading
+            <a
+              href="https://polkadot.js.org/extension"
+              target="_blank"
+              rel="noreferrer noopener"
+              style={{ textDecoration: 'underline', color: 'inherit', opacity: 'inherit' }}
+            >
+              Polkadot.js
+            </a>
+          </Trans>
+        </p>
+        <p>{t('noWallet.description2')}</p>
       </div>
       <div className="cta">
-        <a href="https://polkadot.js.org/extension" target="_blank" rel="noreferrer noopener">
+        <NavLink to="/crowdloans">
           <Button primary>{t('noWallet.primaryCta')}</Button>
-        </a>
+        </NavLink>
       </div>
     </Banner>
   )
