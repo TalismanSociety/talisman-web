6525f3af65f8f2c42c1d00c877b272e6ae4d1ab7
juliuscarvajal <julius@talisman.xyz>
2021-11-05 22:44:16 +1100

    fix(crowdloans): acala info link

--- src/archetypes/Wallet/Crowdloans.tsx
+++ src/archetypes/Wallet/Crowdloans.tsx
@@ -15,15 +15,18 @@ import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 
-const CrowdloanItem = styled(({ id, className }) => {
+const CrowdloanItem = styled(({ id, className, infoOnly = false }) => {
   const { crowdloan } = useCrowdloanById(id)
   const parachainId = crowdloan?.parachain.paraId
   const relayChainId = useMemo(() => id.split('-')[0], [id])
   const relayChain = useChain(relayChainId)
   const chain = useChain(parachainId)
 
+  // Only for info
+  const infoChain = useChain(id)
+
   const { nativeToken: relayNativeToken, tokenDecimals: relayTokenDecimals } = relayChain
-  const { name, longName } = chain
+  const { name, longName } = infoOnly ? infoChain : chain
   const { price: relayTokenPrice, loading: relayPriceLoading } = useTokenPrice(relayNativeToken)
 
   const accounts = useAccountAddresses()
@@ -49,24 +52,31 @@ const CrowdloanItem = styled(({ id, className }) => {
   return (
     <div className={`${className} ${id}`}>
       <span className="left">
-        <Info title={name} subtitle={longName || name} graphic={<ChainLogo chain={chain} type="logo" size={4} />} />
-      </span>
-      <span className="right">
         <Info
-          title={
-            <Pendor suffix={` ${relayTokenSymbol} Contributed`}>
-              {contributedTokens && formatCommas(contributedTokens)}
-            </Pendor>
-          }
-          subtitle={
-            contributedTokens ? (
-              <Pendor prefix={!contributedUsd && '-'} require={!relayPriceLoading}>
-                {contributedUsd && formatCurrency(contributedUsd)}
-              </Pendor>
-            ) : null
-          }
+          title={name}
+          subtitle={longName || name}
+          graphic={<ChainLogo chain={infoOnly ? infoChain : chain} type="logo" size={4} />}
         />
       </span>
+      {infoOnly}
+      {!infoOnly && (
+        <span className="right">
+          <Info
+            title={
+              <Pendor suffix={` ${relayTokenSymbol} Contributed`}>
+                {contributedTokens && formatCommas(contributedTokens)}
+              </Pendor>
+            }
+            subtitle={
+              contributedTokens ? (
+                <Pendor prefix={!contributedUsd && '-'} require={!relayPriceLoading}>
+                  {contributedUsd && formatCurrency(contributedUsd)}
+                </Pendor>
+              ) : null
+            }
+          />
+        </span>
+      )}
     </div>
   )
 })`
@@ -131,11 +141,29 @@ const Crowdloans = ({ className }: { className?: string }) => {
         ) : Object.keys(totalAliveContributions).length < 1 ? (
           <PanelSection comingSoon>{`${`😕 `} ${t('You have not contributed to any Crowdloans')}`}</PanelSection>
         ) : (
-          Object.keys(totalAliveContributions).map(id => (
-            <PanelSection key={id}>
-              <CrowdloanItem id={id} />
+          <>
+            {Object.keys(totalAliveContributions).map(id => (
+              <PanelSection key={id}>
+                <CrowdloanItem id={id} />
+              </PanelSection>
+            ))}
+            {/* Hardcode for now */}
+            <PanelSection>
+              <CrowdloanItem
+                id="0-2000"
+                infoOnly={
+                  <a
+                    href="https://apps.acala.network/wallet"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ textDecoration: 'underline', color: 'var(--color-primary)' }}
+                  >
+                    {t('View contributions')}
+                  </a>
+                }
+              />
             </PanelSection>
-          ))
+          </>
         )}
       </Panel>
     </section>
