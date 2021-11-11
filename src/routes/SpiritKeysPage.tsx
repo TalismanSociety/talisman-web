b36cbeccfde53ff9dae202d96d827a58fa23e1c4
juliuscarvajal <julius@talisman.xyz>
2021-11-11 23:19:16 +1100

    fix(spirit-keys): copy changes and nft animation

--- src/routes/SpiritKeysPage.tsx
+++ src/routes/SpiritKeysPage.tsx
@@ -430,6 +430,143 @@ const SendNft = styled(({ className, nft }) => {
   }
 `
 
+const SpiritKeyNft = styled(({ className, src }) => {
+  return (
+    <div className={className}>
+      <div className="floating-card__card">
+        <div className="floating-card__card__inner-container">
+          <img className="spirit-key-image" src={src} alt="Spirit Key" />
+
+          <div className="floating-card__card__inner-container__shimmer"></div>
+        </div>
+
+        <div className="floating-card__card__background-scroller"></div>
+      </div>
+
+      <div className="floating-card__floor-shadow"></div>
+    </div>
+  )
+})`
+  .spirit-key-image {
+    width: 100%;
+    object-fit: cover;
+  }
+
+  .floating-card__card {
+    width: 316px;
+    height: 316px;
+    background: #fff;
+    position: relative;
+    margin: 4rem auto 0;
+    padding: 10px;
+    text-align: center;
+    box-shadow: 1px 1px 1px #000;
+    overflow: hidden;
+    animation: floating 10s ease-in-out infinite;
+  }
+
+  .floating-card__card__inner-container {
+    width: 97%;
+    height: 97%;
+    background: #dbdbdb;
+    margin: 0 auto;
+    z-index: 2;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    overflow: hidden;
+  }
+
+  .floating-card__card__inner-container__shimmer {
+    width: 200%;
+    height: 40px;
+    background: #eee;
+    position: absolute;
+    top: -80%;
+    left: 50%;
+    transform: translate(-50%, -50%) rotate(-45deg);
+    animation-name: slideBottomRightAndBack;
+    animation-duration: 4s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+  }
+
+  .floating-card__card__background-scroller {
+    width: 1800px;
+    height: 300px;
+    background: orange;
+    position: absolute;
+    top: -800px;
+    left: 20%;
+    transform: translate(-50%, 0) rotate(-35deg);
+    animation-name: slideBottomRight;
+    animation-duration: 5s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+  }
+
+  .floating-card__floor-shadow {
+    width: 200px;
+    height: 10px;
+    background: #000;
+    margin: 20px auto 0 auto;
+    border-radius: 40%;
+    animation: scalingShadow 10s ease-in-out infinite;
+  }
+
+  @keyframes floating {
+    25% {
+      transform: translateY(5px);
+    }
+    50% {
+      transform: translateY(0px);
+    }
+    75% {
+      transform: translateY(5px);
+    }
+    100% {
+      transform: translateY(0px);
+    }
+  }
+
+  @keyframes scalingShadow {
+    25% {
+      transform: scale(1.05);
+    }
+    50% {
+      transform: scale(1);
+    }
+    75% {
+      transform: scale(1.05);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  @keyframes slideBottomRight {
+    0% {
+      transform: translate(-50%, 0) rotate(-35deg);
+    }
+    100% {
+      transform: translate(-50%, 2200px) rotate(-35deg);
+    }
+  }
+
+  @keyframes slideBottomRightAndBack {
+    0% {
+      transform: translate(-50%, 0) rotate(-45deg);
+    }
+    90% {
+      transform: translate(-50%, 0) rotate(-45deg);
+    }
+    100% {
+      transform: translate(-50%, 900px) rotate(-45deg);
+    }
+  }
+`
+
 const SpiritKey = styled(({ className }) => {
   const baseImage = 'https://rmrk.mypinata.cloud/ipfs/bafybeicuuasrqnqndfw3k6rqacfpfil5sc5fhyjh63riqnd2imm5eucrk4'
   const [currentNFT, setCurrentNFT] = useState<number>(0)
@@ -466,11 +603,7 @@ const SpiritKey = styled(({ className }) => {
     <section className={className}>
       {isMobileBrowser() && <DesktopRequired />}
       <div className="content">
-        <img
-          className="spirit-key-image"
-          src={hasNfts ? nft?.image?.replace('ipfs://', 'https://rmrk.mypinata.cloud/') : baseImage}
-          alt="Spirit Key"
-        />
+        <SpiritKeyNft src={hasNfts ? nft?.image?.replace('ipfs://', 'https://rmrk.mypinata.cloud/') : baseImage} />
         <p>You have {hasNfts ? totalNFTs.length : 'no'} Spirit Keys</p>
         {totalNFTs.length < 1 && (
           <div className="empty-state-buttons-div">
@@ -517,11 +650,11 @@ const SpiritKey = styled(({ className }) => {
             <ul>
               <li>The Talisman Discord Server</li>
               <li>Various DotSama communities</li>
-              <li>Contribute to Crowdloans during the Spirit Hour(See Twitter for more info)</li>
+              <li>Contribute to Crowdloans during the Spirit Hour</li>
             </ul>
           </div>
           <div className="section">
-            <h2>Benefits?</h2>
+            <h2>Benefits</h2>
             <p>Become a Paraverse explorer and gain special access to a variety of perks including:</p>
             <ul>
               <li>Participate in the Talisman Wallet Alpha release</li>
@@ -532,15 +665,22 @@ const SpiritKey = styled(({ className }) => {
         </div>
         <article>
           <h1>
-            Talisman Seeker, it's your spirit that's key. At the end of your seeking, you'll finally be free. A key to
-            the new world is a difficult thing. You should ask yourself honestly, "what on earth should I bring?" Give a
-            moment of thought as to whether it can be brought. All I can guarantee, is that nothing is for naught.
+            <li>Talisman Seeker, it's your spirit that's key.</li>
+            <li>At the end of your seeking, you'll finally be free.</li>
+            <li>A key to the new world is a difficult thing.</li>
+            <li>You should ask yourself honestly, "what on earth should I bring?"</li>
+            <li>Give a moment of thought as to whether it can be brought.</li>
+            <li>All I can guarantee, is that nothing is for naught.</li>
           </h1>
         </article>
       </div>
     </section>
   )
 })`
+  h2 {
+    color: var(--color-text);
+  }
+
   > .content {
     width: 100%;
     max-width: 1280px;
@@ -580,14 +720,6 @@ const SpiritKey = styled(({ className }) => {
         font-size: 16px;
         font-weight: 400;
       }
-    }
-
-    .spirit-key-image {
-      width: 316px;
-      height: 316px;
-      margin: 4rem auto 2rem auto;
-      border-radius: 2rem;
-      object-fit: cover;
     }
 
     p {
@@ -687,7 +819,7 @@ const SpiritKey = styled(({ className }) => {
     }
 
     > article {
-      width: 75%;
+      width: 80%;
       color: var(--color-text);
       text-align: center;
       margin: 6rem auto 0 auto;
@@ -696,6 +828,7 @@ const SpiritKey = styled(({ className }) => {
         font-family: 'ATApocRevelations', sans-serif;
         font-size: 4rem;
         font-weight: 400;
+        list-style-type: none;
       }
     }
   }
