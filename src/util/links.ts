7d3a8c675947a8a1eaa04cb4725bace59033f289
juliuscarvajal <julius@talisman.xyz>
2021-12-15 16:56:58 +1100

    chore: update talisman extension download url

--- src/util/links.ts
+++ src/util/links.ts
@@ -1,4 +1,4 @@
 export const SPIRIT_KEY_URL = 'https://medium.com/we-are-talisman/seek-the-spirit-key-enter-the-talisman-b68d3541157b'
 export const DISCORD_JOIN_URL = 'https://discord.gg/EMBQKVeedJ'
 export const TALISMAN_EXTENSION_DOWNLOAD_URL =
-  'https://drive.google.com/file/d/1-4sKzAgVd7vb23UE4Kb3v-JA3MK4P5I0/view?usp=sharing'
+  'https://drive.google.com/drive/folders/1OqQ6I7HGKeW3WmsS_R_XJzXEl0wge1lU?usp=sharing'
