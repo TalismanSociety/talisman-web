098b517d660e5e49ff7877bcedcf4e5a403ef1db
juliuscarvajal <julius@talisman.xyz>
2021-12-17 17:25:07 +1100

    fix: update url

--- src/util/links.ts
+++ src/util/links.ts
@@ -1,4 +1,4 @@
 export const SPIRIT_KEY_URL = 'https://medium.com/we-are-talisman/seek-the-spirit-key-enter-the-talisman-b68d3541157b'
 export const DISCORD_JOIN_URL = 'https://discord.gg/EMBQKVeedJ'
 export const TALISMAN_EXTENSION_DOWNLOAD_URL =
-  'https://drive.google.com/u/0/uc?id=1thxY8xCgMDa6snC36Dp8_paInyG_GcWa&export=download'
+  'https://drive.google.com/u/0/uc?id=1M-nOghb0VhKeEOfCtmWeCKc7FboppQ9X&export=download'
