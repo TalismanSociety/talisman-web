e14ce6d4f559a0613eaa3007083946e1198fefe9
juliuscarvajal <julius@talisman.xyz>
2022-02-08 19:03:12 +0800

    fix: try suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ModalProvider } from '@components'
+import { TalismanHandLoader } from '@components/TalismanHandLoader'
 import * as Crowdloans from '@libs/crowdloans'
 import * as MoonbeamContributors from '@libs/moonbeam-contributors'
 import * as Portfolio from '@libs/portfolio'
@@ -10,8 +11,26 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 import ThemeProvider from './App.Theme'
 
+const Loader = () => {
+  return (
+    <div
+      style={{
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        left: 0,
+        right: 0,
+      }}
+    >
+      <TalismanHandLoader />
+    </div>
+  )
+}
+
 const App: React.FC = () => (
-  <Suspense fallback="Loading...">
+  <Suspense fallback={<Loader />}>
     <Portfolio.Provider>
       <Tokenprices.Provider>
         <TalismanProvider>
