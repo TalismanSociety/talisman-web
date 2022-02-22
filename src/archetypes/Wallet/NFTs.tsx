ea19f68439abe347692ad89bba5cee8e3cbc5fd8
juliuscarvajal <julius@talisman.xyz>
2022-02-22 09:19:45 +0800

    fix

--- src/archetypes/Wallet/NFTs.tsx
+++ src/archetypes/Wallet/NFTs.tsx
@@ -2,6 +2,7 @@ import '@talisman-connect/nft/nft.esm.css'
 
 import { useAccountAddresses } from '@libs/talisman'
 import { NftCard, useNftsByAddress } from '@talisman-connect/nft'
+import { device } from '@util/breakpoints'
 import { Key } from 'react'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
@@ -14,13 +15,7 @@ const NFTs = styled(({ className }: { className?: string }) => {
   return (
     <section className={`wallet-assets ${className}`}>
       <h1>{t('NFTs')}</h1>
-      <div
-        style={{
-          display: 'grid',
-          gap: '2rem',
-          gridTemplateColumns: '1fr 1fr 1fr',
-        }}
-      >
+      <div className="nft-grid">
         {nfts?.map((nft: { id: Key | null | undefined }) => {
           return <NftCard key={nft.id} nft={nft} />
         })}
@@ -28,6 +23,20 @@ const NFTs = styled(({ className }: { className?: string }) => {
     </section>
   )
 })`
+  .nft-grid {
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: 1fr 1fr;
+
+    @media ${device.lg} {
+      grid-template-columns: repeat(3, 1fr);
+    }
+
+    @media ${device.xl} {
+      grid-template-columns: repeat(4, 1fr);
+    }
+  }
+
   h1 {
     display: flex;
     align-items: baseline;
