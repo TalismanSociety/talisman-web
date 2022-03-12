fd5b36bd752aaadfe0f1b2c3c043c794a132ad0e
juliuscarvajal <julius@talisman.xyz>
2022-03-12 17:17:54 +0800

    fix(portfolio): updated banner and other fixes

--- src/archetypes/StateBanner/NoWalletBanner.tsx
+++ src/archetypes/StateBanner/NoWalletBanner.tsx
@@ -1,22 +1,29 @@
 import bannerImage from '@assets/agyle-extension.png'
 import { Button } from '@components'
 import { Banner } from '@components/Banner'
+import { DAPP_NAME } from '@libs/talisman'
+import { useTalismanInstalled } from '@libs/talisman/useIsTalismanInstalled'
+import { WalletSelect } from '@talisman-connect/components'
 import { TALISMAN_EXTENSION_CHROMESTORE_URL } from '@util/links'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 
 export const NoWalletBanner = styled(({ className }) => {
-  const { t, ready } = useTranslation('banners')
-  if (!ready) {
-    return null
-  }
+  const { t } = useTranslation('banners')
+  const { t: tBase } = useTranslation('banners')
+  const isTalismanInstalled = useTalismanInstalled()
   return (
     <Banner className={className}>
       <span className="heavy">{t('noWallet.header')}</span>
       <div className="cta">
-        <a href={TALISMAN_EXTENSION_CHROMESTORE_URL} target="_blank" rel="noopener noreferrer">
-          <Button primary>{t('noWallet.primaryCta')}</Button>
-        </a>
+        {isTalismanInstalled && (
+          <WalletSelect dappName={DAPP_NAME} triggerComponent={<Button primary>{tBase('Connect wallet')}</Button>} />
+        )}
+        {!isTalismanInstalled && (
+          <a href={TALISMAN_EXTENSION_CHROMESTORE_URL} target="_blank" rel="noopener noreferrer">
+            <Button primary>{t('noWallet.primaryCta')}</Button>
+          </a>
+        )}
       </div>
     </Banner>
   )
