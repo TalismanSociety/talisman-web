d766b3228c7f1818605205656ddb0710b9fdfe3a
darah3ng <daragitgud@gmail.com>
2022-03-14 15:50:42 +1000

    feat(nft): nfts sort by collection component

---
+++ src/components/NFTsBySort.tsx
@@ -0,0 +1,55 @@
+import { NftCard } from '@talisman-components/nft'
+import { device } from '@util/breakpoints'
+import styled from 'styled-components'
+
+const NFTsGridWrapper = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: 1fr;
+  margin-bottom: 3rem;
+
+  @media ${device.md} {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media ${device.lg} {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media ${device.xl} {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`
+
+export interface NFTsBySortProps {
+  sortedNfts: any[]
+}
+
+const NFTsBySort = ({ sortedNfts }: NFTsBySortProps) => {
+  if (!sortedNfts) {
+    return <h3>No sorted NFTs found!</h3>
+  }
+
+  const renderCollection = Object.keys(sortedNfts).map((collectionName: any) => {
+    return (
+      <div key={collectionName}>
+      <h2>{collectionName}</h2>
+
+      <NFTsGridWrapper>
+        {sortedNfts[collectionName].map((nft: any) => (
+            <NftCard key={nft.id} nft={nft} />
+          )
+        )}
+      </NFTsGridWrapper>
+    </div>
+    )
+  })
+
+  return (
+    <div>
+      {renderCollection}
+    </div>
+  )
+}
+
+export { NFTsGridWrapper, NFTsBySort }
