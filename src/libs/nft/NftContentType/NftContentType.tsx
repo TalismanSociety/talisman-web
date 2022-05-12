531e3f0eaa283dee406b65567613e9161c124416
Joshua <ahimaz.joshua@gmail.com>
2022-05-12 09:46:01 +0800

    Added NFT Icons

--- src/libs/nft/NftContentType/NftContentType.tsx
+++ src/libs/nft/NftContentType/NftContentType.tsx
@@ -1,7 +1,13 @@
 import { NftElement } from '../../../util/nfts/types'
 import useNftAsset from '../useNftAsset/useNftAsset'
 import styles from './NftContentType.module.css'
-import { ReactComponent as PlayCircleIcon } from './play-circle.svg'
+// import { ReactComponent as PlayCircleIcon } from './play-circle.svg'
+// Icons Import
+import { ReactComponent as NFTAudioIcon } from '@assets/icons/NFT-Audio-icon.svg'
+import { ReactComponent as NFT3DIcon } from '@assets/icons/NFT-3D-icon.svg'
+import { ReactComponent as NFTVideoIcon } from '@assets/icons/NFT-Video-icon.svg'
+import { ReactComponent as NFTPDFIcon } from '@assets/icons/NFT-PDF-icon.svg'
+
 
 export type NftContentTypes = 'audio' | 'video' | 'image'
 
@@ -10,9 +16,13 @@ export function NftContentType(props: NftElement) {
   const { contentCategory: type } = useNftAsset(nft)
   switch (type) {
     case 'audio':
-      return <PlayCircleIcon className={styles['nft-content-type-root']} />
+      return <NFTAudioIcon className={styles['nft-content-type-root']} />
     case 'video':
-      return <PlayCircleIcon className={styles['nft-content-type-root']} />
+      return <NFTVideoIcon className={styles['nft-content-type-root']} />
+    case 'model':
+      return <NFT3DIcon className={styles['nft-content-type-root']} />
+    case 'application':
+      return <NFTPDFIcon className={styles['nft-content-type-root']} />
     default:
       return null
   }
