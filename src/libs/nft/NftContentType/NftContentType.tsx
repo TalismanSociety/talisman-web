99cf7ee64a77fc0acf8c60a4ca4ee8cde41cdfa8
JAhimaz <JAhimaz@users.noreply.github.com>
2022-05-16 04:41:03 +0000

    ci: prettier format

--- src/libs/nft/NftContentType/NftContentType.tsx
+++ src/libs/nft/NftContentType/NftContentType.tsx
@@ -16,13 +16,15 @@ export function NftContentType(props: NftElement) {
   const { contentCategory: type } = useNftAsset(nft)
   switch (type) {
     case 'audio':
-      return <img src={NFTAudioIcon} className={styles['nft-content-type-root']} title="Audio" alt="Audio NFT"/>
+      return <img src={NFTAudioIcon} className={styles['nft-content-type-root']} title="Audio" alt="Audio NFT" />
     case 'video':
-      return <img src={NFTVideoIcon} className={styles['nft-content-type-root']} title="Video" alt="Video NFT"/>
+      return <img src={NFTVideoIcon} className={styles['nft-content-type-root']} title="Video" alt="Video NFT" />
     case 'model':
-      return <img src={NFT3DIcon} className={styles['nft-content-type-root']} title="3D Model" alt="3D NFT"/>
+      return <img src={NFT3DIcon} className={styles['nft-content-type-root']} title="3D Model" alt="3D NFT" />
     case 'application':
-      return <img src={NFTPDFIcon} className={styles['nft-content-type-root']} title="PDF / Application" alt="PDF NFT"/>
+      return (
+        <img src={NFTPDFIcon} className={styles['nft-content-type-root']} title="PDF / Application" alt="PDF NFT" />
+      )
     default:
       return null
   }
