e3c5836379a4490ba755f32f716f250b53374d3b
Josh <ahimaz.joshua@gmail.com>
2022-05-16 12:39:40 +0800

    Added Alts to mini icons

--- src/libs/nft/NftContentType/NftContentType.tsx
+++ src/libs/nft/NftContentType/NftContentType.tsx
@@ -16,13 +16,13 @@ export function NftContentType(props: NftElement) {
   const { contentCategory: type } = useNftAsset(nft)
   switch (type) {
     case 'audio':
-      return <img src={NFTAudioIcon} className={styles['nft-content-type-root']} title="Audio" />
+      return <img src={NFTAudioIcon} className={styles['nft-content-type-root']} title="Audio" alt="Audio NFT"/>
     case 'video':
-      return <img src={NFTVideoIcon} className={styles['nft-content-type-root']} title="Video" />
+      return <img src={NFTVideoIcon} className={styles['nft-content-type-root']} title="Video" alt="Video NFT"/>
     case 'model':
-      return <img src={NFT3DIcon} className={styles['nft-content-type-root']} title="3D Model" />
+      return <img src={NFT3DIcon} className={styles['nft-content-type-root']} title="3D Model" alt="3D NFT"/>
     case 'application':
-      return <img src={NFTPDFIcon} className={styles['nft-content-type-root']} title="PDF / Application" />
+      return <img src={NFTPDFIcon} className={styles['nft-content-type-root']} title="PDF / Application" alt="PDF NFT"/>
     default:
       return null
   }
