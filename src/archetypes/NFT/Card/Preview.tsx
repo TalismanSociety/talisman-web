0b423781a0229709d355f66af6039d2575a573ee
JAhimaz <JAhimaz@users.noreply.github.com>
2022-07-27 06:09:32 +0000

    ci: prettier format

--- src/archetypes/NFT/Card/Preview.tsx
+++ src/archetypes/NFT/Card/Preview.tsx
@@ -7,9 +7,9 @@ import ModelPlaceholder from '@assets/generic-model.png'
 import PDFPlaceholder from '@assets/generic-pdf.png'
 import UnknownPlaceholder from '@assets/generic-unknown.png'
 import VideoPlaceholder from '@assets/generic-video.png'
+import { Spinner } from '@components'
 import { NFTDetail } from '@libs/@talisman-nft/types'
 import styled from 'styled-components'
-import { Spinner } from '@components'
 
 type PreviewType = {
   className?: string
