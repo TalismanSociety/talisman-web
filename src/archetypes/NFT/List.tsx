49bbca706c4118763a1855268f218e274f5a22fc
Joshua <ahimaz.joshua@gmail.com>
2022-08-03 11:24:10 +0800

    Removed Stuff

--- src/archetypes/NFT/List.tsx
+++ src/archetypes/NFT/List.tsx
@@ -51,8 +51,6 @@ const List = ({ address }: ListPropsType) => {
 
   useEffect(() => {
     setAddress(address)
-    // setAddress("ESMddXAAxypPjiRnFinnHWfmG5V4H2sZB5P6g3QFXjzUwYF")
-    // setAddress("FAvF9v7EF6oVKTUwk7v6GNPCPjjBPtmHq5fwR71PyVwQrTf")
   }, [address, setAddress])
 
   return !!loading ? (
