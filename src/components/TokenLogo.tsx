edf19e70ac524d59adf3d6d283b8a4daf8d248b0
Josh <ahimaz.joshua@gmail.com>
2022-10-03 09:18:18 +0800

    feat: added fiat balances to the assets

---
+++ src/components/TokenLogo.tsx
@@ -0,0 +1,21 @@
+import { Image } from '@components'
+import styled from 'styled-components'
+
+export default styled(({ token, type, className }) => {
+  return (
+    <Image
+      src={token.logo}
+      alt={`${token.symbol} ${type}`}
+      className={`token-asset token-${type} ${className}`}
+      data-type={type}
+    />
+  )
+})`
+  &[data-type='logo'] {
+    font-size: ${({ size = 8 }) => `${size}rem`};
+    width: 1em;
+    height: 1em;
+    border-radius: 50%;
+    display: block;
+  }
+`
