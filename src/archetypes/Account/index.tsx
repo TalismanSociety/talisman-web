c0d9b76d504e14a1a3767bffe7a069122a7f4a1a
alecdwm <alec@owls.io>
2022-10-19 23:26:45 +0000

    chore: removed unused components

--- src/archetypes/Account/index.tsx
+++ src/archetypes/Account/index.tsx
@@ -1,3 +1,2 @@
 export { default as Button } from './Button'
-export { default as Picker } from './Picker'
 export { default as Wallet } from './Wallet'
--- src/archetypes/Chain/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as LogoById } from './LogoById'
