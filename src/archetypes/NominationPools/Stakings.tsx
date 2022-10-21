3db62fac38f6b7e21fa6f4bba5920a5e0f28e909
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2022-10-21 15:20:34 +1300

    refactor: cleanup pools folder structure

---
+++ src/archetypes/NominationPools/Stakings.tsx
@@ -0,0 +1,144 @@
+import Button from '@components/atoms/Button'
+import Text from '@components/atoms/Text'
+import HiddenDetails from '@components/molecules/HiddenDetails'
+import PoolStake, { PoolStakeList } from '@components/recipes/PoolStake/PoolStake'
+import { selectedPolkadotAccountsState } from '@domains/accounts/recoils'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useRecoilValue, waitForAll } from 'recoil'
+
+import { nativeTokenDecimalState, nativeTokenPriceState } from '../../domains/chains/recoils'
+import useChainState from '../../domains/common/hooks/useChainState'
+import useExtrinsic from '../../domains/common/hooks/useExtrinsic'
+import { allPendingPoolRewardsState } from '../../domains/nominationPools/recoils'
+import AddStakeDialog from './AddStakeDialog'
+import UnstakeDialog from './UnstakeDialog'
+
+const Stakings = () => {
+  const claimPayoutExtrinsic = useExtrinsic('nominationPools', 'claimPayout')
+  const unbondExtrinsic = useExtrinsic('nominationPools', 'unbond')
+
+  const [pendingRewards, accounts, decimalFromAtomics, nativeTokenPrice] = useRecoilValue(
+    waitForAll([
+      allPendingPoolRewardsState,
+      selectedPolkadotAccountsState,
+      nativeTokenDecimalState,
+      nativeTokenPriceState('usd'),
+    ])
+  )
+
+  const poolMembersLoadable = useChainState(
+    'query',
+    'nominationPools',
+    'poolMembers.multi',
+    accounts.map(({ address }) => address)
+  )
+
+  const poolMetadatumLoadable = useChainState(
+    'query',
+    'nominationPools',
+    'metadata.multi',
+    poolMembersLoadable.valueMaybe()?.map(x => x.unwrapOrDefault().poolId) ?? [],
+    { enabled: poolMembersLoadable.state === 'hasValue' }
+  )
+
+  const pools = useMemo(
+    () =>
+      poolMembersLoadable.state !== 'hasValue'
+        ? undefined
+        : poolMembersLoadable.contents
+            .map((poolMember, index) => {
+              return {
+                account: accounts[index],
+                poolName: poolMetadatumLoadable.valueMaybe()?.[index]?.toUtf8(),
+                poolMember,
+                pendingRewards: pendingRewards.find(rewards => rewards[0] === accounts[index]?.address)?.[1],
+              }
+            })
+            .filter(x => x.poolMember.isSome && !x.poolMember.unwrapOrDefault().points.isZero()),
+    [poolMembersLoadable.state, poolMembersLoadable.contents, accounts, poolMetadatumLoadable, pendingRewards]
+  )
+
+  const [unstakeAccount, setUnstakeAccount] = useState<string>()
+  const [addStakeAccount, setAddStakeAccount] = useState<string>()
+
+  useEffect(() => {
+    if (unbondExtrinsic.state === 'loading' && unbondExtrinsic.contents?.status.isBroadcast) {
+      setUnstakeAccount(undefined)
+    }
+  }, [unbondExtrinsic.contents?.status?.isBroadcast, unbondExtrinsic.state])
+
+  return (
+    <div>
+      <AddStakeDialog account={addStakeAccount} onDismiss={useCallback(() => setAddStakeAccount(undefined), [])} />
+      <UnstakeDialog account={unstakeAccount} onDismiss={useCallback(() => setUnstakeAccount(undefined), [])} />
+      <header>
+        <Text.H4>Staking</Text.H4>
+      </header>
+      {pools?.length === 0 && (
+        <HiddenDetails
+          hidden
+          overlay={
+            <div
+              css={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                gap: '3.2rem',
+              }}
+            >
+              <Text.Body>You have no staked assets yet...</Text.Body>
+              <Button as={Link} variant="outlined" to="/staking">
+                Get started
+              </Button>
+            </div>
+          }
+        >
+          <PoolStakeList>
+            <PoolStake.Skeleton animate={false} />
+            <PoolStake.Skeleton animate={false} />
+            <PoolStake.Skeleton animate={false} />
+          </PoolStakeList>
+        </HiddenDetails>
+      )}
+      <PoolStakeList>
+        {pools?.map(pool => (
+          <PoolStake
+            accountName={pool.account?.name ?? ''}
+            accountAddress={pool.account?.address ?? ''}
+            stakingAmount={decimalFromAtomics.fromAtomics(pool.poolMember.unwrapOrDefault().points).toHuman()}
+            stakingAmountInFiat={(
+              decimalFromAtomics.fromAtomics(pool.poolMember.unwrapOrDefault().points).toFloatApproximation() *
+              nativeTokenPrice
+            ).toLocaleString(undefined, { style: 'currency', currency: 'usd', currencyDisplay: 'narrowSymbol' })}
+            rewardsAmount={decimalFromAtomics.fromAtomics(pool.pendingRewards?.toString()).toHuman()}
+            rewardsAmountInFiat={(
+              decimalFromAtomics.fromAtomics(pool.pendingRewards).toFloatApproximation() * nativeTokenPrice
+            ).toLocaleString(undefined, { style: 'currency', currency: 'usd', currencyDisplay: 'narrowSymbol' })}
+            poolName={pool.poolName ?? ''}
+            onRequestClaim={() => claimPayoutExtrinsic.signAndSend(pool.account?.address ?? '')}
+            claimState={
+              pool.pendingRewards?.isZero() ?? true
+                ? 'unavailable'
+                : claimPayoutExtrinsic.state === 'loading'
+                ? 'pending'
+                : undefined
+            }
+            onRequestUnstake={() => setUnstakeAccount(pool.account?.address)}
+            unstakeState={
+              pool.poolMember.unwrapOrDefault().points.isZero()
+                ? 'unavailable'
+                : unbondExtrinsic.state === 'loading'
+                ? 'pending'
+                : undefined
+            }
+            onRequestAdd={() => setAddStakeAccount(pool.account?.address)}
+          />
+        ))}
+      </PoolStakeList>
+    </div>
+  )
+}
+
+export default Stakings
