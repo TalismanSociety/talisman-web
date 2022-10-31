c146482510138987f514966cf1d7115a9f77b863
Jonathan Dunne <jonathan@talisman.xyz>
2022-11-01 02:36:34 +1100

    fix: change nom pools available block (#334)

--- src/domains/nominationPools/hooks/useCountDownToNomsPool.ts
+++ src/domains/nominationPools/hooks/useCountDownToNomsPool.ts
@@ -3,7 +3,7 @@ import { useChainState } from '@domains/common/hooks'
 import { BN } from '@polkadot/util'
 import { useRecoilValue } from 'recoil'
 
-const AVAILABLE_AT_BLOCK = new BN(12_721_880)
+const AVAILABLE_AT_BLOCK = new BN(12_722_480)
 
 export const useCountDownToNomsPool = () => {
   const api = useRecoilValue(apiState)
