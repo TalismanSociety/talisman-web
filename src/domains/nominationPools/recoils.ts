502bc7918633d785fb04c128d4c036eebd36f399
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2022-11-03 17:26:46 +1300

    feat: include all pools & push non-recommended pools toward the end

--- src/domains/nominationPools/recoils.ts
+++ src/domains/nominationPools/recoils.ts
@@ -3,11 +3,10 @@ import '@polkadot/api-augment/substrate'
 import { polkadotAccountsState } from '@domains/accounts/recoils'
 import { chainReadIdState } from '@domains/common/recoils'
 import type { AnyNumber } from '@polkadot/types-codec/types'
-import { BN } from '@polkadot/util'
 import DotPoolSelector, { ValidatorSelector, defaultOptions } from '@talismn/dot-pool-selector'
 import { SerializableParam, selector, selectorFamily } from 'recoil'
 
-import { apiState, chainIdState } from '../chains/recoils'
+import { apiState } from '../chains/recoils'
 
 export const allPendingPoolRewardsState = selector({
   key: 'AllPendingRewards',
@@ -43,33 +42,17 @@ export const recommendedPoolsState = selector({
   key: 'Staking/BondedPools',
   get: async ({ get }) => {
     const api = get(apiState)
-    const chainId = get(chainIdState)
 
-    const recommendedPools = await new DotPoolSelector(
-      new ValidatorSelector(api),
-      api,
-      chainId === 'polkadot'
-        ? {
-            ...defaultOptions,
-            numberOfPools: Infinity,
-            rootMinStake: new BN(10),
-            minNumberOfValidators: 16,
-            checkRootVerified: true,
-            checkForDuplicateValidators: true,
-          }
-        : { ...defaultOptions, numberOfPools: Infinity }
-    ).getPoolsMeetingCriteria()
+    const recommendedPoolIds = await new DotPoolSelector(new ValidatorSelector(api), api, {
+      ...defaultOptions,
+      numberOfPools: Infinity,
+    })
+      .getPoolsMeetingCriteria()
+      .then(x => x.map(({ poolId }) => poolId))
 
-    const pools =
-      recommendedPools.length > 0
-        ? await api.query.nominationPools.bondedPools
-            .multi(recommendedPools.map(({ poolId }) => poolId))
-            .then(bondedPools =>
-              bondedPools.map((pool, index) => ({ poolId: recommendedPools[index]?.poolId ?? 0, bondedPool: pool }))
-            )
-        : await api.query.nominationPools.bondedPools
-            .entries()
-            .then(x => x.map(y => ({ poolId: y[0].args[0].toNumber() ?? 0, bondedPool: y[1] })))
+    const pools = await api.query.nominationPools.bondedPools
+      .entries()
+      .then(x => x.map(y => ({ poolId: y[0].args[0].toNumber() ?? 0, bondedPool: y[1] })))
 
     const names = await api.query.nominationPools.metadata.multi(pools.map(({ poolId }) => poolId))
 
@@ -77,7 +60,13 @@ export const recommendedPoolsState = selector({
       .map((pool, index) => ({ ...pool, name: names[index]?.toUtf8() }))
       .filter(pool => pool.bondedPool.isSome)
       .map(pool => ({ ...pool, bondedPool: pool.bondedPool.unwrap() }))
-      .sort((a, b) => b.bondedPool.points.sub(a.bondedPool.points).toNumber())
+      .sort((a, b) =>
+        recommendedPoolIds.includes(a.poolId) && !recommendedPoolIds.includes(b.poolId)
+          ? -1
+          : recommendedPoolIds.includes(b.poolId) && !recommendedPoolIds.includes(a.poolId)
+          ? 1
+          : b.bondedPool.points.sub(a.bondedPool.points).toNumber()
+      )
   },
   cachePolicy_UNSTABLE: { eviction: 'most-recent' },
 })
