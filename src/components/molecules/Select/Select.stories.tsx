550b156ed9633e37544ae1ca6e2c2b222f0f0d8f
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2022-11-05 13:39:19 +1300

    refactor: select component
    - add scroll on overflow
    - better dimiss logic & accessibility label
    - refactor props to match standard HTML select element

--- src/components/molecules/Select/Select.stories.tsx
+++ src/components/molecules/Select/Select.stories.tsx
@@ -16,31 +16,55 @@ export default {
 } as ComponentMeta<typeof Select>
 
 export const Default = () => {
-  const [selected, setSelected] = useState<1 | 2 | 3 | undefined>(undefined)
+  const [selected, setSelected] = useState<string | undefined>(undefined)
 
   return (
-    <Select>
+    <Select placeholder="Select account" value={selected} onChange={value => setSelected(value)}>
       <Select.Item
-        selected={selected === 1}
-        leadingIcon={<Identicon value="5CcU6DRpocLUWYJHuNLjB4gGyHJrkWuruQD5XFbRYffCfSAP" size={40} theme="polkadot" />}
+        value={0}
+        leadingIcon={<Identicon value="5CcU6DRpocLUWYJHuNLjB4gGyHJrkWuruQD5XFbRYffCfSAP" size={40} />}
         headlineText="Polkadot.js Import"
         supportingText="420 DOT"
-        onClick={() => setSelected(1)}
       />
       <Select.Item
-        selected={selected === 2}
-        leadingIcon={<Identicon value="143wN4e1nTTWJZHy1CFVXDHpAg6YJsNn2jDN52J2Xfjf8MWs" size={40} theme="polkadot" />}
+        value={1}
+        leadingIcon={<Identicon value="143wN4e1nTTWJZHy1CFVXDHpAg6YJsNn2jDN52J2Xfjf8MWs" size={40} />}
         headlineText="Yeet Account"
         supportingText="35 DOT"
-        onClick={() => setSelected(2)}
       />
       <Select.Item
-        selected={selected === 3}
-        leadingIcon={<Identicon value="1YmEYgtfPbwx5Jos1PjKDWRpuJWSpTzytwZgYan6kgiquNS" size={40} theme="polkadot" />}
+        value={2}
+        leadingIcon={<Identicon value="1YmEYgtfPbwx5Jos1PjKDWRpuJWSpTzytwZgYan6kgiquNS" size={40} />}
         headlineText="My Porkydot Account"
         supportingText="2,443.33 DOT"
-        onClick={() => setSelected(3)}
+      />
+      <Select.Item
+        value={3}
+        leadingIcon={<Identicon value="16JfrnmcA7ncfANSXnyAzH9LRZ2gPRQhXforwKtdDhY4edpt" size={40} />}
+        headlineText="My Porkydot Account"
+        supportingText="2,443.33 DOT"
       />
     </Select>
+  )
+}
+
+export const Overflow = () => {
+  const [selected, setSelected] = useState<string | undefined>(undefined)
+
+  return (
+    <div css={{ height: '50vh', overflow: 'auto', background: 'white', padding: 40 }}>
+      <div css={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100vh' }}>
+        <Select placeholder="Select account" value={selected} onChange={value => setSelected(value)}>
+          {Array.from({ length: 50 }, (_, index) => (
+            <Select.Item
+              value={index}
+              leadingIcon={<Identicon value="5CcU6DRpocLUWYJHuNLjB4gGyHJrkWuruQD5XFbRYffCfSAP" size={40} />}
+              headlineText="Polkadot.js Import"
+              supportingText="420 DOT"
+            />
+          ))}
+        </Select>
+      </div>
+    </div>
   )
 }
--- src/components/molecules/Select/Select.tsx
+++ src/components/molecules/Select/Select.tsx
@@ -1,26 +1,48 @@
 import { ChevronDown } from '@components/atoms/Icon'
 import Text from '@components/atoms/Text'
 import { useTheme } from '@emotion/react'
-import useOnClickOutside from '@util/useOnClickOutside'
-import { AnimatePresence, motion } from 'framer-motion'
-import React, { useRef } from 'react'
-import { MouseEventHandler, ReactElement, ReactNode, useCallback, useState } from 'react'
+import {
+  autoUpdate,
+  offset,
+  size,
+  useClick,
+  useDismiss,
+  useFloating,
+  useInteractions,
+  useListNavigation,
+  useRole,
+} from '@floating-ui/react-dom-interactions'
+import { motion } from 'framer-motion'
+import React, {
+  ReactElement,
+  ReactNode,
+  forwardRef,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react'
+
+type Value = string | number | undefined
 
 export type SelectProps = {
+  value?: Value
+  placeholder?: ReactNode
   width?: string | number
   children: ReactElement<SelectItemProps> | ReactElement<SelectItemProps>[]
+  onChange?: (value: string | undefined) => unknown
 }
 
 type SelectItemProps = {
-  selected?: boolean
+  value?: Value
   leadingIcon: ReactNode
   headlineText: ReactNode
   supportingText: ReactNode
-  onClick?: MouseEventHandler
 }
 
-const SelectItem = (props: SelectItemProps) => (
-  <div css={{ display: 'flex', alignItems: 'center', gap: '1rem' }} onClick={props.onClick}>
+const SelectItem = forwardRef<HTMLDivElement, SelectItemProps>((props, ref) => (
+  <div ref={ref} css={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
     <figure css={{ maxWidth: 40, maxHeight: 40, margin: 0 }}>{props.leadingIcon}</figure>
     <div>
       <div>
@@ -31,82 +53,192 @@ const SelectItem = (props: SelectItemProps) => (
       </div>
     </div>
   </div>
-)
+))
+
+// slight overlap for better border radius animation
+const OVERLAP = 6
 
 const Select = Object.assign(
-  ({ width = '30rem', children }: SelectProps) => {
+  ({ width = '30rem', children, ...props }: SelectProps) => {
     const theme = useTheme()
-    const containerRef = useRef<HTMLDivElement>(null)
+    const listRef = useRef<HTMLLIElement[]>([])
     const [open, setOpen] = useState(false)
+    const [pointer, setPointer] = useState(false)
+    const [activeIndex, setActiveIndex] = useState<number | null>(null)
 
-    const selectedChild = React.Children.toArray(children)
-      .filter((x): x is ReactElement<SelectItemProps> => x as any)
-      .find(x => x.props.selected)
+    const childrenArray = React.Children.toArray(children)
 
-    useOnClickOutside(containerRef, () => setOpen(false))
+    const selectedIndex =
+      props.value === undefined
+        ? undefined
+        : childrenArray
+            .filter((x): x is ReactElement<SelectItemProps> => x as any)
+            .findIndex(x => x.props.value !== undefined && x.props.value.toString() === props.value?.toString())
+
+    const selectedChild = selectedIndex === undefined ? undefined : childrenArray[selectedIndex]
+
+    const { context, x, y, reference, floating, strategy } = useFloating({
+      open,
+      onOpenChange: setOpen,
+      whileElementsMounted: autoUpdate,
+      middleware: [
+        // TODO: right now only work for bottom overflow
+        // which is what we need. Implement support for top overflow later
+        size({
+          apply: ({ rects, availableHeight, elements }) => {
+            Object.assign(elements.floating.style, {
+              width: `${rects.reference.width}px`,
+              maxHeight: `${availableHeight}px`,
+            })
+          },
+        }),
+        offset(-OVERLAP),
+      ],
+    })
+
+    const { getReferenceProps, getFloatingProps, getItemProps } = useInteractions([
+      useRole(context, { role: 'listbox' }),
+      useClick(context),
+      useListNavigation(context, {
+        // TODO: this caused element to jump a little
+        // so disabling for now, need to investigate further
+        enabled: false,
+        listRef,
+        activeIndex,
+        selectedIndex,
+        onNavigate: setActiveIndex,
+        loop: true,
+      }),
+      useDismiss(context),
+    ])
+
+    const select = useCallback(
+      (value: Value) => {
+        setOpen(false)
+        setActiveIndex(null)
+        props.onChange?.(value?.toString())
+      },
+      [props]
+    )
+
+    useEffect(() => {
+      if (!open && pointer) {
+        setPointer(false)
+      }
+    }, [open, pointer])
+
+    useLayoutEffect(() => {
+      if (open && activeIndex !== null && !pointer) {
+        requestAnimationFrame(() => {
+          listRef.current[activeIndex]?.scrollIntoView({
+            block: 'nearest',
+          })
+        })
+      }
+    }, [open, activeIndex, pointer])
 
     return (
-      <div
-        ref={containerRef}
-        css={[
-          {
+      <motion.div initial={String(false)} animate={String(open)}>
+        <motion.button
+          ref={reference}
+          variants={{
+            true: { transitionEnd: { borderBottomLeftRadius: 0, borderBottomRightRadius: 0 } },
+            false: { transitionEnd: { borderBottomLeftRadius: '0.8rem', borderBottomRightRadius: '0.8rem' } },
+          }}
+          css={{
             position: 'relative',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'space-between',
             gap: '2rem',
+            textAlign: 'start',
             backgroundColor: theme.color.foreground,
             padding: '0.75rem 1.25rem',
+            border: 'none',
             borderRadius: '0.8rem',
             cursor: 'pointer',
             width: width,
-          },
-          open && { borderBottomLeftRadius: 0, borderBottomRightRadius: 0 },
-        ]}
-        onClick={useCallback(() => setOpen(x => !x), [])}
-      >
-        <div css={{ pointerEvents: 'none', userSelect: 'none' }}>{selectedChild ?? <Text>Select account</Text>}</div>
-        <ChevronDown css={{ transform: open ? 'rotate(180deg)' : undefined, transition: 'ease 0.25s' }} />
-        <AnimatePresence>
-          {open && (
-            <motion.ul
-              initial={{ height: 0 }}
-              animate={{ height: 'unset' }}
-              exit={{ height: 0 }}
-              css={{
-                width,
-                'position': 'absolute',
-                'zIndex': 1,
-                // slight overlap for better border radius animation
-                'top': '95%',
-                'left': 0,
-                'margin': 0,
-                'padding': 0,
-                'borderBottomLeftRadius': '0.5rem',
-                'borderBottomRightRadius': '0.5rem',
-                'backgroundColor': theme.color.foreground,
-                'listStyle': 'none',
-                'overflow': 'hidden',
-                'li > *': {
-                  'margin': '0.5rem 0',
-                  'padding': '1rem 1.25rem',
-                  'backgroundColor': theme.color.foreground,
-                  ':hover': {
-                    filter: 'brightness(1.2)',
-                  },
-                  ':last-child': {
-                    margin: '0.5rem 0 0 0',
-                  },
-                },
+          }}
+          {...getReferenceProps()}
+        >
+          <Text.Body as="div" css={{ pointerEvents: 'none', userSelect: 'none' }}>
+            {selectedChild ?? props.placeholder}
+          </Text.Body>
+          <ChevronDown css={{ transform: open ? 'rotate(180deg)' : undefined, transition: 'ease 0.25s' }} />
+        </motion.button>
+        <motion.ul
+          ref={floating}
+          variants={{
+            true: { height: 'unset', transitionEnd: { overflow: 'auto' } },
+            false: { height: 0, overflow: 'hidden', transitionEnd: { display: 'none' } },
+          }}
+          css={{
+            margin: 0,
+            padding: 0,
+            paddingTop: OVERLAP,
+            borderBottomLeftRadius: '0.5rem',
+            borderBottomRightRadius: '0.5rem',
+            backgroundColor: theme.color.foreground,
+            listStyle: 'none',
+            li: {
+              'padding': '1.5rem 1.25rem',
+              'backgroundColor': theme.color.foreground,
+              ':hover': {
+                filter: 'brightness(1.2)',
+              },
+              ':focus-visible': {
+                filter: 'brightness(1.2)',
+              },
+              ':last-child': {
+                padding: '1.5rem 1.25rem 1rem 1.25rem',
+              },
+            },
+          }}
+          {...getFloatingProps({
+            style: {
+              position: strategy,
+              top: y ?? 0,
+              left: x ?? 0,
+              width: 'max-content',
+              zIndex: 1,
+            },
+            onPointerMove: () => {
+              setPointer(true)
+            },
+            onKeyDown: event => {
+              setPointer(false)
+
+              if (event.key === 'Tab') {
+                setOpen(false)
+              }
+            },
+          })}
+        >
+          {React.Children.map(children, (child, index) => (
+            <li
+              key={child.key}
+              role="option"
+              ref={node => {
+                listRef.current[index] = node!
               }}
+              tabIndex={!open ? -1 : index === activeIndex ? 0 : 1}
+              aria-selected={index === activeIndex}
+              css={{ cursor: 'pointer' }}
+              {...getItemProps({
+                onClick: () => select(child.props.value),
+                onKeyDown: event => {
+                  if (event.key === 'Enter') {
+                    event.preventDefault()
+                    select(child.props.value)
+                  }
+                },
+              })}
             >
-              {React.Children.map(children, child => (
-                <li key={child.key}>{child}</li>
-              ))}
-            </motion.ul>
-          )}
-        </AnimatePresence>
-      </div>
+              {child}
+            </li>
+          ))}
+        </motion.ul>
+      </motion.div>
     )
   },
   { Item: SelectItem }
