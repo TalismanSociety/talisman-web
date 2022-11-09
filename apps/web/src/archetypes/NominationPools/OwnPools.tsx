f051f54dc2f70c6243dea107d581406c35af5f92
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2022-11-09 13:01:48 +1300

    chore: remove nomination pools countdown

--- apps/web/src/archetypes/NominationPools/OwnPools.tsx
+++ apps/web/src/archetypes/NominationPools/OwnPools.tsx
@@ -1,67 +1,9 @@
-import Button from '@components/atoms/Button'
 import Text from '@components/atoms/Text'
-import HiddenDetails from '@components/molecules/HiddenDetails'
 import PoolStake, { PoolStakeList } from '@components/recipes/PoolStake/PoolStake'
-import { chainIdState } from '@domains/chains/recoils'
-import { useCountDownToNomsPool } from '@domains/nominationPools/hooks'
 import { Suspense } from 'react'
-import { Link } from 'react-router-dom'
-import { useRecoilValue } from 'recoil'
 
 import Stakings from './Stakings'
 import Unstakings from './Unstakings'
-
-const OwnPoolsWithSneekPeek = () => {
-  // TODO: remove the countdown stuff
-  const currentChainId = useRecoilValue(chainIdState)
-  const nomsPoolCountdown = useCountDownToNomsPool()
-
-  if (
-    currentChainId === 'polkadot' &&
-    nomsPoolCountdown !== undefined &&
-    nomsPoolCountdown.blocksRemaining !== undefined
-  ) {
-    return (
-      <div>
-        <header>
-          <Text.H4 css={{ marginBottom: '2.4rem' }}>Staking</Text.H4>
-        </header>
-        <HiddenDetails
-          overlay={
-            <div
-              css={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                gap: '3.2rem',
-              }}
-            >
-              <Text.Body>This door will open soon...</Text.Body>
-              <Button as={Link} variant="outlined" to="/staking">
-                Sneak peek
-              </Button>
-            </div>
-          }
-          hidden
-        >
-          <PoolStakeList>
-            <PoolStake.Skeleton animate={false} />
-            <PoolStake.Skeleton animate={false} />
-            <PoolStake.Skeleton animate={false} />
-          </PoolStakeList>
-        </HiddenDetails>
-      </div>
-    )
-  }
-
-  return (
-    <>
-      <Stakings />
-      <Unstakings />
-    </>
-  )
-}
 
 const OwnPools = () => {
   return (
@@ -80,7 +22,8 @@ const OwnPools = () => {
           </div>
         }
       >
-        <OwnPoolsWithSneekPeek />
+        <Stakings />
+        <Unstakings />
       </Suspense>
     </div>
   )
--- apps/web/src/domains/nominationPools/hooks/index.ts
+++ apps/web/src/domains/nominationPools/hooks/index.ts
@@ -1,4 +1,3 @@
-export * from './useCountDownToNomsPool'
 export * from './useInflation'
 export * from './usePoolAddForm'
 export * from './usePoolUnstakeForm'
