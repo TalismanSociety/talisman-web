cb8739af8b416e9d6ce639b8ad8b3380603a3495
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2022-11-16 15:08:23 +1300

    fix: broken storybook

--- apps/web/.storybook/talismanTheme.js
+++ apps/web/.storybook/talismanTheme.js
@@ -1,11 +1,12 @@
 import { create } from '@storybook/theming'
 
+import brandImage from './brandImage.png'
+
 const theme = create({
   base: 'dark',
   brandTitle: 'Talisman',
   brandUrl: 'https://talisman.xyz/',
-  brandImage:
-    'https://uc31ebed52109ce0410b1a063cbe.previews.dropboxusercontent.com/p/thumb/ABqheVDZ48O5et-2zF1JMORZhCQ9IqWLgPCCO5Ego9PFIA61aZO9kz0w9S6OnLqkqOk6MPUD8vinEK8bM2bIwmwJnghxeu6Ib4RiKnrtz0WNg7qLOCGF9tbMfPj3m776KIB4oHppDX9RmPhjBS9M23F6YGBHXibtF2Mlmk43DsetwD3b0RpkrFiVzQm_6S0ElH13OD9bWuJvUWesWCeASLfDKr70tKMkHmqlriYzjx7CJ39kNaFqHtLo-wRh0LULmgOwez1d_aQonzJsQRZYsUQP-OiLa8Z4gZa_0BhUfdJ9w2AsE3OtJ9oQeWUIOFucpCt9Gadmyurl3MoeUOKROUk_Y9ec-BI18BCeSs5JJ8PBGy4M1BiK-lMQKf_WmZA8a8w/p.png',
+  brandImage: brandImage,
 })
 
 export default theme
