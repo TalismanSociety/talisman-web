2be41ba92902d9ba79a26780a16bf87eadcb9979
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2022-11-23 17:16:28 +0700

    style: disable body scroll when dialog is open

--- apps/web/src/components/molecules/AlertDialog/AlertDialog.tsx
+++ apps/web/src/components/molecules/AlertDialog/AlertDialog.tsx
@@ -2,7 +2,7 @@ import Button from '@components/atoms/Button'
 import Dialog, { DialogProps } from '@components/atoms/Dialog'
 import { X } from '@components/atoms/Icon'
 import Text from '@components/atoms/Text'
-import { keyframes, useTheme } from '@emotion/react'
+import { Global, keyframes, useTheme } from '@emotion/react'
 import { ReactNode } from 'react'
 
 export type AlertDialogProps = DialogProps & {
@@ -35,51 +35,54 @@ const AlertDialog = (props: AlertDialogProps) => {
   const theme = useTheme()
 
   return (
-    <Dialog
-      {...props}
-      title={undefined}
-      onClickBackdrop={props.onRequestDismiss}
-      onClose={props.onRequestDismiss}
-      onCancel={props.onRequestDismiss}
-      css={{
-        'maxWidth': '46rem',
-        'padding': '2.4rem',
-        'background': theme.color.surface,
-        'border': 'none',
-        'borderRadius': '1.6rem',
-        '&[open]': {
-          'animation': `${show} .5s ease`,
-          '::backdrop': {
-            background: 'rgba(0,0,0,0.6)',
-            backdropFilter: 'blur(16px)',
-            animation: `${backdropKeyframes} .5s ease forwards`,
-          },
-        },
-      }}
-    >
-      <header
-        css={{ display: 'flex', justifyContent: 'space-between', alignItems: 'baseline', marginBottom: '2.6rem' }}
-      >
-        <Text.H4 css={{ marginBottom: 0 }}>{props.title}</Text.H4>
-        <Button variant="noop" onClick={props.onRequestDismiss}>
-          <X width="1.6rem" height="1.6rem" />
-        </Button>
-      </header>
-      {props.content}
-      <div
+    <>
+      {props.open && <Global styles={{ body: { overflow: 'hidden' } }} />}
+      <Dialog
+        {...props}
+        title={undefined}
+        onClickBackdrop={props.onRequestDismiss}
+        onClose={props.onRequestDismiss}
+        onCancel={props.onRequestDismiss}
         css={{
-          'display': 'flex',
-          'gap': '1.6rem',
-          'marginTop': '4.6rem',
-          '> *': {
-            flex: 1,
+          'maxWidth': '46rem',
+          'padding': '2.4rem',
+          'background': theme.color.surface,
+          'border': 'none',
+          'borderRadius': '1.6rem',
+          '&[open]': {
+            'animation': `${show} .5s ease`,
+            '::backdrop': {
+              background: 'rgba(0,0,0,0.6)',
+              backdropFilter: 'blur(16px)',
+              animation: `${backdropKeyframes} .5s ease forwards`,
+            },
           },
         }}
       >
-        {props.dismissButton}
-        {props.confirmButton}
-      </div>
-    </Dialog>
+        <header
+          css={{ display: 'flex', justifyContent: 'space-between', alignItems: 'baseline', marginBottom: '2.6rem' }}
+        >
+          <Text.H4 css={{ marginBottom: 0 }}>{props.title}</Text.H4>
+          <Button variant="noop" onClick={props.onRequestDismiss}>
+            <X width="1.6rem" height="1.6rem" />
+          </Button>
+        </header>
+        {props.content}
+        <div
+          css={{
+            'display': 'flex',
+            'gap': '1.6rem',
+            'marginTop': '4.6rem',
+            '> *': {
+              flex: 1,
+            },
+          }}
+        >
+          {props.dismissButton}
+          {props.confirmButton}
+        </div>
+      </Dialog>
+    </>
   )
 }
 
