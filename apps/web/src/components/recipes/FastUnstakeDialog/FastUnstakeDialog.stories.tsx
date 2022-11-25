e9705ef42ec391a65232579798ece5b035d100c8
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2022-11-25 13:54:22 +0700

    feat: fast unstake dialog component

---
+++ apps/web/src/components/recipes/FastUnstakeDialog/FastUnstakeDialog.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentMeta, Story } from '@storybook/react'
+
+import FastUnstakeDialog, { FastUnstakeDialogProps } from './FastUnstakeDialog'
+
+export default {
+  title: 'Recipes/FastUnstakeDialog',
+  component: FastUnstakeDialog,
+} as ComponentMeta<typeof FastUnstakeDialog>
+
+export const Default: Story<FastUnstakeDialogProps> = args => <FastUnstakeDialog {...args} />
+
+Default.args = {
+  open: true,
+  fastUnstakeEligibility: 'pending',
+}
+
+export const Eligible = Default.bind({})
+
+Eligible.args = {
+  open: true,
+  fastUnstakeEligibility: 'eligible',
+}
+
+export const Ineligible = Default.bind({})
+
+Ineligible.args = {
+  open: true,
+  fastUnstakeEligibility: 'ineligible',
+}
