6e32d2fbc30a09c14ba63a82b09d30b087cc9cf2
Jonathan Dunne <37194960+jonathanpdunne@users.noreply.github.com>
2022-12-19 14:27:47 +1000

    fix: z-index on cookie banner

--- apps/web/src/archetypes/CookieBanner.tsx
+++ apps/web/src/archetypes/CookieBanner.tsx
@@ -6,7 +6,7 @@ const CookieBanner = () => {
   const [visible, setVisible] = useState(!posthog.has_opted_in_capturing() && !posthog.has_opted_out_capturing())
 
   return (
-    <div css={{ position: 'fixed', right: '1.5rem', bottom: '2.4rem' }}>
+    <div css={{ position: 'fixed', right: '1.5rem', bottom: '2.4rem', zIndex: '2' }}>
       <BaseCookieBanner
         privacyPolicyHref="https://docs.talisman.xyz/talisman/prepare-for-your-journey/privacy-policy"
         visible={visible}
