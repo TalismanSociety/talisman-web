5d7cf57c0b6e10698da71c70628dafe024c5e400
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-02-09 09:21:49 +0700

    feat: sentry error reporting

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,10 +1,17 @@
 import './libs/i18n/i18n'
 import './index.css'
 
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App'
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+})
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
