7dfb707bed368d3f8d6545ea2a9e610ec905c66c
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-02-09 14:14:53 +0700

    build: remove unused env vars & config keys (#489)

--- apps/web/src/react-app-env.d.ts
+++ apps/web/src/react-app-env.d.ts
@@ -14,8 +14,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     REACT_APP_APPLICATION_NAME: string
     REACT_APP_POSTHOG_AUTH_TOKEN: string
-    REACT_APP_RAMP_API_KEY: string
-    REACT_APP_RAMP_URL: string
     REACT_APP_TX_HISTORY_INDEXER: string
   }
 }
