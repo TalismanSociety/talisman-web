c8c27fbb8528e617001342bd73b9a6522e840d6d
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-02-25 11:54:43 +0700

    perf: offload staker rewards calculation to web worker

---
+++ apps/web/src/domains/staking/worker.ts
@@ -0,0 +1,28 @@
+import { ApiPromise, WsProvider } from '@polkadot/api'
+import { expose } from 'threads/worker'
+
+export type WorkerFunction = typeof getStakersReward
+
+// Needed to extract to a worker since this calculation is very CPU heavy
+// and will slow down the UI thread
+const getStakersReward = async (endpoints: string | string[], addresses: string[], eras: number[]) => {
+  const api = await ApiPromise.create({ provider: new WsProvider(endpoints) })
+
+  const stakerRewards = await api.derive.staking.stakerRewardsMultiEras(
+    addresses,
+    eras.map(x => api.createType('u32', x))
+  )
+
+  await api.disconnect()
+
+  return Object.fromEntries(
+    stakerRewards.map((x, index) => [
+      addresses[index]!,
+      x
+        .map(y => Object.values(y.validators).reduce((previous, current) => previous + current.value.toBigInt(), 0n))
+        .reduce((previous, current) => previous + current, 0n),
+    ])
+  )
+}
+
+expose(getStakersReward)
