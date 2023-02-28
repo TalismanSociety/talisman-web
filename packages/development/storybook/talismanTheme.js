ef901869cfa23d6ab82da7fd1d8876e3ee556087
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-02-28 11:39:29 +0700

    build: improve storybook code sharing

--- packages/development/storybook/talismanTheme.js
+++ packages/development/storybook/talismanTheme.js
@@ -2,11 +2,9 @@ import { create } from '@storybook/theming'
 
 import brandImage from './brandImage.png'
 
-const theme = create({
+export const theme = create({
   base: 'dark',
   brandTitle: 'Talisman',
   brandUrl: 'https://talisman.xyz/',
   brandImage: brandImage,
 })
-
-export default theme
