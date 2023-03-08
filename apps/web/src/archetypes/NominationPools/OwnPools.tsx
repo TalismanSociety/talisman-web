12913bed0f89f7f4c03f1f78b76bfa0efae61e2c
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-03-09 00:13:38 +0700

    style: adjust stake item skeleton

--- apps/web/src/archetypes/NominationPools/OwnPools.tsx
+++ apps/web/src/archetypes/NominationPools/OwnPools.tsx
@@ -1,4 +1,4 @@
-import PoolStake, { PoolStakeList } from '@components/recipes/PoolStake/PoolStake'
+import StakeItem from '@components/recipes/StakeItem'
 import { useTotalStaked } from '@domains/staking/hooks'
 import { useTheme } from '@emotion/react'
 import { Text } from '@talismn/ui'
@@ -28,11 +28,11 @@ const OwnPools = () => (
           <header>
             <Text.H4 css={{ marginBottom: '2.4rem' }}>Staking</Text.H4>
           </header>
-          <PoolStakeList>
-            <PoolStake.Skeleton />
-            <PoolStake.Skeleton />
-            <PoolStake.Skeleton />
-          </PoolStakeList>
+          <section css={{ display: 'flex', flexDirection: 'column', gap: '1.6rem' }}>
+            <StakeItem.Skeleton />
+            <StakeItem.Skeleton />
+            <StakeItem.Skeleton />
+          </section>
         </div>
       }
     >
