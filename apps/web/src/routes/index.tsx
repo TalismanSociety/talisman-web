12dc5b85745637f18f98654e1d2c071cc67829d6
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-03-13 11:02:45 +0700

    refactor: layout

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -79,179 +79,179 @@ const Main = () => {
   const [drawerOpen, setDrawerOpen] = useState(false)
 
   return (
-    // TODO: remove legacy imperative modals
-    <ModalProvider>
-      <MoonbeamContributors.PopupProvider>
-        <Scaffold
-          breakpoints={{
-            topBar: 'narrow',
-            bottomBar: 'narrow',
-            sideBar: 'wide',
-            drawer: 'narrow',
-            footer: 'wide',
-          }}
-          topBar={
-            <TopAppBar
-              navigationIcon={
-                <IconButton as={Link} to="/">
-                  <TalismanHand />
-                </IconButton>
-              }
-              actions={
-                <TopAppBar.Actions>
-                  <AccountsManagementMenu button={<AccountsManagementMenu.IconButton />} />
-                  <IconButton onClick={useCallback(() => setDrawerOpen(true), [])}>
-                    <MoreHorizontal />
-                  </IconButton>
-                </TopAppBar.Actions>
-              }
-            />
+    <Scaffold
+      breakpoints={{
+        topBar: 'narrow',
+        bottomBar: 'narrow',
+        sideBar: 'wide',
+        drawer: 'narrow',
+        footer: 'wide',
+      }}
+      topBar={
+        <TopAppBar
+          navigationIcon={
+            <IconButton as={Link} to="/">
+              <TalismanHand />
+            </IconButton>
           }
-          bottomBar={
-            <NavigationBar>
-              <Link to="/portfolio">
-                <NavigationBar.Item label="Portfolio" icon={<Eye />} />
-              </Link>
-              <Link to="/portfolio?action=stake">
-                <NavigationBar.Item label="Staking" icon={<Zap />} />
-              </Link>
-              <Link to="/explore">
-                <NavigationBar.Item label="Explore" icon={<Compass />} />
-              </Link>
-              <Link to="/crowdloans">
-                <NavigationBar.Item label="Crowdloans" icon={<Star />} />
-              </Link>
-              <Link to="https://talisman.banxa.com/" target="_blank">
-                <NavigationBar.Item label="Buy" icon={<CreditCard />} />
-              </Link>
-            </NavigationBar>
+          actions={
+            <TopAppBar.Actions>
+              <AccountsManagementMenu button={<AccountsManagementMenu.IconButton />} />
+              <IconButton onClick={useCallback(() => setDrawerOpen(true), [])}>
+                <MoreHorizontal />
+              </IconButton>
+            </TopAppBar.Actions>
           }
-          sideBar={
-            <NavigationRail
-              header={
-                <IconButton as={Link} to="/">
-                  <TalismanHand />
-                </IconButton>
-              }
-            >
-              <Link to="/portfolio">
-                <NavigationRail.Item label="Portfolio" icon={<Eye />} />
-              </Link>
-              <Link to="/portfolio?action=stake">
-                <NavigationRail.Item label="Staking" icon={<Zap />} />
-              </Link>
-              <Link to="/explore">
-                <NavigationRail.Item label="Explore" icon={<Compass />} />
-              </Link>
-              <Link to="/crowdloans">
-                <NavigationRail.Item label="Crowdloans" icon={<Star />} />
-              </Link>
-              <Link to="https://talisman.banxa.com/" target="_blank">
-                <NavigationRail.Item label="Buy" icon={<CreditCard />} />
-              </Link>
-            </NavigationRail>
-          }
-          drawer={
-            <NavigationDrawer
-              open={drawerOpen}
-              onRequestDismiss={useCallback(() => setDrawerOpen(false), [])}
-              headerIcon={<TalismanHand />}
-              footer={
-                <NavigationDrawer.Footer>
-                  <Link to="https://discord.gg/talisman" target="_blank">
-                    <NavigationDrawer.Footer.Icon>
-                      <Discord width="2.4rem" height="2.4rem" />
-                    </NavigationDrawer.Footer.Icon>
-                  </Link>
-                  <Link to="https://github.com/TalismanSociety/talisman-web" target="_blank">
-                    <NavigationDrawer.Footer.Icon>
-                      <GitHub width="2.4rem" height="2.4rem" />
-                    </NavigationDrawer.Footer.Icon>
-                  </Link>
-                  <Link to="https://twitter.com/wearetalisman" target="_blank">
-                    <NavigationDrawer.Footer.Icon>
-                      <Twitter width="2.4rem" height="2.4rem" />
-                    </NavigationDrawer.Footer.Icon>
-                  </Link>
-                  <Link to="https://medium.com/we-are-talisman" target="_blank">
-                    <NavigationDrawer.Footer.Icon>
-                      <Medium width="2.4rem" height="2.4rem" />
-                    </NavigationDrawer.Footer.Icon>
-                  </Link>
-                  <NavigationDrawer.Footer.A
-                    href="https://docs.talisman.xyz/talisman/legal-and-security/terms-of-use"
-                    target="_blank"
-                  >
-                    Terms
-                  </NavigationDrawer.Footer.A>
-                  <NavigationDrawer.Footer.A
-                    href="https://docs.talisman.xyz/talisman/legal-and-security/privacy-policy"
-                    target="_blank"
-                  >
-                    Privacy
-                  </NavigationDrawer.Footer.A>
-                </NavigationDrawer.Footer>
-              }
-            >
-              <Link to="/portfolio">
-                <NavigationDrawer.Item label="Portfolio" icon={<Eye />} />
-              </Link>
-              <Link to="/staking">
-                <NavigationDrawer.Item label="Staking" icon={<Zap />} />
-              </Link>
-              <Link to="/explore">
-                <NavigationDrawer.Item label="Explore" icon={<Compass />} />
-              </Link>
-              <Link to="https://talisman.banxa.com/" target="_blank">
-                <NavigationDrawer.Item label="Buy crypto" icon={<CreditCard />} />
-              </Link>
-            </NavigationDrawer>
-          }
-          footer={
-            <div css={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0 2.4rem' }}>
-              <Text.BodyLarge>Talisman Portal</Text.BodyLarge>
-              <ul
-                css={theme => ({
-                  display: 'flex',
-                  gap: '3.2rem',
-                  a: { 'opacity': theme.contentAlpha.medium, ':hover': { opacity: theme.contentAlpha.high } },
-                })}
-              >
-                <Text.BodyLarge alpha="high" as="a" href="https://twitter.com/wearetalisman" target="_blank">
-                  Twitter
-                </Text.BodyLarge>
-                <Text.BodyLarge alpha="high" as="a" href="https://discord.gg/talisman" target="_blank">
-                  Discord
-                </Text.BodyLarge>
-                <Text.BodyLarge alpha="high" as="a" href="https://docs.talisman.xyz" target="_blank">
-                  Docs
-                </Text.BodyLarge>
-                <Text.BodyLarge
-                  alpha="high"
-                  as="a"
-                  href="https://docs.talisman.xyz/talisman/legal-and-security/terms-of-use"
-                  target="_blank"
-                >
-                  Terms
-                </Text.BodyLarge>
-                <Text.BodyLarge
-                  alpha="high"
-                  as="a"
-                  href="https://docs.talisman.xyz/talisman/legal-and-security/privacy-policy"
-                  target="_blank"
-                >
-                  Privacy
-                </Text.BodyLarge>
-              </ul>
-            </div>
+        />
+      }
+      bottomBar={
+        <NavigationBar>
+          <Link to="/portfolio">
+            <NavigationBar.Item label="Portfolio" icon={<Eye />} />
+          </Link>
+          <Link to="/portfolio?action=stake">
+            <NavigationBar.Item label="Staking" icon={<Zap />} />
+          </Link>
+          <Link to="/explore">
+            <NavigationBar.Item label="Explore" icon={<Compass />} />
+          </Link>
+          <Link to="/crowdloans">
+            <NavigationBar.Item label="Crowdloans" icon={<Star />} />
+          </Link>
+          <Link to="https://talisman.banxa.com/" target="_blank">
+            <NavigationBar.Item label="Buy" icon={<CreditCard />} />
+          </Link>
+        </NavigationBar>
+      }
+      sideBar={
+        <NavigationRail
+          header={
+            <IconButton as={Link} to="/">
+              <TalismanHand />
+            </IconButton>
           }
         >
+          <Link to="/portfolio">
+            <NavigationRail.Item label="Portfolio" icon={<Eye />} />
+          </Link>
+          <Link to="/portfolio?action=stake">
+            <NavigationRail.Item label="Staking" icon={<Zap />} />
+          </Link>
+          <Link to="/explore">
+            <NavigationRail.Item label="Explore" icon={<Compass />} />
+          </Link>
+          <Link to="/crowdloans">
+            <NavigationRail.Item label="Crowdloans" icon={<Star />} />
+          </Link>
+          <Link to="https://talisman.banxa.com/" target="_blank">
+            <NavigationRail.Item label="Buy" icon={<CreditCard />} />
+          </Link>
+        </NavigationRail>
+      }
+      drawer={
+        <NavigationDrawer
+          open={drawerOpen}
+          onRequestDismiss={useCallback(() => setDrawerOpen(false), [])}
+          headerIcon={<TalismanHand />}
+          footer={
+            <NavigationDrawer.Footer>
+              <Link to="https://discord.gg/talisman" target="_blank">
+                <NavigationDrawer.Footer.Icon>
+                  <Discord width="2.4rem" height="2.4rem" />
+                </NavigationDrawer.Footer.Icon>
+              </Link>
+              <Link to="https://github.com/TalismanSociety/talisman-web" target="_blank">
+                <NavigationDrawer.Footer.Icon>
+                  <GitHub width="2.4rem" height="2.4rem" />
+                </NavigationDrawer.Footer.Icon>
+              </Link>
+              <Link to="https://twitter.com/wearetalisman" target="_blank">
+                <NavigationDrawer.Footer.Icon>
+                  <Twitter width="2.4rem" height="2.4rem" />
+                </NavigationDrawer.Footer.Icon>
+              </Link>
+              <Link to="https://medium.com/we-are-talisman" target="_blank">
+                <NavigationDrawer.Footer.Icon>
+                  <Medium width="2.4rem" height="2.4rem" />
+                </NavigationDrawer.Footer.Icon>
+              </Link>
+              <NavigationDrawer.Footer.A
+                href="https://docs.talisman.xyz/talisman/legal-and-security/terms-of-use"
+                target="_blank"
+              >
+                Terms
+              </NavigationDrawer.Footer.A>
+              <NavigationDrawer.Footer.A
+                href="https://docs.talisman.xyz/talisman/legal-and-security/privacy-policy"
+                target="_blank"
+              >
+                Privacy
+              </NavigationDrawer.Footer.A>
+            </NavigationDrawer.Footer>
+          }
+        >
+          <Link to="/portfolio">
+            <NavigationDrawer.Item label="Portfolio" icon={<Eye />} />
+          </Link>
+          <Link to="/staking">
+            <NavigationDrawer.Item label="Staking" icon={<Zap />} />
+          </Link>
+          <Link to="/explore">
+            <NavigationDrawer.Item label="Explore" icon={<Compass />} />
+          </Link>
+          <Link to="https://talisman.banxa.com/" target="_blank">
+            <NavigationDrawer.Item label="Buy crypto" icon={<CreditCard />} />
+          </Link>
+        </NavigationDrawer>
+      }
+      footer={
+        <div css={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0 2.4rem' }}>
+          <Text.BodyLarge>Talisman Portal</Text.BodyLarge>
+          <ul
+            css={theme => ({
+              display: 'flex',
+              gap: '3.2rem',
+              a: { 'opacity': theme.contentAlpha.medium, ':hover': { opacity: theme.contentAlpha.high } },
+            })}
+          >
+            <Text.BodyLarge alpha="high" as="a" href="https://twitter.com/wearetalisman" target="_blank">
+              Twitter
+            </Text.BodyLarge>
+            <Text.BodyLarge alpha="high" as="a" href="https://discord.gg/talisman" target="_blank">
+              Discord
+            </Text.BodyLarge>
+            <Text.BodyLarge alpha="high" as="a" href="https://docs.talisman.xyz" target="_blank">
+              Docs
+            </Text.BodyLarge>
+            <Text.BodyLarge
+              alpha="high"
+              as="a"
+              href="https://docs.talisman.xyz/talisman/legal-and-security/terms-of-use"
+              target="_blank"
+            >
+              Terms
+            </Text.BodyLarge>
+            <Text.BodyLarge
+              alpha="high"
+              as="a"
+              href="https://docs.talisman.xyz/talisman/legal-and-security/privacy-policy"
+              target="_blank"
+            >
+              Privacy
+            </Text.BodyLarge>
+          </ul>
+        </div>
+      }
+    >
+      {/* TODO: remove legacy imperative modals */}
+      <ModalProvider>
+        <MoonbeamContributors.PopupProvider>
           <Header />
           <Outlet />
-        </Scaffold>
-        <StakeDialog />
-      </MoonbeamContributors.PopupProvider>
-    </ModalProvider>
+          <StakeDialog />
+        </MoonbeamContributors.PopupProvider>
+      </ModalProvider>
+    </Scaffold>
   )
 }
 
