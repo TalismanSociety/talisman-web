fb2fc1ca0ad349d30dbbad9f142c36c4af005aee
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-03-17 19:04:40 +0700

    chore: remove cookie banner for non-gdpr countries

--- apps/web/src/archetypes/CookieBanner.tsx
+++ apps/web/src/archetypes/CookieBanner.tsx
@@ -1,9 +1,56 @@
 import BaseCookieBanner from '@components/recipes/CookieBanner'
 import posthog from 'posthog-js'
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+
+const GDPR_TIMEZONES = [
+  'Europe/Vienna',
+  'Europe/Brussels',
+  'Europe/Sofia',
+  'Europe/Zagreb',
+  'Asia/Famagusta',
+  'Asia/Nicosia',
+  'Europe/Prague',
+  'Europe/Copenhagen',
+  'Europe/Tallinn',
+  'Europe/Helsinki',
+  'Europe/Paris',
+  'Europe/Berlin',
+  'Europe/Busingen',
+  'Europe/Athens',
+  'Europe/Budapest',
+  'Europe/Dublin',
+  'Europe/Rome',
+  'Europe/Riga',
+  'Europe/Vilnius',
+  'Europe/Luxembourg',
+  'Europe/Malta',
+  'Europe/Amsterdam',
+  'Europe/Warsaw',
+  'Atlantic/Azores',
+  'Atlantic/Madeira',
+  'Europe/Lisbon',
+  'Europe/Bucharest',
+  'Europe/Bratislava',
+  'Europe/Ljubljana',
+  'Africa/Ceuta',
+  'Atlantic/Canary',
+  'Europe/Madrid',
+  'Europe/Stockholm',
+]
+
+const isGdprComplianceRequired = GDPR_TIMEZONES.includes(Intl.DateTimeFormat().resolvedOptions().timeZone)
 
 const CookieBanner = () => {
-  const [visible, setVisible] = useState(!posthog.has_opted_in_capturing() && !posthog.has_opted_out_capturing())
+  const [visible, setVisible] = useState(
+    isGdprComplianceRequired && !posthog.has_opted_in_capturing() && !posthog.has_opted_out_capturing()
+  )
+
+  // When user is no longer in a GDPR compliance country
+  useEffect(() => {
+    if (!isGdprComplianceRequired) {
+      posthog.clear_opt_in_out_capturing()
+    }
+  }, [])
 
   return (
     <div css={{ position: 'fixed', right: '1.5rem', bottom: '2.4rem', zIndex: 11 }}>
