64d13f3499137f47233d899a0137473959196fa0
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-03-17 21:14:40 +0700

    fix: watch only account duplicates
    same public key with different ss58 prefix

--- apps/web/src/util/addressValidation.ts
+++ apps/web/src/util/addressValidation.ts
@@ -2,16 +2,14 @@ import { decodeAddress, encodeAddress } from '@polkadot/keyring'
 import { hexToU8a, isHex } from '@polkadot/util'
 import { ethers } from 'ethers'
 
-export const isValidSubstrateOrEthereumAddress = (address: string) => {
+export const tryParseSubstrateOrEthereumAddress = (address: string) => {
   if (ethers.utils.isAddress(address)) {
-    return true
+    return address
   }
 
   try {
-    encodeAddress(isHex(address) ? hexToU8a(address) : decodeAddress(address))
-
-    return true
+    return encodeAddress(isHex(address) ? hexToU8a(address) : decodeAddress(address))
   } catch (error) {
-    return false
+    return undefined
   }
 }
