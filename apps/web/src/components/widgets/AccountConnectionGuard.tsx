f0dd87e7ffa1be574f1bfd358e5323a281b7fae8
Josh <ahimaz.joshua@gmail.com>
2023-03-21 14:41:42 +0800

    fix: bills name and replaced eth1 for vitalik

--- apps/web/src/components/widgets/AccountConnectionGuard.tsx
+++ apps/web/src/components/widgets/AccountConnectionGuard.tsx
@@ -13,8 +13,8 @@ const POPULAR_ACCOUNTS: Array<ReadonlyAccount & { description?: string }> = [
   { name: 'Swader', address: '5CK8D1sKNwF473wbuBP6NuhQfPaWUetNsWUNAAzVwTfxqjfr', description: '200+ NFTs' },
   { name: '🍺 Gav', address: '5F7LiCA6T4DWUDRQyFAWsRqVwxrJEznUtcw4WNnb5fe6snCH', description: 'Polkadot founder' },
   { name: 'Jay', address: '5DfAiCavECjh37Bdgy7q5ib7AtjJmvZDmSkVBoBXPjVWXCST', description: '$1M+ assets' },
-  { name: 'Bill Laboor', address: '5HjZCeVcUVpThHNMyMBMKqN5ajph9CkDmZhn9BK48TmC3K4Y', description: '50+ Crowdloans' },
-  { address: '0x0D0707963952f2fBA59dD06f2b425ace40b492Fe', description: '$767M+ EVM assets' },
+  { name: 'Bill Laboon', address: '5HjZCeVcUVpThHNMyMBMKqN5ajph9CkDmZhn9BK48TmC3K4Y', description: '50+ Crowdloans' },
+  { name: 'Vitalik.eth', address: '0xd8da6bf26964af9d7eed9e03e53415d37aa96045', description: 'Vitalik Buterin' },
   { address: '0x804c4c527f3b278a1b328ebe239359e1c1008398', description: '$13M+ EVM assets' },
 ]
 
