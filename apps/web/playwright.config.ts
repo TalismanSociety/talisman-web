9aa9f0048a8b9f172267ab5c921f03a1c20f7608
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-03-22 14:05:03 +1300

    test: playwright improvements

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -1,7 +1,22 @@
 import type { PlaywrightTestConfig } from '@playwright/test'
 import { devices } from '@playwright/test'
 
-const PORT = 3000
+const PORT = process.env.PORT ?? 3000
+
+const projects = [
+  {
+    name: 'chromium',
+    use: {
+      ...devices['Desktop Chrome'],
+    },
+  },
+  {
+    name: 'firefox',
+    use: {
+      ...devices['Desktop Firefox'],
+    },
+  },
+]
 
 const config: PlaywrightTestConfig = {
   testDir: './e2e',
@@ -9,19 +24,14 @@ const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   reporter: process.env.CI ? 'github' : 'list',
+  timeout: process.env.CI ? 60_000 : undefined,
   use: {
     baseURL: `http://localhost:${PORT}`,
+    trace: 'on-first-retry',
   },
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-      },
-    },
-  ],
+  projects: process.env.CI ? projects.slice(0, 1) : projects,
   webServer: {
-    command: `yarn dev -p ${PORT}`,
+    command: `PORT=${PORT} yarn dev`,
     port: PORT,
     timeout: process.env.CI ? 10 * 60000 : 5 * 60000,
     reuseExistingServer: !process.env.CI,
