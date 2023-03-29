046f5d5b3bf550d7ae56c1d35b39c767b59f1810
Joshua Ahimaz <ahimaz.joshua@gmail.com>
2023-03-29 11:18:57 +0800

    fix: crowdloan images and desc from baserow (#568)
    * fix: crowdloan images and desc from baserow
    
    * fix: removed parachainDetails not being used
    
    * fix: pulled yarn.lock
    
    * fix: yarn.lock
    
    * fix: yarn.lock

--- apps/web/src/archetypes/Wallet/Crowdloans.tsx
+++ apps/web/src/archetypes/Wallet/Crowdloans.tsx
@@ -5,12 +5,13 @@ import { selectedSubstrateAccountsState } from '@domains/accounts/recoils'
 import { tokenPriceState } from '@domains/chains/recoils'
 import { useTotalCrowdloanTotalFiatAmount } from '@domains/crowdloans/hooks'
 import styled from '@emotion/styled'
+import crowdloanDataState from '@libs/@talisman-crowdloans/provider'
 import { CrowdloanContribution, useCrowdloanContributions } from '@libs/crowdloans'
 import { Moonbeam } from '@libs/crowdloans/crowdloanOverrides'
 import { MoonbeamPortfolioTag } from '@libs/moonbeam-contributors'
 import { calculateCrowdloanPortfolioAmounts, useTaggedAmountsInPortfolio } from '@libs/portfolio'
 import { useCrowdloanById, useParachainAssets, useParachainDetailsById } from '@libs/talisman'
-import { SupportedRelaychains, parachainDetails } from '@libs/talisman/util/_config'
+import { SupportedRelaychains } from '@libs/talisman/util/_config'
 import { planckToTokens } from '@talismn/util'
 import { formatCommas, formatCurrency } from '@util/helpers'
 import { Maybe } from '@util/monads'
@@ -28,9 +29,11 @@ const CrowdloanItem = styled(
 
     const asset = useParachainAssets(id)
 
+    const crowdloans = useRecoilValue(crowdloanDataState)
+
     const relayChainId = contribution.parachain.paraId.split('-')[0]
     const relayChain = Maybe.of(relayChainId).mapOrUndefined(x => SupportedRelaychains[x]!)
-    const chain = parachainDetails.find(x => x.id === id)
+    const chain = crowdloans.find(x => x.id === id)
 
     const { tokenSymbol: relayNativeToken, coingeckoId, tokenDecimals: relayTokenDecimals } = relayChain ?? {}
     const { name } = chain ?? {}
