68f29aede43e7e8c1ffa2e1994861c4ddc0ff4b5
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-05-18 22:58:34 +1200

    style: improve teleport skeleton

--- apps/web/src/components/recipes/TeleportForm/TeleportForm.skeleton.tsx
+++ apps/web/src/components/recipes/TeleportForm/TeleportForm.skeleton.tsx
@@ -14,22 +14,16 @@ const TeleportFormSkeleton = (props: SkeletonProps) => {
         justifyContent: 'space-between',
         backgroundColor: theme.color.surface,
         borderRadius: '1.6rem',
-        padding: '3.2rem',
-        minHeight: '33.8rem',
-        width: '47rem',
+        padding: '2.4rem',
+        width: '42.8rem',
         maxWidth: '100%',
       }}
     >
-      <div>
-        <Skeleton.Foreground css={{ height: '5.5rem', marginBottom: '1.6rem' }} />
-        <div css={{ display: 'flex', justifyContent: 'space-between', marginBottom: '0.8rem' }}>
-          <Skeleton.Foreground css={{ width: '9.7rem', height: '1.7rem' }} />
-          <Skeleton.Foreground css={{ width: '9.7rem', height: '1.7rem' }} />
-        </div>
-        <Skeleton.Foreground css={{ height: '6.8rem', marginBottom: '0.8rem' }} />
-        <Skeleton.Foreground css={{ width: '9.7rem', height: '1.7rem' }} />
+      <div css={{ marginBottom: '4.7rem' }}>
+        <Skeleton.Foreground css={{ height: '4rem', marginBottom: '2.4rem' }} />
+        <Skeleton.Foreground css={{ height: '6.8rem' }} />
       </div>
-      <Skeleton.Foreground css={{ height: '4.26334rem' }} />
+      <Skeleton.Foreground css={{ height: '4rem' }} />
     </Skeleton.Surface>
   )
 }
