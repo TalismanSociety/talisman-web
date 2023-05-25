908c987fd65e0b363bb9fd3e9a1c8ca53faf36a0
Liam Aharon <liam.aharon@hotmail.com>
2023-05-25 18:27:11 +0400

    feat(signet): transfer details

--- apps/multisig/src/layouts/Overview/Transactions/TransactionDetailsExpandable.tsx
+++ apps/multisig/src/layouts/Overview/Transactions/TransactionDetailsExpandable.tsx
@@ -29,6 +29,87 @@ const AmountRow = ({ amount, token, price }: { amount: number; token?: Token; pr
   )
 }
 
+const AddressPill = ({ a }: { a: string }) => {
+  return (
+    <a
+      className={css`
+        display: flex;
+        align-items: center;
+        height: 25px;
+        width: 138px;
+        border-radius: 100px;
+        background-color: var(--color-backgroundLighter);
+        padding-left: 8px;
+        font-size: 14px;
+        gap: 4px;
+      `}
+      href={`https://subscan.io/address/${a}`}
+      target="_blank"
+      rel="noreferrer"
+    >
+      <Identicon value={a} size={'16px'} />
+      <span css={{ marginTop: '3px' }}>{truncateMiddle(a, 5, 5, '...')}</span>
+    </a>
+  )
+}
+
+const MultiSendExpendedDetails = ({ t }: { t: Transaction }) => {
+  const theme = useTheme()
+  const recipients = t.decoded.recipients || []
+  return (
+    <div css={{ paddingBottom: '8px' }}>
+      {t.decoded.recipients.map(([addr, amt], i) => {
+        const last = i === recipients.length - 1
+        return (
+          <div
+            css={{
+              display: 'grid',
+              gap: '16px',
+              borderBottom: `${last ? '0px' : '1px'} solid rgb(${theme.backgroundLighter})`,
+              padding: `${last ? '24px 0 0 0' : '24px 0'}`,
+            }}
+          >
+            <div css={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
+              <span css={{ color: 'var(--color-offWhite)' }}>Send</span>
+              <IconButton contentColor={`rgb(${theme.primary})`} size={'20px'}>
+                <Send size={'16px'} />
+              </IconButton>
+              <div
+                className={css`
+                  display: flex;
+                  align-items: center;
+                  height: 25px;
+                  border-radius: 100px;
+                  background-color: var(--color-backgroundLighter);
+                  padding: 8px 14px;
+                  font-size: 14px;
+                  gap: 4px;
+                  margin-left: 8px;
+                `}
+              >
+                <span css={{ marginTop: '3px' }}>
+                  {i + 1} of {recipients.length}
+                </span>
+              </div>
+              <div css={{ marginLeft: 'auto' }}>
+                <AmountRow
+                  amount={amt}
+                  token={t.decoded?.outgoingToken?.token}
+                  price={t.decoded.outgoingToken?.price || 0}
+                />
+              </div>
+            </div>
+            <div css={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+              Destination
+              <AddressPill a={addr} />
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
 const TransactionDetailsExpandable = ({ t }: { t: Transaction }) => {
   const theme = useTheme()
   const [expanded, setExpanded] = useState(false)
@@ -36,10 +117,13 @@ const TransactionDetailsExpandable = ({ t }: { t: Transaction }) => {
   return (
     <div
       className={css`
+        display: grid;
+        align-content: center;
+        width: 100%;
         padding: 8px 24px;
         border-radius: 16px;
         background-color: var(--color-backgroundLight);
-        height: auto;
+        min-height: 56px;
       `}
     >
       <div
@@ -61,7 +145,7 @@ const TransactionDetailsExpandable = ({ t }: { t: Transaction }) => {
           </>
         ) : t.decoded.type === TransactionType.Transfer ? (
           <>
-            <p css={{ marginTop: '4px' }}>Transfer</p>
+            <p css={{ marginTop: '4px' }}>Send</p>
             <Send />
           </>
         ) : t.decoded.type === TransactionType.Other ? (
@@ -70,58 +154,72 @@ const TransactionDetailsExpandable = ({ t }: { t: Transaction }) => {
             <List />
           </>
         ) : null}
-        <div
-          className={css`
-            display: flex;
-            margin-left: auto;
-            align-items: center;
-            gap: 4px;
-            height: 25px;
-            background-color: var(--color-backgroundLighter);
-            color: var(--color-foreground);
-            border-radius: 12px;
-            padding: 5px 8px;
-            margin-right: 16px;
-          `}
-        >
+        {t.decoded.type === TransactionType.MultiSend ? (
           <div
             className={css`
               display: flex;
+              margin-left: auto;
               align-items: center;
-              height: 16px;
-              width: 16px;
-              border-radius: 100px;
-              background-color: var(--color-dim);
-              svg {
-                color: var(--color-primary);
-                height: 8px;
-              }
+              gap: 4px;
+              height: 25px;
+              background-color: var(--color-backgroundLighter);
+              color: var(--color-foreground);
+              border-radius: 12px;
+              padding: 5px 8px;
+              margin-right: 16px;
             `}
           >
-            <Users />
+            <div
+              className={css`
+                display: flex;
+                align-items: center;
+                height: 16px;
+                width: 16px;
+                border-radius: 100px;
+                background-color: var(--color-dim);
+                svg {
+                  color: var(--color-primary);
+                  height: 8px;
+                }
+              `}
+            >
+              <Users />
+            </div>
+            <p css={{ fontSize: '14px', marginTop: '4px' }}>
+              {recipients.length} Recipient{recipients.length !== 1 && 's'}
+            </p>
           </div>
-          <p css={{ fontSize: '14px', marginTop: '4px' }}>
-            {recipients.length} Recipient{recipients.length !== 1 && 's'}
-          </p>
-        </div>
+        ) : t.decoded.type === TransactionType.Transfer ? (
+          <div
+            className={css`
+              margin-left: auto;
+              color: var(--color-foreground);
+              margin-right: 16px;
+            `}
+          >
+            <AddressPill a={(recipients[0] || ['', 0])[0]} />
+          </div>
+        ) : null}
         <AmountRow
           amount={t.decoded.outgoingToken?.amount || 0}
           token={t.decoded.outgoingToken?.token}
           price={t.decoded.outgoingToken?.price || 0}
         />
-        <div css={{ width: '28px' }}>
-          <IconButton
-            contentColor={`rgb(${theme.offWhite})`}
-            onClick={() => {
-              setExpanded(!expanded)
-            }}
-            className={css`
-              ${expanded && 'transform: rotate(90deg);'}
-            `}
-          >
-            <ChevronRight />
-          </IconButton>
-        </div>
+        {t.decoded.type === TransactionType.MultiSend ? (
+          <div css={{ width: '28px' }}>
+            <IconButton
+              contentColor={`rgb(${theme.offWhite})`}
+              onClick={() => {
+                setExpanded(!expanded)
+              }}
+              className={css`
+                ${expanded && 'transform: rotate(90deg);'}
+              `}
+            >
+              <ChevronRight />
+            </IconButton>
+          </div>
+        ) : null}
       </div>
       <div
         className={css`
@@ -131,74 +229,7 @@ const TransactionDetailsExpandable = ({ t }: { t: Transaction }) => {
         `}
       >
         <Collapse isOpened={expanded}>
-          <div css={{ paddingBottom: '8px' }}>
-            {t.decoded.recipients.map(([addr, amt], i) => {
-              const last = i === recipients.length - 1
-              return (
-                <div
-                  css={{
-                    display: 'grid',
-                    gap: '16px',
-                    borderBottom: `${last ? '0px' : '1px'} solid rgb(${theme.backgroundLighter})`,
-                    padding: `${last ? '24px 0 0 0' : '24px 0'}`,
-                  }}
-                >
-                  <div css={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
-                    <span css={{ color: 'var(--color-offWhite)' }}>Send</span>
-                    <IconButton contentColor={`rgb(${theme.primary})`} size={'20px'}>
-                      <Send size={'16px'} />
-                    </IconButton>
-                    <div
-                      className={css`
-                        display: flex;
-                        align-items: center;
-                        height: 25px;
-                        border-radius: 100px;
-                        background-color: var(--color-backgroundLighter);
-                        padding: 8px 14px;
-                        font-size: 14px;
-                        gap: 4px;
-                        margin-left: 8px;
-                      `}
-                    >
-                      <span css={{ marginTop: '3px' }}>
-                        {i + 1} of {recipients.length}
-                      </span>
-                    </div>
-                    <div css={{ marginLeft: 'auto' }}>
-                      <AmountRow
-                        amount={amt}
-                        token={t.decoded?.outgoingToken?.token}
-                        price={t.decoded.outgoingToken?.price || 0}
-                      />
-                    </div>
-                  </div>
-                  <div css={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                    Destination
-                    <a
-                      className={css`
-                        display: flex;
-                        align-items: center;
-                        height: 25px;
-                        width: 138px;
-                        border-radius: 100px;
-                        background-color: var(--color-backgroundLighter);
-                        padding-left: 8px;
-                        font-size: 14px;
-                        gap: 4px;
-                      `}
-                      href={`https://subscan.io/address/${addr}`}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <Identicon value={addr} size={'16px'} />
-                      <span css={{ marginTop: '3px' }}>{truncateMiddle(addr, 5, 5, '...')}</span>
-                    </a>
-                  </div>
-                </div>
-              )
-            })}
-          </div>
+          {t.decoded.type === TransactionType.MultiSend ? <MultiSendExpendedDetails t={t} /> : null}
         </Collapse>
       </div>
     </div>
