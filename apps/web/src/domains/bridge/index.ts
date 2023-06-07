facfbbeefafdf900143c394578925f066a3e5520
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-06-07 22:57:43 +1200

    chore: re-enable disabled routes

--- apps/web/src/domains/bridge/index.ts
+++ apps/web/src/domains/bridge/index.ts
@@ -23,16 +23,6 @@ export const bridgeState = selector({
             // TODO: re-enable once we have custom recipient address, to target EVM addresses
             { to: 'moonbeam' },
             { to: 'moonriver' },
-            // These has not passed testing
-            { from: 'astar', to: 'acala', token: 'LDOT' },
-            { from: 'parallel', to: 'acala', token: 'PARA' },
-            { from: 'bifrost', to: 'karura', token: 'KAR' },
-            { from: 'khala', to: 'karura', token: 'PHA' },
-            { from: 'turing', to: 'karura', token: 'TUR' },
-            { from: 'bifrost', to: 'karura', token: 'BNC' },
-            { from: 'listen', to: 'karura', token: 'LKSM' },
-            { from: 'acala', to: 'hydradx', token: 'DAI' },
-            { from: 'karura', to: 'basilisk', token: 'DAI' },
           ],
     })
     await bridge.isReady
