dbfd5ec0cb8bc7b50e25d714288636e2085ca920
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-08-30 00:51:20 +1200

    style: add success color indicator

--- apps/web/src/components/recipes/ExtrinsicDetailsSideSheet/ExtrinsicDetailsSideSheet.tsx
+++ apps/web/src/components/recipes/ExtrinsicDetailsSideSheet/ExtrinsicDetailsSideSheet.tsx
@@ -1,5 +1,6 @@
 import type { Account } from '@domains/accounts'
 import { copyAddressToClipboard, copyExtrinsicHashToClipboard } from '@domains/common'
+import { useTheme } from '@emotion/react'
 import { CheckCircle, XCircle } from '@talismn/icons'
 import {
   DescriptionList,
@@ -58,156 +59,163 @@ const AccountItem = (props: { account: Account }) => (
   />
 )
 
-const ExtrinsicDetailsSideSheet = (props: ExtrinsicDetailsSideSheetProps) => (
-  <SideSheet title="Extrinsic details" onRequestDismiss={props.onRequestDismiss}>
-    <DescriptionList emphasis="details">
-      <DescriptionList.Description>
-        <DescriptionList.Term>Extrinsic ID</DescriptionList.Term>
-        <DescriptionList.Details>
-          {props.subscanUrl ? (
-            <Text.Noop.A href={props.subscanUrl} target="_blank">
-              {props.id}
-            </Text.Noop.A>
-          ) : (
-            props.id
-          )}
-        </DescriptionList.Details>
-      </DescriptionList.Description>
-      <DescriptionList.Description>
-        <DescriptionList.Term>Block</DescriptionList.Term>
-        <DescriptionList.Details>{props.blockHeight}</DescriptionList.Details>
-      </DescriptionList.Description>
-      <DescriptionList.Description>
-        <DescriptionList.Term>Extrinsic hash</DescriptionList.Term>
-        <DescriptionList.Details
-          css={{ textDecoration: 'underline', cursor: 'copy' }}
-          onClick={() => {
-            void copyExtrinsicHashToClipboard(props.hash)
-          }}
-        >
-          {shortenAddress('0xbae40a865e1b719d7c0e076c935de340c4b9759fbb1362415e87feb33353fcd2', 6)}
-        </DescriptionList.Details>
-      </DescriptionList.Description>
-      <DescriptionList.Description>
-        <DescriptionList.Term>Module</DescriptionList.Term>
-        <DescriptionList.Details>{props.module}</DescriptionList.Details>
-      </DescriptionList.Description>
-      <DescriptionList.Description>
-        <DescriptionList.Term>Call</DescriptionList.Term>
-        <DescriptionList.Details>{props.call}</DescriptionList.Details>
-      </DescriptionList.Description>
-      {props.signer && (
+const ExtrinsicDetailsSideSheet = (props: ExtrinsicDetailsSideSheetProps) => {
+  const theme = useTheme()
+  return (
+    <SideSheet title="Extrinsic details" onRequestDismiss={props.onRequestDismiss}>
+      <DescriptionList emphasis="details">
         <DescriptionList.Description>
-          <DescriptionList.Term>Signer</DescriptionList.Term>
+          <DescriptionList.Term>Extrinsic ID</DescriptionList.Term>
+          <DescriptionList.Details>
+            {props.subscanUrl ? (
+              <Text.Noop.A href={props.subscanUrl} target="_blank">
+                {props.id}
+              </Text.Noop.A>
+            ) : (
+              props.id
+            )}
+          </DescriptionList.Details>
+        </DescriptionList.Description>
+        <DescriptionList.Description>
+          <DescriptionList.Term>Block</DescriptionList.Term>
+          <DescriptionList.Details>{props.blockHeight}</DescriptionList.Details>
+        </DescriptionList.Description>
+        <DescriptionList.Description>
+          <DescriptionList.Term>Extrinsic hash</DescriptionList.Term>
           <DescriptionList.Details
             css={{ textDecoration: 'underline', cursor: 'copy' }}
             onClick={() => {
-              if (props.signer !== undefined) {
-                void copyAddressToClipboard(props.signer.address)
-              }
+              void copyExtrinsicHashToClipboard(props.hash)
             }}
           >
-            {shortenAddress(props.signer.address)}
+            {shortenAddress('0xbae40a865e1b719d7c0e076c935de340c4b9759fbb1362415e87feb33353fcd2', 6)}
           </DescriptionList.Details>
         </DescriptionList.Description>
+        <DescriptionList.Description>
+          <DescriptionList.Term>Module</DescriptionList.Term>
+          <DescriptionList.Details>{props.module}</DescriptionList.Details>
+        </DescriptionList.Description>
+        <DescriptionList.Description>
+          <DescriptionList.Term>Call</DescriptionList.Term>
+          <DescriptionList.Details>{props.call}</DescriptionList.Details>
+        </DescriptionList.Description>
+        {props.signer && (
+          <DescriptionList.Description>
+            <DescriptionList.Term>Signer</DescriptionList.Term>
+            <DescriptionList.Details
+              css={{ textDecoration: 'underline', cursor: 'copy' }}
+              onClick={() => {
+                if (props.signer !== undefined) {
+                  void copyAddressToClipboard(props.signer.address)
+                }
+              }}
+            >
+              {shortenAddress(props.signer.address)}
+            </DescriptionList.Details>
+          </DescriptionList.Description>
+        )}
+        <DescriptionList.Description>
+          <DescriptionList.Term>Time</DescriptionList.Term>
+          <DescriptionList.Details>
+            {useMemo(() => formatDistanceToNow(props.date, { addSuffix: true }), [props.date])}
+          </DescriptionList.Details>
+        </DescriptionList.Description>
+        <DescriptionList.Description>
+          <DescriptionList.Term>Result</DescriptionList.Term>
+          <DescriptionList.Details>
+            {props.success ? (
+              <CheckCircle size="1em" css={{ color: '#38D448' }} />
+            ) : (
+              <XCircle size="1em" css={{ color: theme.color.error }} />
+            )}
+          </DescriptionList.Details>
+        </DescriptionList.Description>
+      </DescriptionList>
+      {props.transfers && props.transfers.length > 0 && (
+        <section css={{ marginTop: '4.8rem' }}>
+          <header>
+            <Text.H4 alpha="high" css={{ marginBottom: '1.6rem' }}>
+              Transfers
+            </Text.H4>
+          </header>
+          <Table>
+            <thead>
+              <tr>
+                <Text.BodySmall as="th">From</Text.BodySmall>
+                <Text.BodySmall as="th">To</Text.BodySmall>
+                <Text.BodySmall as="th">Amount</Text.BodySmall>
+              </tr>
+            </thead>
+            <tbody>
+              {props.transfers.map(({ debit, credit, amount }, index) => (
+                <tr key={index}>
+                  <Text.Body as="td" alpha="high">
+                    <AccountItem account={credit} />
+                  </Text.Body>
+                  <Text.Body as="td" alpha="high">
+                    <AccountItem account={debit} />
+                  </Text.Body>
+                  <Text.Body as="td" alpha="high">
+                    {amount}
+                  </Text.Body>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </section>
       )}
-      <DescriptionList.Description>
-        <DescriptionList.Term>Time</DescriptionList.Term>
-        <DescriptionList.Details>
-          {useMemo(() => formatDistanceToNow(props.date, { addSuffix: true }), [props.date])}
-        </DescriptionList.Details>
-      </DescriptionList.Description>
-      <DescriptionList.Description>
-        <DescriptionList.Term>Result</DescriptionList.Term>
-        <DescriptionList.Details>
-          {props.success ? <CheckCircle size="1em" /> : <XCircle size="1em" />}
-        </DescriptionList.Details>
-      </DescriptionList.Description>
-    </DescriptionList>
-    {props.transfers && props.transfers.length > 0 && (
-      <section css={{ marginTop: '4.8rem' }}>
-        <header>
-          <Text.H4 alpha="high" css={{ marginBottom: '1.6rem' }}>
-            Transfers
-          </Text.H4>
-        </header>
-        <Table>
-          <thead>
-            <tr>
-              <Text.BodySmall as="th">From</Text.BodySmall>
-              <Text.BodySmall as="th">To</Text.BodySmall>
-              <Text.BodySmall as="th">Amount</Text.BodySmall>
-            </tr>
-          </thead>
-          <tbody>
-            {props.transfers.map(({ debit, credit, amount }, index) => (
-              <tr key={index}>
-                <Text.Body as="td" alpha="high">
-                  <AccountItem account={credit} />
-                </Text.Body>
-                <Text.Body as="td" alpha="high">
-                  <AccountItem account={debit} />
-                </Text.Body>
-                <Text.Body as="td" alpha="high">
-                  {amount}
-                </Text.Body>
+      {props.rewards && props.rewards?.length > 0 && (
+        <section css={{ marginTop: '4.8rem' }}>
+          <header>
+            <Text.H4 alpha="high" css={{ marginBottom: '1.6rem' }}>
+              Rewards
+            </Text.H4>
+          </header>
+          <Table>
+            <thead>
+              <tr>
+                <Text.BodySmall as="th">For</Text.BodySmall>
+                <Text.BodySmall as="th">Amount</Text.BodySmall>
               </tr>
-            ))}
-          </tbody>
-        </Table>
-      </section>
-    )}
-    {props.rewards && props.rewards?.length > 0 && (
-      <section css={{ marginTop: '4.8rem' }}>
-        <header>
-          <Text.H4 alpha="high" css={{ marginBottom: '1.6rem' }}>
-            Rewards
-          </Text.H4>
-        </header>
-        <Table>
-          <thead>
-            <tr>
-              <Text.BodySmall as="th">For</Text.BodySmall>
-              <Text.BodySmall as="th">Amount</Text.BodySmall>
-            </tr>
-          </thead>
-          <tbody>
-            {props.rewards.map(({ debit, amount }, index) => (
-              <tr key={index}>
-                <Text.Body as="td" alpha="high">
-                  <AccountItem account={debit} />
-                </Text.Body>
-                <Text.Body as="td" alpha="high">
-                  {amount}
-                </Text.Body>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </section>
-    )}
-    <Surface
-      css={{
-        marginTop: '4.8rem',
-        borderRadius: '1.6rem',
-        padding: '1.6rem',
-        [SIDE_SHEET_WIDE_BREAK_POINT_SELECTOR]: { width: '50rem' },
-      }}
-    >
-      <Text.Body as="header" alpha="high" css={{ marginBottom: '1.6rem' }}>
-        Parameters
-      </Text.Body>
+            </thead>
+            <tbody>
+              {props.rewards.map(({ debit, amount }, index) => (
+                <tr key={index}>
+                  <Text.Body as="td" alpha="high">
+                    <AccountItem account={debit} />
+                  </Text.Body>
+                  <Text.Body as="td" alpha="high">
+                    {amount}
+                  </Text.Body>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </section>
+      )}
       <Surface
         css={{
-          borderRadius: '0.8rem',
-          padding: '1rem',
-          overflow: 'auto',
-          maxHeight: '35vh',
+          marginTop: '4.8rem',
+          borderRadius: '1.6rem',
+          padding: '1.6rem',
+          [SIDE_SHEET_WIDE_BREAK_POINT_SELECTOR]: { width: '50rem' },
         }}
       >
-        <ObjectView data={props.arguments} palette={{ base00: 'transparent' }} options={{ expandLevel: 10 }} />
+        <Text.Body as="header" alpha="high" css={{ marginBottom: '1.6rem' }}>
+          Parameters
+        </Text.Body>
+        <Surface
+          css={{
+            borderRadius: '0.8rem',
+            padding: '1rem',
+            overflow: 'auto',
+            maxHeight: '35vh',
+          }}
+        >
+          <ObjectView data={props.arguments} palette={{ base00: 'transparent' }} options={{ expandLevel: 10 }} />
+        </Surface>
       </Surface>
-    </Surface>
-  </SideSheet>
-)
+    </SideSheet>
+  )
+}
 export default ExtrinsicDetailsSideSheet
