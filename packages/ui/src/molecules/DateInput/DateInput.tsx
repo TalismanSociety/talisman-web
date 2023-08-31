4406f75d2bf1378781121881229b1b5e0aa00a00
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-08-31 16:46:32 +1200

    fix: incorrect date due to timezone offset

--- packages/ui/src/molecules/DateInput/DateInput.tsx
+++ packages/ui/src/molecules/DateInput/DateInput.tsx
@@ -13,7 +13,10 @@ const DateInput = (props: DateInputProps) => {
   const value = useMemo(() => {
     if (props.value instanceof Date) {
       try {
-        return props.value.toISOString().split('T')[0]
+        const year = props.value.getFullYear().toString()
+        const month = (props.value.getMonth() + 1).toString().padStart(2, '0')
+        const day = props.value.getDate().toString().padStart(2, '0')
+        return `${year}-${month}-${day}`
       } catch {
         return undefined
       }
