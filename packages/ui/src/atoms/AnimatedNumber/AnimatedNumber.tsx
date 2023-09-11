b1822baf89691e128d755973ee6848cc3969e22c
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-09-11 12:32:17 +1200

    fix: `countup` bug
    `countup` has a bug where not explicitly re-render
    on formatting function change may reset number to 0

--- packages/ui/src/atoms/AnimatedNumber/AnimatedNumber.tsx
+++ packages/ui/src/atoms/AnimatedNumber/AnimatedNumber.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import CountUp, { type CountUpProps } from 'react-countup'
 
 export type AnimatedNumberProps = Pick<CountUpProps, 'start' | 'end'> & {
@@ -8,6 +8,9 @@ export type AnimatedNumberProps = Pick<CountUpProps, 'start' | 'end'> & {
 }
 
 const AnimatedNumber = ({ animate = true, ...props }: AnimatedNumberProps) => {
+  // IMPORTANT: countup has a bug where not explicitly re-render on formatting function change may reset number to 0
+  const [key, setKey] = useState(0)
+
   const formatter = useMemo(() => {
     if (props.formatter === undefined) {
       return
@@ -16,7 +19,18 @@ const AnimatedNumber = ({ animate = true, ...props }: AnimatedNumberProps) => {
     return props.formatter instanceof Intl.NumberFormat ? props.formatter.format.bind(props.formatter) : props.formatter
   }, [props.formatter])
 
-  return <CountUp start={animate ? 0 : props.end} duration={0.4} preserveValue formattingFn={formatter} {...props} />
+  useEffect(() => setKey(x => x + 1), [formatter])
+
+  return (
+    <CountUp
+      key={key}
+      start={animate ? 0 : props.end}
+      duration={0.4}
+      preserveValue
+      formattingFn={formatter}
+      {...props}
+    />
+  )
 }
 
 export default AnimatedNumber
