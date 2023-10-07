2b8c57d8dd8d5f731bfd9b006a8b6a8a6f0f8dab
chrisling-dev <chrisling.dev@gmail.com>
2023-10-07 23:06:32 +0800

    feat: wip

--- apps/multisig/src/components/AccountComboBox.tsx
+++ apps/multisig/src/components/AccountComboBox.tsx
@@ -4,11 +4,11 @@ import { useEffect, useMemo, useRef, useState } from 'react'
 import truncateMiddle from 'truncate-middle'
 import { ChevronVertical, Search } from '@talismn/icons'
 import { useOnClickOutside } from '../domains/common/useOnClickOutside'
+import { useSignIn } from '../domains/auth'
 
 type Props = {
   accounts: InjectedAccount[]
-  selectedAccount: InjectedAccount
-  signingInTo?: InjectedAccount
+  selectedAccount?: InjectedAccount
   onSelect?: (account: InjectedAccount) => void
 }
 
@@ -18,7 +18,7 @@ const AccountRow = ({ account }: { account: InjectedAccount }) => {
     <div css={{ display: 'flex', alignItems: 'center', gap: 8 }}>
       <Identicon size={24} value={addressString} />
       <p css={({ color }) => ({ marginTop: 4, color: color.lightGrey })}>
-        {account.meta.name}{' '}
+        <span css={{ width: '100%' }}>{account.meta.name}</span>{' '}
         <span css={({ color }) => ({ color: color.offWhite })}>({truncateMiddle(addressString, 4, 5, '...')})</span>
       </p>
     </div>
@@ -27,31 +27,38 @@ const AccountRow = ({ account }: { account: InjectedAccount }) => {
 
 const AccountComboBox: React.FC<Props> = ({ accounts, onSelect, selectedAccount }) => {
   const [expanded, setExpanded] = useState(false)
+  const { signIn, signingIn } = useSignIn()
   const ref = useRef(null)
   const [query, setQuery] = useState('')
+  const [accountToSignIn, setAccountToSignIn] = useState<InjectedAccount>()
   useOnClickOutside(ref.current, () => setExpanded(false))
 
   const filteredAccounts = useMemo(() => {
-    if (!query) return accounts
-    return accounts.filter(
-      acc =>
-        acc.meta.name?.toLowerCase().includes(query.toLowerCase()) ||
-        acc.address.toSs58().toLowerCase().includes(query.toLowerCase())
-    )
-  }, [query, accounts])
+    return accounts.filter(acc => {
+      const isSelectedAccount = selectedAccount?.address.isEqual(acc.address)
+      const isQueryMatch =
+        !query || `${acc.meta.name} ${acc.address.toSs58()}`.toLowerCase().includes(query.toLowerCase())
+      return !isSelectedAccount && isQueryMatch
+    })
+  }, [query, accounts, selectedAccount])
 
   useEffect(() => {
     if (!expanded && query.length > 0) setQuery('')
   }, [expanded, query.length])
 
+  if (!selectedAccount) return null
+
   return (
     <div ref={ref} css={{ position: 'relative', width: '100%' }}>
       <div
         css={({ color }) => ({
+          alignItems: 'center',
           display: 'flex',
           justifyContent: 'space-between',
-          background: color.foreground,
+          background: color.surface,
           borderRadius: 8,
+          border: `solid 1px ${expanded ? color.border : 'rgba(0,0,0,0)'}`,
+          borderBottom: 'none',
           width: '100%',
           padding: '16px 24px',
           ...(accounts.length > 1
@@ -68,6 +75,7 @@ const AccountComboBox: React.FC<Props> = ({ accounts, onSelect, selectedAccount 
         <AccountRow account={selectedAccount} />
         <div
           css={({ color }) => ({
+            height: 'max-content',
             lineHeight: 1,
             visibility: accounts.length > 1 ? 'visible' : 'hidden',
             color: expanded ? color.offWhite : color.lightGrey,
@@ -76,63 +84,67 @@ const AccountComboBox: React.FC<Props> = ({ accounts, onSelect, selectedAccount 
           <ChevronVertical size={24} />
         </div>
       </div>
-      {accounts.length > 1 && (
+      <div
+        css={({ color }) => ({
+          position: 'absolute',
+          top: '100%',
+          // to cover the transition of bottom border radius
+          marginTop: -8,
+          paddingTop: 8,
+          left: 0,
+          backgroundColor: color.surface,
+          borderRadius: '0px 0px 4px 4px',
+          border: `solid 1px ${expanded ? color.border : 'rgba(0,0,0,0)'}`,
+          visibility: expanded ? 'visible' : 'hidden',
+          borderTop: 'none',
+          padding: '0 24px',
+          width: '100%',
+          zIndex: 1,
+          height: 'min-content',
+          maxHeight: expanded ? 400 : 0,
+          overflow: 'hidden',
+          transition: '0.2s ease-in-out',
+        })}
+      >
         <div
-          css={({ color }) => ({
-            position: 'absolute',
-            top: '100%',
-            left: 0,
-            marginTop: 16,
-            backgroundColor: color.foreground,
-            borderRadius: 8,
-            padding: '0 24px',
-            width: '100%',
-            zIndex: 1,
-            height: 'min-content',
-            maxHeight: expanded ? 220 : 0,
-            overflow: 'hidden',
-            transition: 'max-height 0.2s ease-in-out',
+          css={({ foreground }) => ({
+            display: 'flex',
+            gap: 8,
+            alignItems: 'center',
+            borderBottom: `rgba(${foreground}, 0.1) solid 1px`,
+            paddingTop: 8,
           })}
         >
-          <div
-            css={({ foreground }) => ({
-              display: 'flex',
-              gap: 8,
-              alignItems: 'center',
-              borderBottom: `rgba(${foreground}, 0.1) solid 1px`,
-              paddingTop: 8,
-            })}
-          >
-            <Search />
-            <input
-              css={{ border: 'none', backgroundColor: 'transparent', width: '100%', padding: '16px 0px' }}
-              placeholder="Search Account..."
-              value={query}
-              onChange={e => setQuery(e.target.value)}
-            />
-          </div>
-          <div css={{ maxHeight: 160, overflowY: 'auto', padding: '8px 0' }}>
-            {filteredAccounts.map(acc => (
-              <div
-                css={({ color }) => ({
-                  'borderRadius': 8,
-                  'padding': '8px 0',
-                  'cursor': 'pointer',
-                  ':hover': { div: { p: { color: color.offWhite } } },
-                  'width': '100%',
-                })}
-                key={acc.address.toSs58()}
-                onClick={() => {
-                  setExpanded(false)
-                  onSelect?.(acc)
-                }}
-              >
-                <AccountRow account={acc} />
-              </div>
-            ))}
-          </div>
+          <Search />
+          <input
+            css={{ border: 'none', backgroundColor: 'transparent', width: '100%', padding: '16px 0px' }}
+            placeholder="Search Account..."
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+          />
         </div>
-      )}
+        <div css={{ maxHeight: 150, overflowY: 'auto', padding: '8px 0' }}>
+          {filteredAccounts.map(acc => (
+            <div
+              css={({ color }) => ({
+                'borderRadius': 8,
+                'padding': '8px 0',
+                'cursor': 'pointer',
+                'width': '100%',
+                ':hover': {
+                  div: { p: { color: color.offWhite } },
+                },
+              })}
+              key={acc.address.toSs58()}
+              onClick={() => {
+                onSelect?.(acc)
+              }}
+            >
+              <AccountRow account={acc} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
