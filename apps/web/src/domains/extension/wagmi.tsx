f4b34b5aab522dd80793a1af24e17fe5e9b65f68
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-11-15 15:39:11 +1300

    feat: only connect to talisman eth provider

--- apps/web/src/domains/extension/wagmi.tsx
+++ apps/web/src/domains/extension/wagmi.tsx
@@ -1,16 +1,29 @@
 import type { PropsWithChildren } from 'react'
-import { WagmiConfig, configureChains, createConfig } from 'wagmi'
+import { WagmiConfig, configureChains, createConfig, type WindowProvider } from 'wagmi'
 import { mainnet, moonbeam, moonriver } from 'wagmi/chains'
 import { publicProvider } from 'wagmi/providers/public'
+import '@wagmi/core/window'
 
 import { InjectedConnector } from 'wagmi/connectors/injected'
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface Window {
+    talismanEth?: WindowProvider
+  }
+}
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet, moonbeam, moonriver],
   [publicProvider()]
 )
 
-export const wagmiInjectedConnector = new InjectedConnector({ chains })
+export const wagmiInjectedConnector = new InjectedConnector({
+  chains,
+  options: {
+    getProvider: () => window.talismanEth,
+  },
+})
 
 const config = createConfig({
   autoConnect: true,
