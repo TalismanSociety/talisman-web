cc3b2b57504bd255aae7ea123580e5153be7f0f1
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-11-23 15:48:20 +1300

    feat: new header layout

--- apps/web/src/routes/staking.tsx
+++ apps/web/src/routes/staking.tsx
@@ -4,28 +4,25 @@ import Stakes from '@components/widgets/staking/Stakes'
 import { Layers, Zap } from '@talismn/icons'
 import { SegmentedButton, Surface, Text } from '@talismn/ui'
 import { useState } from 'react'
+import { TitlePortal } from './layout'
 
 const Staking = () => {
   const sections = ['stakeable-assets', 'positions'] as const
   const [selectedSection, setSelectedSection] = useState<(typeof sections)[number]>('stakeable-assets')
   return (
     <AccountConnectionGuard>
-      <div>
-        <header>
-          <Text.H2>Staking</Text.H2>
-        </header>
-        <Surface css={{ borderRadius: '1.6rem', padding: '1.6rem' }}>
-          <SegmentedButton value={selectedSection} onChange={setSelectedSection} css={{ marginBottom: '2.4rem' }}>
-            <SegmentedButton.ButtonSegment value="stakeable-assets" leadingIcon={<Zap />}>
-              Stake
-            </SegmentedButton.ButtonSegment>
-            <SegmentedButton.ButtonSegment value="positions" leadingIcon={<Layers />}>
-              My positions
-            </SegmentedButton.ButtonSegment>
-          </SegmentedButton>
-          {selectedSection === 'positions' ? <Stakes hideHeader /> : <StakeProviders />}
-        </Surface>
-      </div>
+      <TitlePortal>Staking</TitlePortal>
+      <Surface css={{ borderRadius: '1.6rem', padding: '1.6rem' }}>
+        <SegmentedButton value={selectedSection} onChange={setSelectedSection} css={{ marginBottom: '2.4rem' }}>
+          <SegmentedButton.ButtonSegment value="stakeable-assets" leadingIcon={<Zap />}>
+            Stake
+          </SegmentedButton.ButtonSegment>
+          <SegmentedButton.ButtonSegment value="positions" leadingIcon={<Layers />}>
+            My positions
+          </SegmentedButton.ButtonSegment>
+        </SegmentedButton>
+        {selectedSection === 'positions' ? <Stakes hideHeader /> : <StakeProviders />}
+      </Surface>
     </AccountConnectionGuard>
   )
 }
