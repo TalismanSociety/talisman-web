13ff78391bcb883598e7a7e9d3a6f7bc91b1d323
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-11-23 15:56:59 +1300

    feat: staking page stats header

--- apps/web/src/routes/staking.tsx
+++ apps/web/src/routes/staking.tsx
@@ -2,9 +2,18 @@ import AccountConnectionGuard from '@components/widgets/AccountConnectionGuard'
 import StakeProviders from '@components/widgets/staking/StakeProviders'
 import Stakes from '@components/widgets/staking/Stakes'
 import { Layers, Zap } from '@talismn/icons'
-import { SegmentedButton, Surface, Text } from '@talismn/ui'
-import { useState } from 'react'
-import { TitlePortal } from './layout'
+import { CircularProgressIndicator, SegmentedButton, Surface, Text } from '@talismn/ui'
+import { Suspense, useState } from 'react'
+import { HeaderWidgetPortal, TitlePortal } from './layout'
+import { useTotalStaked } from '@domains/staking'
+import { useRecoilValue } from 'recoil'
+import { selectedCurrencyState } from '@domains/balances'
+
+const TotalStaked = () => (
+  <>
+    {useTotalStaked().toLocaleString(undefined, { style: 'currency', currency: useRecoilValue(selectedCurrencyState) })}
+  </>
+)
 
 const Staking = () => {
   const sections = ['stakeable-assets', 'positions'] as const
@@ -12,6 +21,16 @@ const Staking = () => {
   return (
     <AccountConnectionGuard>
       <TitlePortal>Staking</TitlePortal>
+      <HeaderWidgetPortal>
+        <div css={{ marginTop: '1.25em' }}>
+          <Text.BodyLarge as="div">Staking balance</Text.BodyLarge>
+          <Text.H3 as="div" css={{ marginTop: '0.125em' }}>
+            <Suspense fallback={<CircularProgressIndicator size="1em" />}>
+              <TotalStaked />
+            </Suspense>
+          </Text.H3>
+        </div>
+      </HeaderWidgetPortal>
       <Surface css={{ borderRadius: '1.6rem', padding: '1.6rem' }}>
         <SegmentedButton value={selectedSection} onChange={setSelectedSection} css={{ marginBottom: '2.4rem' }}>
           <SegmentedButton.ButtonSegment value="stakeable-assets" leadingIcon={<Zap />}>
