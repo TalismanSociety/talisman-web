e14a2a8148a92c2d88e7cf0deeacf926cb186b9a
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-12-08 16:49:11 +1300

    fix: don't include reserved balance in stake percentage calculation

--- apps/web/src/components/widgets/staking/substrate/StakeProviders.tsx
+++ apps/web/src/components/widgets/staking/substrate/StakeProviders.tsx
@@ -54,10 +54,7 @@ const StakePercentage = () => {
   const balances = useRecoilValue(
     waitForAll(addresses.map(address => chainDeriveState(apiId, 'balances', 'all', [address])))
   )
-  const total = useMemo(
-    () => balances.reduce((prev, curr) => prev + curr.freeBalance.toBigInt() + curr.reservedBalance.toBigInt(), 0n),
-    [balances]
-  )
+  const total = useMemo(() => balances.reduce((prev, curr) => prev + curr.freeBalance.toBigInt(), 0n), [balances])
   const poolMembers = useRecoilValue(useQueryState('nominationPools', 'poolMembers.multi', addresses))
   const staked = useMemo(
     () => poolMembers.reduce((prev, curr) => prev + curr.unwrapOrDefault().points.toBigInt(), 0n),
