619dbf8a7161693b43c1c621db8199dd0eac0624
alecdwm <alec@owls.io>
2024-01-09 17:03:31 +0000

    fix: show watched account crowdloan contributions & improve contribution sorting

--- apps/web/src/libs/crowdloans/useCrowdloanContributions.ts
+++ apps/web/src/libs/crowdloans/useCrowdloanContributions.ts
@@ -1,4 +1,5 @@
 import { selectedSubstrateAccountsState } from '@domains/accounts'
+import { useChainmetaValue } from '@libs/talisman'
 import { encodeAnyAddress } from '@talismn/util'
 import BigNumber from 'bignumber.js'
 import { request } from 'graphql-request'
@@ -12,7 +13,8 @@ type ContributionsIndexerConfig = {
   accountIndex: number
   genesisHash: string
   indexerUrl: string
-  setStateFn: (contributions: GqlContribution[]) => void
+  setStateFn: (contributions: _GqlContribution[]) => void
+  setHydratedFn: (hydrated: boolean) => void
 }
 
 const ContributionsIndexerConfigs = {
@@ -40,20 +42,33 @@ export type CrowdloanContribution = {
   fund: { id: string }
 }
 
-export type GqlContribution = ContributionsQuery['contributions'][0] & { relay: { genesisHash: string } }
+type _GqlContribution = ContributionsQuery['contributions'][0] & { relay: { genesisHash: string } }
+export type GqlContribution = ContributionsQuery['contributions'][0] & {
+  relay: { genesisHash: string }
+  blockNumber: number
+  blockPeriod: number
+  lockedSeconds: number
+  isLocked: boolean
+  isUnlockingSoon: boolean
+  isFundsReturned: boolean
+  oldAndReturned: boolean
+}
 
 export function useCrowdloanContributions(accounts?: string[]): {
   gqlContributions: GqlContribution[]
+  sortedGqlContributions: GqlContribution[]
   hydrated: boolean
 } {
   const allAccounts = useRecoilValue(selectedSubstrateAccountsState)
-  const [hydrated, setHydrated] = useState(false)
 
-  const [dotContributions, setDotContributions] = useState<GqlContribution[]>([])
-  const [ksmContributions, setKsmContributions] = useState<GqlContribution[]>([])
+  const [dotContributions, setDotContributions] = useState<_GqlContribution[]>([])
+  const [ksmContributions, setKsmContributions] = useState<_GqlContribution[]>([])
+
+  const [dotHydrated, setDotHydrated] = useState(false)
+  const [ksmHydrated, setKsmHydrated] = useState(false)
 
   const fetchGqlContributions = useCallback(
-    ({ accountIndex, genesisHash, indexerUrl, setStateFn }: ContributionsIndexerConfig) => {
+    ({ accountIndex, genesisHash, indexerUrl, setStateFn, setHydratedFn }: ContributionsIndexerConfig) => {
       let cancelled = false
 
       const addresses = (accounts ?? allAccounts.map(account => account.address)).map(address =>
@@ -107,7 +122,7 @@ export function useCrowdloanContributions(accounts?: string[]): {
             ...contribution,
             relay: { genesisHash },
           }))
-          const byAccountFundIndex = new Map<string, GqlContribution>()
+          const byAccountFundIndex = new Map<string, _GqlContribution>()
           gqlContributions.forEach(contribution => {
             const accountFundIndex = `${contribution.account.id}-${contribution.crowdloan.fundIndex}`
             const additionalAmount = BigInt(byAccountFundIndex.get(accountFundIndex)?.amount ?? '0')
@@ -122,7 +137,7 @@ export function useCrowdloanContributions(accounts?: string[]): {
             })
           })
           setStateFn(Array.from(byAccountFundIndex.values()))
-          setHydrated(true)
+          setHydratedFn(true)
         })
         .catch(error => {
           console.error('Failed to fetch contributions', error)
@@ -136,25 +151,101 @@ export function useCrowdloanContributions(accounts?: string[]): {
   )
 
   const fetchDotContributions = useCallback(() => {
-    fetchGqlContributions({ ...ContributionsIndexerConfigs.polkadot, setStateFn: setDotContributions })
-  }, [fetchGqlContributions])
-  const fetchKsmContributions = useCallback(() => {
-    fetchGqlContributions({ ...ContributionsIndexerConfigs.kusama, setStateFn: setKsmContributions })
+    fetchGqlContributions({
+      ...ContributionsIndexerConfigs.polkadot,
+      setStateFn: setDotContributions,
+      setHydratedFn: setDotHydrated,
+    })
   }, [fetchGqlContributions])
 
+  const fetchKsmContributions = useCallback(() => {
+    fetchGqlContributions({
+      ...ContributionsIndexerConfigs.kusama,
+      setStateFn: setKsmContributions,
+      setHydratedFn: setKsmHydrated,
+    })
+  }, [fetchGqlContributions])
+
+  const { recentDotBlock, recentDotPeriod, recentKsmBlock, recentKsmPeriod, updateRecentBlocks } = useRecentBlocks()
+
+  // fetch on load
   useEffect(() => {
-    const cancel = fetchDotContributions
-    return cancel
-  }, [fetchDotContributions])
-  useEffect(() => {
-    const cancel = fetchKsmContributions
-    return cancel
-  }, [fetchKsmContributions])
-  useInterval(fetchKsmContributions, 120_000) // re-fetch every 2 minutes
+    updateRecentBlocks()
+
+    const cancelDot = fetchDotContributions
+    const cancelKsm = fetchKsmContributions
+
+    return () => {
+      cancelDot()
+      cancelKsm()
+    }
+  }, [fetchDotContributions, fetchKsmContributions, updateRecentBlocks])
+
+  // re-fetch every 2 minutes
+  useInterval(() => {
+    updateRecentBlocks()
+
+    fetchDotContributions()
+    fetchKsmContributions()
+  }, 120_000)
+
+  const gqlContributions = useMemo(
+    () =>
+      [...dotContributions, ...ksmContributions].flatMap(contribution => {
+        const isDot = contribution.relay.genesisHash === ContributionsIndexerConfigs.polkadot.genesisHash
+        const isKsm = contribution.relay.genesisHash === ContributionsIndexerConfigs.kusama.genesisHash
+
+        const blockNumber = (isDot ? recentDotBlock : isKsm ? recentKsmBlock : 0) ?? 0
+        const blockPeriod = (isDot ? recentDotPeriod : isKsm ? recentKsmPeriod : 6) ?? 6
+
+        const lockedSeconds = ((contribution.crowdloan.lastBlock ?? 0) - blockNumber) * blockPeriod
+
+        const isLocked = lockedSeconds > 0
+        const isUnlockingSoon = lockedSeconds <= 60 * 60 * 72 // 72 hours
+
+        const isFundsReturned = contribution.returned || contribution.crowdloan.dissolved
+
+        // hide returned contributions which were unlocked more than 30 days ago
+        const oldAndReturned = isFundsReturned && lockedSeconds < -60 * 60 * 24 * 30
+        if (oldAndReturned) return []
+
+        return {
+          ...contribution,
+
+          /** Updated every 2 minutes */
+          blockNumber,
+          /** Updated every 2 minutes */
+          blockPeriod,
+          /** Updated every 2 minutes */
+          lockedSeconds,
+          /** Updated every 2 minutes */
+          isLocked,
+          /** Updated every 2 minutes */
+          isUnlockingSoon,
+          /** Updated every 2 minutes */
+          isFundsReturned,
+          /** Updated every 2 minutes */
+          oldAndReturned,
+        }
+      }),
+    [dotContributions, ksmContributions, recentDotBlock, recentDotPeriod, recentKsmBlock, recentKsmPeriod]
+  )
+
+  const sortedGqlContributions = useMemo(() => {
+    return gqlContributions.slice().sort((a, b) => {
+      const aLockedSeconds = a.lockedSeconds ?? Number.MAX_SAFE_INTEGER
+      const bLockedSeconds = b.lockedSeconds ?? Number.MAX_SAFE_INTEGER
+
+      if (aLockedSeconds !== bLockedSeconds) return aLockedSeconds - bLockedSeconds
+      if (a.crowdloan.fundIndex !== b.crowdloan.fundIndex) return a.crowdloan.fundIndex - b.crowdloan.fundIndex
+      return a.timestamp - b.timestamp
+    })
+  }, [gqlContributions])
 
   return {
-    gqlContributions: useMemo(() => [...dotContributions, ...ksmContributions], [dotContributions, ksmContributions]),
-    hydrated,
+    gqlContributions,
+    sortedGqlContributions,
+    hydrated: dotHydrated && recentDotBlock !== null && ksmHydrated && recentKsmBlock !== null,
   }
 }
 
@@ -178,4 +269,30 @@ export function getTotalContributionForCrowdloan(crowdloan: string, contribution
     .map(contribution => contribution.amount)
     .reduce((prev, curr) => prev.plus(curr), new BigNumber(0))
     .toString()
+}
+
+//
+// Helpers (internal)
+//
+
+// We need this for sorting, but we don't want to update every UI component every 6s
+// So we'll only do the calculation every 15 minutes
+function useRecentBlocks() {
+  const [[recentDotBlock, recentDotPeriod], setRecentDot] = useState<[number | null, number | null]>([null, null])
+  const [[recentKsmBlock, recentKsmPeriod], setRecentKsm] = useState<[number | null, number | null]>([null, null])
+
+  const dotBlockNumber = useChainmetaValue(0, 'blockNumber')
+  const dotBlockPeriod = useChainmetaValue(0, 'blockPeriod')
+
+  const ksmBlockNumber = useChainmetaValue(2, 'blockNumber')
+  const ksmBlockPeriod = useChainmetaValue(2, 'blockPeriod')
+
+  const updateRecentBlocks = useCallback(() => {
+    if (dotBlockNumber === null || ksmBlockNumber === null) return
+
+    setRecentDot([parseInt(dotBlockNumber ?? '0'), dotBlockPeriod])
+    setRecentKsm([parseInt(ksmBlockNumber ?? '0'), ksmBlockPeriod])
+  }, [dotBlockNumber, dotBlockPeriod, ksmBlockNumber, ksmBlockPeriod])
+
+  return { recentDotBlock, recentDotPeriod, recentKsmBlock, recentKsmPeriod, updateRecentBlocks }
 }
