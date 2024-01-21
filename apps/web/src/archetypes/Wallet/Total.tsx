b30664d2475e29525b6701b3d6372542a0efb895
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-01-21 20:00:45 +1300

    refactor: balances
    - Move balances effect to recoil selector
    - Remove unused atoms

--- apps/web/src/archetypes/Wallet/Total.tsx
+++ apps/web/src/archetypes/Wallet/Total.tsx
@@ -1,19 +1,11 @@
 import AnimatedFiatNumber from '@components/widgets/AnimatedFiatNumber'
-import { totalSelectedAccountsFiatBalance } from '@domains/balances'
+import { selectedBalancesFiatSumState } from '@domains/balances'
 import { CircularProgressIndicator } from '@talismn/ui'
 import { Suspense } from 'react'
 import { useRecoilValue } from 'recoil'
 
 const TotalSuspense = () => {
-  const fiatTotal = useRecoilValue(totalSelectedAccountsFiatBalance)
-
-  // TODO: move these value into balances lib
-  // const crowdloanTotal = useTotalCrowdloanTotalFiatAmount()
-  // const totalStaked = useTotalStaked()
-
-  const totalPortfolioValue = fiatTotal
-
-  return <AnimatedFiatNumber end={totalPortfolioValue} />
+  return <AnimatedFiatNumber end={useRecoilValue(selectedBalancesFiatSumState).total} />
 }
 
 const Total = () => {
