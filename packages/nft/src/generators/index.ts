a8ca48958cbbe2fc71fbe7a2c8839faca3d5414a
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-03-06 17:28:09 +1300

    chore: remove deprecated Substrate `unique` pallet NFT support

--- packages/nft/src/generators/index.ts
+++ packages/nft/src/generators/index.ts
@@ -2,7 +2,6 @@ export { createAcalaNftAsyncGenerator, createBitCountryNftAsyncGenerator } from 
 export { createEvmNftAsyncGenerator } from './evm/index.js'
 
 export { createRmrk2NftAsyncGenerator } from './rmrk2.js'
-export { createStatemineNftAsyncGenerator } from './statemine.js'
 export { createSubstrateNftKusamaAssetHubNftAsyncGenerator } from './substrateNftKusamaAssetHub.js'
 export { createSubstrateNftPolkadotAssetHubNftAsyncGenerator } from './substrateNftPolkadotAssetHub.js'
 export { createUniqueNetworkNftAsyncGenerator } from './unique.js'
