a375d0341db1c177750c1ecd73fa966297f51365
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-05-02 11:05:10 +1200

    chore: add error boundary

--- apps/portal/src/routes/dex/layout.tsx
+++ apps/portal/src/routes/dex/layout.tsx
@@ -1,3 +1,4 @@
+import ErrorBoundary from '@components/widgets/ErrorBoundary'
 import { Tabs, TalismanHandProgressIndicator } from '@talismn/ui'
 import { Suspense, startTransition } from 'react'
 import { NavLink, Outlet, useNavigate } from 'react-router-dom'
@@ -13,29 +14,31 @@ const Layout = () => {
         alignItems: 'center',
       }}
     >
-      <Suspense fallback={<TalismanHandProgressIndicator />}>
-        <div>
-          <Tabs css={{ width: 'fit-content', marginBottom: '1.6rem' }}>
-            <NavLink to="/transfer/swap" css={{ display: 'contents' }} onClick={event => event.preventDefault()}>
-              {({ isActive }) => (
-                <Tabs.Item selected={isActive} onClick={() => startTransition(() => navigate('/transfer/swap'))}>
-                  Swap
-                </Tabs.Item>
-              )}
-            </NavLink>
-            <NavLink to="/transfer/transport" css={{ display: 'contents' }} onClick={event => event.preventDefault()}>
-              {({ isActive }) => (
-                <Tabs.Item selected={isActive} onClick={() => startTransition(() => navigate('/transfer/transport'))}>
-                  Transport
-                </Tabs.Item>
-              )}
-            </NavLink>
-          </Tabs>
-          <div css={{ width: 'min-content' }}>
-            <Outlet />
+      <ErrorBoundary>
+        <Suspense fallback={<TalismanHandProgressIndicator />}>
+          <div>
+            <Tabs css={{ width: 'fit-content', marginBottom: '1.6rem' }}>
+              <NavLink to="/transfer/swap" css={{ display: 'contents' }} onClick={event => event.preventDefault()}>
+                {({ isActive }) => (
+                  <Tabs.Item selected={isActive} onClick={() => startTransition(() => navigate('/transfer/swap'))}>
+                    Swap
+                  </Tabs.Item>
+                )}
+              </NavLink>
+              <NavLink to="/transfer/transport" css={{ display: 'contents' }} onClick={event => event.preventDefault()}>
+                {({ isActive }) => (
+                  <Tabs.Item selected={isActive} onClick={() => startTransition(() => navigate('/transfer/transport'))}>
+                    Transport
+                  </Tabs.Item>
+                )}
+              </NavLink>
+            </Tabs>
+            <div css={{ width: 'min-content' }}>
+              <Outlet />
+            </div>
           </div>
-        </div>
-      </Suspense>
+        </Suspense>
+      </ErrorBoundary>
     </div>
   )
 }
