141663618da841e166f2c241cebd712f2c8acca7
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-05-06 11:49:50 +1200

    chore: delete deprecated & unused components

--- apps/portal/src/components/recipes/StakeDetails/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './StakeDetails'
-export { default } from './StakeDetails'
-export { default as EmptyStakeDetails } from './EmptyStakeDetails'
--- apps/portal/src/components/templates/StakeDashboard/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './StakeDashboard'
-export { default } from './StakeDashboard'
