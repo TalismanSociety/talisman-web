c654eb91c690626a38f72b6da0306a253efca82d
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-05-10 14:27:43 +1200

    refactor: create pre-styled `Menu.Item.Button` component

--- packages/ui/src/molecules/Menu/Menu.stories.tsx
+++ packages/ui/src/molecules/Menu/Menu.stories.tsx
@@ -3,8 +3,7 @@ import { ChevronDown } from '@talismn/web-icons'
 import { motion } from 'framer-motion'
 
 import { Button, Identicon } from '../../atoms'
-import ListItem from '../ListItem'
-import Menu, { type MenuProps, MENU_OFFSET } from './Menu'
+import Menu, { MENU_OFFSET, type MenuProps } from './Menu'
 
 export default {
   title: 'Molecules/Menu',
@@ -38,34 +37,26 @@ Default.args = {
       </Button>
     </Menu.Button>,
     <Menu.Items key={1}>
-      <Menu.Item>
-        <ListItem
-          headlineContent="Polkadot.js import"
-          overlineContent="$356,120.32"
-          leadingContent={<Identicon value="1YmEYgtfPbwx5Jos1PjKDWRpuJWSpTzytwZgYan6kgiquNS" size="4rem" />}
-        />
-      </Menu.Item>
-      <Menu.Item>
-        <ListItem
-          headlineContent="Polkadot.js import"
-          overlineContent="$356,120.32"
-          leadingContent={<Identicon value="1YmEYgtfPbwx5Jos1PjKDWRpuJWSpTzytwZgYan6kgiquNS" size="4rem" />}
-        />
-      </Menu.Item>
-      <Menu.Item>
-        <ListItem
-          headlineContent="Polkadot.js import"
-          overlineContent="$356,120.32"
-          leadingContent={<Identicon value="1YmEYgtfPbwx5Jos1PjKDWRpuJWSpTzytwZgYan6kgiquNS" size="4rem" />}
-        />
-      </Menu.Item>
-      <Menu.Item>
-        <ListItem
-          headlineContent="Polkadot.js import"
-          overlineContent="$356,120.32"
-          leadingContent={<Identicon value="1YmEYgtfPbwx5Jos1PjKDWRpuJWSpTzytwZgYan6kgiquNS" size="4rem" />}
-        />
-      </Menu.Item>
+      <Menu.Item.Button
+        headlineContent="Polkadot.js import"
+        overlineContent="$356,120.32"
+        leadingContent={<Identicon value="1YmEYgtfPbwx5Jos1PjKDWRpuJWSpTzytwZgYan6kgiquNS" size="4rem" />}
+      />
+      <Menu.Item.Button
+        headlineContent="Polkadot.js import"
+        overlineContent="$356,120.32"
+        leadingContent={<Identicon value="1YmEYgtfPbwx5Jos1PjKDWRpuJWSpTzytwZgYan6kgiquNS" size="4rem" />}
+      />
+      <Menu.Item.Button
+        headlineContent="Polkadot.js import"
+        overlineContent="$356,120.32"
+        leadingContent={<Identicon value="1YmEYgtfPbwx5Jos1PjKDWRpuJWSpTzytwZgYan6kgiquNS" size="4rem" />}
+      />
+      <Menu.Item.Button
+        headlineContent="Polkadot.js import"
+        overlineContent="$356,120.32"
+        leadingContent={<Identicon value="1YmEYgtfPbwx5Jos1PjKDWRpuJWSpTzytwZgYan6kgiquNS" size="4rem" />}
+      />
     </Menu.Items>,
   ],
 }
