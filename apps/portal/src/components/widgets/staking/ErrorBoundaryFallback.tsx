5573bd1691d25449f8f629c1cd19af7b752f16d1
Will Urban <will.urban.dev@gmail.com>
2024-06-10 11:21:05 +0700

    refactor: errorBoundaryFallback match Positions style

--- apps/portal/src/components/widgets/staking/ErrorBoundaryFallback.tsx
+++ apps/portal/src/components/widgets/staking/ErrorBoundaryFallback.tsx
@@ -1,66 +1,69 @@
 import ErrorMessage from '../../recipes/ErrorMessage'
 import StakeProvider from '../../recipes/StakeProvider'
-import { Surface, Text } from '@talismn/ui'
+import { Surface, Text, SurfaceIconButton } from '@talismn/ui'
+import { MoreHorizontal } from '@talismn/web-icons'
 
 type ErrorBoundaryFallbackProps = {
   symbol: string
   logo: string
   provider: string
+  list?: 'stakes' | 'positions'
 }
 
-export default function ErrorBoundaryFallback({ symbol, logo, provider }: ErrorBoundaryFallbackProps) {
+export default function ErrorBoundaryFallback({ symbol, logo, provider, list }: ErrorBoundaryFallbackProps) {
+  const isPositions = list === 'positions'
   return (
-    <div css={{ containerType: 'inline-size' }}>
-      <Surface
+    <div css={{ containerType: 'inline-size', position: 'relative' }}>
+      <div
         css={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          borderRadius: '0.8rem',
           padding: '1.6rem',
-          position: 'relative',
+          position: 'absolute',
+          top: '0',
+          left: '0',
+          width: '100%',
+          height: '100%',
+          background: 'rgba(255, 255, 255, 0.1)',
+          backdropFilter: 'blur(2px)',
+          borderRadius: isPositions ? '1.6rem' : '0.8rem',
         }}
       >
         <div
           css={{
             display: 'flex',
             alignItems: 'center',
-            gap: '0.6rem',
+            justifyContent: 'center',
+            padding: '1.6rem',
+            height: '100%',
           }}
         >
-          <div
-            css={{
-              padding: '1.6rem',
-              position: 'absolute',
-              top: '0',
-              left: '0',
-              width: '100%',
-              height: '100%',
-              background: 'rgba(255, 255, 255, 0.1)',
-              backdropFilter: 'blur(2px)',
-            }}
-          >
-            <div
-              css={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                padding: '1.6rem',
-                height: '100%',
-              }}
-            >
-              <ErrorMessage
-                orientation="horizontal"
-                title="Oops!"
-                message={<span> {`Error loading ${provider} staking provider data for ${symbol} token.`}</span>}
-              />
-            </div>
-          </div>
+          <ErrorMessage
+            orientation="horizontal"
+            title="Oops!"
+            message={<span> {`Error loading ${provider} staking provider data for ${symbol} token.`}</span>}
+          />
+        </div>
+      </div>
+      <Surface
+        css={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          borderRadius: isPositions ? '1.6rem' : '0.8rem',
+          padding: '1.6rem',
+        }}
+      >
+        <div
+          css={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: isPositions ? '1.6rem' : '0.6rem',
+          }}
+        >
           <img
             src={logo}
             css={{
-              width: '2em',
-              height: '2em',
+              width: isPositions ? '4rem' : '2em',
+              height: isPositions ? '4rem' : '2em',
               borderRadius: '50%',
             }}
           />
@@ -71,7 +74,13 @@ export default function ErrorBoundaryFallback({ symbol, logo, provider }: ErrorB
             <Text.BodySmall as="div">{provider}</Text.BodySmall>
           </div>
         </div>
-        <StakeProvider.StakeButton />
+        {isPositions ? (
+          <SurfaceIconButton>
+            <MoreHorizontal />
+          </SurfaceIconButton>
+        ) : (
+          <StakeProvider.StakeButton />
+        )}
       </Surface>
     </div>
   )
