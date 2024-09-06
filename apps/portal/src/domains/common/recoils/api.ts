e9b2a266a6f5382f04cd9e8dcf6c0ef45b3e2bfa
0xKheops <26880866+0xKheops@users.noreply.github.com>
2024-09-06 19:53:32 +0900

    fix: api should never mutate

--- apps/portal/src/domains/common/recoils/api.ts
+++ apps/portal/src/domains/common/recoils/api.ts
@@ -1,5 +1,4 @@
 import { useSubstrateApiEndpoint } from '..'
-import { chainsState } from '@/domains/chains'
 import { ApiPromise, WsProvider } from '@polkadot/api'
 import * as AvailJsSdk from 'avail-js-sdk'
 import { atom, useAtom } from 'jotai'
@@ -8,21 +7,20 @@ import { selectorFamily, useRecoilCallback } from 'recoil'
 
 export const substrateApiState = selectorFamily<ApiPromise, string | undefined>({
   key: 'SubstrateApiState',
-  get:
-    endpoint =>
-    async ({ get }) => {
-      const availEndpoints = (get(chainsState).find(chain => chain.id === 'avail')?.rpcs ?? []).map(({ url }) => url)
-      const isAvail = endpoint && availEndpoints.includes(endpoint)
-      if (isAvail)
-        return await ApiPromise.create({
-          provider: new WsProvider(endpoint),
-          types: AvailJsSdk.spec.types,
-          rpc: AvailJsSdk.spec.rpc,
-          signedExtensions: AvailJsSdk.spec.signedExtensions,
-        })
+  // DO NOT USE any atom dependency here, nothing should invalidate an api object once created
+  get: endpoint => async () => {
+    const isAvail = endpoint && /([/.-]avail-)|([/.-]avail\.)/.test(endpoint)
 
-      return await ApiPromise.create({ provider: new WsProvider(endpoint) })
-    },
+    if (isAvail)
+      return await ApiPromise.create({
+        provider: new WsProvider(endpoint),
+        types: AvailJsSdk.spec.types,
+        rpc: AvailJsSdk.spec.rpc,
+        signedExtensions: AvailJsSdk.spec.signedExtensions,
+      })
+
+    return await ApiPromise.create({ provider: new WsProvider(endpoint) })
+  },
   dangerouslyAllowMutability: true,
   cachePolicy_UNSTABLE: { eviction: 'most-recent' },
 })
