fd27734166fbfac0a329070dad7eee908b70719b
Will Urban <will.urban.dev@gmail.com>
2024-09-06 11:35:53 +0700

    feat: created customHook to cache lodable values between renders

---
+++ apps/portal/src/hooks/useCachedRecoilValueLoadable.ts
@@ -0,0 +1,33 @@
+import { useRef, useEffect } from 'react'
+import { useRecoilValueLoadable, RecoilValue, Loadable } from 'recoil'
+
+type CachedRecoilValueLoadableReturnTest<T> = {
+  state: Loadable<T>['state']
+  contents: T | null
+}
+
+export function useCachedRecoilValueLoadable<T>(selector: RecoilValue<T>): CachedRecoilValueLoadableReturnTest<T> {
+  const loadable = useRecoilValueLoadable(selector)
+  const lastLoadedValue = useRef<T | null>(null)
+
+  useEffect(() => {
+    if (loadable.state === 'hasValue') {
+      const newContents = loadable.contents
+
+      if (lastLoadedValue.current === null) {
+        lastLoadedValue.current = newContents
+      } else if (Array.isArray(newContents)) {
+        newContents.forEach((value, index) => {
+          if (value != null && !(Array.isArray(value) && value.length === 0)) {
+            ;(lastLoadedValue.current as any[])[index] = value
+          }
+        })
+      }
+    }
+  }, [loadable])
+
+  return {
+    state: loadable.state === 'loading' && lastLoadedValue.current ? 'hasValue' : loadable.state,
+    contents: loadable.state === 'loading' ? lastLoadedValue.current : loadable.contents,
+  }
+}
