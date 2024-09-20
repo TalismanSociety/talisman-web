0c5906397856c01c706a864b253e96c06e6ff639
Will Urban <will.urban.dev@gmail.com>
2024-09-20 17:19:10 +0700

    chore: TAO apy placeholder (#1179)
    * chore: added TAO apy placeholder fallback
    
    * revert: vite config change

--- apps/portal/src/domains/staking/subtensor/hooks/useApr.ts
+++ apps/portal/src/domains/staking/subtensor/hooks/useApr.ts
@@ -5,8 +5,10 @@ export const useApr = (genesisHash: string | undefined) => {
   return useAtomValue(stakingAprByChainAtomFamily(genesisHash))
 }
 
+const TAO_APY_PLACEHOLDER = '~16%'
+
 export const useAprFormatted = (genesisHash: string | undefined) => {
   const apr = useApr(genesisHash)
 
-  return apr ? apr.toLocaleString(undefined, { style: 'percent', maximumFractionDigits: 2 }) : '--'
+  return apr ? apr.toLocaleString(undefined, { style: 'percent', maximumFractionDigits: 2 }) : TAO_APY_PLACEHOLDER
 }
