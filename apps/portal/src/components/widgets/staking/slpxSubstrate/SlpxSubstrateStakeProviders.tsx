107352008e49af25a0a1a7c5791ecfb1098db51a
Will Urban <will.urban.dev@gmail.com>
2024-10-03 12:14:12 +0700

    feat: apr and SlpxSubstrate provider data

--- apps/portal/src/components/widgets/staking/slpxSubstrate/SlpxSubstrateStakeProviders.tsx
+++ apps/portal/src/components/widgets/staking/slpxSubstrate/SlpxSubstrateStakeProviders.tsx
@@ -1,6 +1,10 @@
 import { ChainProvider } from '../../../../domains/chains'
 import { slpxSubstratePairsState } from '../../../../domains/staking/slpxSubstrate/recoils'
 import StakeProvider from '../../../recipes/StakeProvider'
+import Apr from '../slpx/Apr'
+import { AvailableBalance, AvailableFiatBalance } from '../slpx/AvailableBalances'
+import StakePercentage from './StakePercentage'
+// import UnlockDuration from '../slpx/UnlockDuration'
 import { useAtomValue } from 'jotai'
 
 const SlpxSubstrateStakeProviders = () => {
@@ -8,7 +12,8 @@ const SlpxSubstrateStakeProviders = () => {
   const provider = 'Bifrost SLPx'
   return (
     <>
-      {slpxSubstratePairs.map(({ nativeToken, substrateChainGenesisHash }, index) => {
+      {slpxSubstratePairs.map((slpxPair, index) => {
+        const { nativeToken, substrateChainGenesisHash } = slpxPair
         const { symbol, logo } = nativeToken
         return (
           <ChainProvider
@@ -21,13 +26,14 @@ const SlpxSubstrateStakeProviders = () => {
               symbol={symbol}
               logo={logo}
               chain={'Bifrost'}
-              apr={'123%'}
+              apr={<Apr slpxPair={slpxPair} />}
               type="Liquid staking"
               provider={provider}
               unbondingPeriod={'123'}
-              availableBalance={'123'}
-              availableFiatBalance={'123'}
-              stakePercentage={'123'}
+              // unbondingPeriod={<UnlockDuration slpxPair={slpxPair} />}
+              availableBalance={<AvailableBalance slpxPair={slpxPair} />}
+              availableFiatBalance={<AvailableFiatBalance slpxPair={slpxPair} />}
+              stakePercentage={<StakePercentage slpxPair={slpxPair} />}
               stakeButton={
                 <StakeProvider.StakeButton
                   // as={Link}
@@ -35,17 +41,6 @@ const SlpxSubstrateStakeProviders = () => {
                   to={`?action=stake&type=slpx&contract-address=${'123'}`}
                 />
               }
-
-              // unbondingPeriod={<UnlockDuration slpxPair={slpxPair} />}
-              // availableBalance={<AvailableBalance slpxPair={slpxPair} />}
-              // availableFiatBalance={<AvailableFiatBalance slpxPair={slpxPair} />}
-              // stakePercentage={<StakePercentage slpxPair={slpxPair} />}
-              // stakeButton={
-              //   <StakeProvider.StakeButton
-              //     as={Link}
-              //     to={`?action=stake&type=slpx&contract-address=${slpxPair.splx}`}
-              //   />
-              // }
             />
           </ChainProvider>
         )
