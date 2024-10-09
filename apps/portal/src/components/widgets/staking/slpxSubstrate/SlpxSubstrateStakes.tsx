26e0f18a3fb8539fd1cc4d909f0f486a42c91e64
Will Urban <will.urban.dev@gmail.com>
2024-10-09 11:30:10 +0700

    feat: display unlockings

--- apps/portal/src/components/widgets/staking/slpxSubstrate/SlpxSubstrateStakes.tsx
+++ apps/portal/src/components/widgets/staking/slpxSubstrate/SlpxSubstrateStakes.tsx
@@ -17,46 +17,48 @@ const SlpxSubstrateStake = ({
   setShouldRenderLoadingSkeleton: React.Dispatch<React.SetStateAction<boolean>>
 }) => {
   const stakes = useStakes({ slpxSubstratePair })
-  console.log({ slpxSubstratePair, setShouldRenderLoadingSkeleton })
-  return stakes?.map((stake, index) => (
-    <ErrorBoundary
-      key={index}
-      orientation="horizontal"
-      renderFallback={() => (
-        <StakePositionErrorBoundary
+  console.log({ setShouldRenderLoadingSkeleton })
+  return stakes?.map((stake, index) => {
+    return (
+      <ErrorBoundary
+        key={index}
+        orientation="horizontal"
+        renderFallback={() => (
+          <StakePositionErrorBoundary
+            chain={slpxSubstratePair.chainName}
+            assetSymbol={slpxSubstratePair.vToken.symbol}
+            assetLogoSrc={slpxSubstratePair.vToken.logo}
+            account={stake.account}
+            provider="Bifrost liquid staking"
+          />
+        )}
+      >
+        <StakePosition
+          account={stake.account}
+          readonly={stake.account.readonly}
+          provider="Bifrost liquid staking"
+          stakeStatus={stake.balance.planck > 0n ? 'earning_rewards' : 'not_earning_rewards'}
+          balance={<RedactableBalance>{stake.balance?.toLocaleString()}</RedactableBalance>}
+          fiatBalance={<AnimatedFiatNumber end={stake.fiatBalance} />}
           chain={slpxSubstratePair.chainName}
           assetSymbol={slpxSubstratePair.vToken.symbol}
           assetLogoSrc={slpxSubstratePair.vToken.logo}
-          account={stake.account}
-          provider="Bifrost liquid staking"
+          // increaseStakeButton={<StakePosition.IncreaseStakeButton onClick={() => setIncreaseStakeDialogOpen(true)} />}
+          // unstakeButton={
+          //   props.position.balance.planck > 0n && (
+          //     <StakePosition.UnstakeButton onClick={() => setUnstakeDialogOpen(true)} />
+          //   )
+          // }
+          unstakingStatus={
+            stake.unlocking !== undefined &&
+            stake.unlocking.planck > 0n && (
+              <StakePosition.UnstakingStatus amount={stake.unlocking?.toLocaleString()} unlocks={[]} />
+            )
+          }
         />
-      )}
-    >
-      <StakePosition
-        account={stake.account}
-        readonly={stake.account.readonly}
-        provider="Bifrost liquid staking"
-        stakeStatus={stake.balance.planck > 0n ? 'earning_rewards' : 'not_earning_rewards'}
-        balance={<RedactableBalance>{stake.balance?.toLocaleString()}</RedactableBalance>}
-        fiatBalance={<AnimatedFiatNumber end={stake.fiatBalance} />}
-        chain={slpxSubstratePair.chainName}
-        assetSymbol={slpxSubstratePair.vToken.symbol}
-        assetLogoSrc={slpxSubstratePair.vToken.logo}
-        // increaseStakeButton={<StakePosition.IncreaseStakeButton onClick={() => setIncreaseStakeDialogOpen(true)} />}
-        // unstakeButton={
-        //   props.position.balance.planck > 0n && (
-        //     <StakePosition.UnstakeButton onClick={() => setUnstakeDialogOpen(true)} />
-        //   )
-        // }
-        // unstakingStatus={
-        //   props.position.unlocking !== undefined &&
-        //   props.position.unlocking.planck > 0n && (
-        //     <StakePosition.UnstakingStatus amount={props.position.unlocking?.toLocaleString()} unlocks={[]} />
-        //   )
-        // }
-      />
-    </ErrorBoundary>
-  ))
+      </ErrorBoundary>
+    )
+  })
 }
 
 type SlpxSubstrateStakesProps = {
