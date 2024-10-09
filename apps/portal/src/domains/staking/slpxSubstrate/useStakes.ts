c6acadbce2d0879ccfab49472905268b18b381c8
Will Urban <will.urban.dev@gmail.com>
2024-10-09 11:09:41 +0700

    feat: get unlockings, filter connected accs without stake or unlockings

--- apps/portal/src/domains/staking/slpxSubstrate/useStakes.ts
+++ apps/portal/src/domains/staking/slpxSubstrate/useStakes.ts
@@ -1,8 +1,10 @@
+import { Maybe } from '../../../util/monads'
 import { SlpxSubstratePair } from './types'
 import { selectedSubstrateAccountsState } from '@/domains/accounts'
 import { Account } from '@/domains/accounts/recoils'
 import { selectedBalancesState, selectedCurrencyState } from '@/domains/balances'
 import { Decimal } from '@talismn/math'
+import { useQueryState } from '@talismn/react-polkadot-api'
 import { useRecoilValueLoadable, waitForAll } from 'recoil'
 
 const defaultCurrency = 'usd'
@@ -15,19 +17,33 @@ const useStakes = ({ slpxSubstratePair }: { slpxSubstratePair: SlpxSubstratePair
     waitForAll([selectedBalancesState, selectedCurrencyState])
   )
   const [balances, currency] = loadableState === 'hasValue' ? loadableContents : []
-
-  const data = accounts?.map(account => {
-    const balance = balances?.find(
-      x =>
-        x.token?.symbol.toLowerCase() === slpxSubstratePair.vToken.symbol.toLowerCase() && x.address === account.address
+  const userUnlockLedgersLoadable = useRecoilValueLoadable(
+    useQueryState(
+      'vtokenMinting',
+      'userUnlockLedger.multi',
+      accounts?.map(x => [x.address ?? '', slpxSubstratePair.nativeToken.tokenId] as const) ?? []
     )
-    const formattedBalance = Decimal.fromPlanck(balance?.sum.planck.total ?? 0n, vTokenDecimals, {
-      currency: slpxSubstratePair.vToken.symbol,
+  )
+
+  const userUnlockLedgers = userUnlockLedgersLoadable.valueMaybe()
+
+  const data = accounts
+    ?.map((account, index) => {
+      const balance = balances?.find(
+        x =>
+          x.token?.symbol.toLowerCase() === slpxSubstratePair.vToken.symbol.toLowerCase() &&
+          x.address === account.address
+      )
+      const formattedBalance = Decimal.fromPlanck(balance?.sum.planck.total ?? 0n, vTokenDecimals, {
+        currency: slpxSubstratePair.vToken.symbol,
+      })
+      const fiatBalance = balance?.sum.fiat(currency ?? defaultCurrency).total ?? 0
+      const unlocking = Maybe.of(userUnlockLedgers?.[index]?.unwrapOrDefault()).mapOrUndefined(x =>
+        Decimal.fromPlanck(x[0].toBigInt(), vTokenDecimals ?? 0, { currency: slpxSubstratePair.vToken.symbol })
+      )
+      return { account: account as Account, balance: formattedBalance, fiatBalance, unlocking }
     })
-    const fiatBalance = balance?.sum.fiat(currency ?? defaultCurrency).total ?? 0
-    const unlocking = 999
-    return { account: account as Account, balance: formattedBalance, fiatBalance, unlocking }
-  })
+    .filter(x => x.balance.planck !== 0n || (x.unlocking !== undefined && x.unlocking.planck !== 0n))
   return data
 }
 
