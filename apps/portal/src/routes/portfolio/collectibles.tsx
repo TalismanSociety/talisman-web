9b6892afaf12d267aa25286505b17f3a9bc2c63c
Will Urban <will.urban.dev@gmail.com>
2024-10-18 11:51:47 +0700

    Revert "fix: NFT dialog not displaying OG WUD BURN collection (#1191)"
    This reverts commit e3a5bb95f4f5b046537aa2666e5ffcdb44a1d431.

--- apps/portal/src/routes/portfolio/collectibles.tsx
+++ apps/portal/src/routes/portfolio/collectibles.tsx
@@ -164,8 +164,8 @@ const NftCard = ({ nft }: { nft: Nft }) => {
         overline={nft.collection?.name}
         media={
           <MediaDialog.Player
-            src={Maybe.of(nft.media.url).mapOrUndefined(x => toIpfsCompatibleUrl(x, undefined, nft))}
-            type={(nft.media.mimeType?.split('/').at(0) as any) ?? 'image'}
+            src={Maybe.of(nft.media.url).mapOrUndefined(toIpfsCompatibleUrl)}
+            type={nft.media.mimeType?.split('/').at(0) as any}
           />
         }
         content={
