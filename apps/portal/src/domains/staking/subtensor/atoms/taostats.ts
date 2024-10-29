71c6c37a649f77b147860b7eb78d131337806f1d
Will Urban <will.urban.dev@gmail.com>
2024-10-29 12:05:22 +0700

    refactor: updated taostats api call endpoints

--- apps/portal/src/domains/staking/subtensor/atoms/taostats.ts
+++ apps/portal/src/domains/staking/subtensor/atoms/taostats.ts
@@ -1,15 +1,23 @@
-import { ValidatorsData } from '../types'
-import { delegatesAtom } from './delegates'
 import { atom } from 'jotai'
 import { atomFamily } from 'jotai/utils'
 
-const TAOSTATS_API_KEY = import.meta.env.REACT_APP_TAOSTATS_API_KEY
-const TAOSTATS_API_URL = 'https://api.taostats.io/api/v1'
+import { ValidatorsData } from '../types'
+import { delegatesAtom } from './delegates'
 
-const fetchTaoStats = async ({ page = 1, limit = 200 }: { page: number; limit: number }): Promise<ValidatorsData> => {
+const TAOSTATS_API_KEY = import.meta.env.REACT_APP_TAOSTATS_API_KEY
+const TAOSTATS_API_URL = 'https://api-prod-v2.taostats.io/api'
+const MAX_PAGE_SIZE = 100
+
+const fetchTaoStats = async ({
+  page = 1,
+  limit = MAX_PAGE_SIZE,
+}: {
+  page: number
+  limit: number
+}): Promise<ValidatorsData> => {
   try {
     return await (
-      await fetch(`${TAOSTATS_API_URL}/validator?page=${page}&limit=${limit}`, {
+      await fetch(`${TAOSTATS_API_URL}/validator/latest/v1?page=${page}&limit=${limit}`, {
         method: 'GET',
         headers: {
           Authorization: TAOSTATS_API_KEY,
@@ -23,14 +31,24 @@ const fetchTaoStats = async ({ page = 1, limit = 200 }: { page: number; limit: n
 }
 
 export const taostatsAtom = atom(async () => {
-  const stats: ValidatorsData = { count: 0, validators: [] }
-
+  const stats: ValidatorsData = {
+    pagination: {
+      current_page: 0,
+      per_page: 0,
+      total_items: 0,
+      total_pages: 0,
+      next_page: null,
+      prev_page: null,
+    },
+    data: [],
+  }
   let page = 1
-  while (stats.count === 0 || stats.count > stats.validators.length) {
-    const taoStats = await fetchTaoStats({ page: page, limit: 200 })
-    stats.count = taoStats.count
-    stats.validators.push(...taoStats.validators)
-    page++
+
+  while (!stats.data.length || stats.pagination.current_page < stats.pagination.total_pages) {
+    const taoStats = await fetchTaoStats({ page: page, limit: MAX_PAGE_SIZE })
+    stats.pagination = taoStats.pagination
+    stats.data.push(...taoStats.data)
+    page = taoStats.pagination.current_page + 1
   }
 
   return stats
@@ -42,21 +60,15 @@ export const activeTaoDelegatesStatsAtom = atom(async get => {
 
   const activeDelegatesHotKeys = Object.keys(delegates)
 
-  const activeDelegates = taostats.validators.filter(validator =>
-    activeDelegatesHotKeys.includes(validator.hot_key.ss58)
-  )
+  const activeDelegates = taostats.data.filter(validator => activeDelegatesHotKeys.includes(validator.hotkey.ss58))
 
   return activeDelegates
 })
 
 export const highestAprTaoValidatorAtom = atom(async get => {
   const activeDelegatesStats = await get(activeTaoDelegatesStatsAtom)
-  const highestAprValidatorStats = activeDelegatesStats.reduce((acc, validator) => {
-    if (parseFloat(validator.apr) > parseFloat(acc.apr)) {
-      acc = validator
-    }
-    return acc
-  })
+
+  const highestAprValidatorStats = activeDelegatesStats[0]
 
   return highestAprValidatorStats
 })
@@ -64,12 +76,12 @@ export const highestAprTaoValidatorAtom = atom(async get => {
 export const taoDelegateStatsAtomFamily = atomFamily((hotKey: string) =>
   atom(async get => {
     const activeDelegatesStats = await get(activeTaoDelegatesStatsAtom)
-    return activeDelegatesStats.find(validator => validator.hot_key.ss58 === hotKey)
+    return activeDelegatesStats.find(validator => validator.hotkey.ss58 === hotKey)
   })
 )
 
 export const taoTotalStakedTaoAtom = atom(async get => {
-  const { system_total_stake } = await get(highestAprTaoValidatorAtom)
+  const { system_stake = '0' } = (await get(highestAprTaoValidatorAtom)) ?? {}
 
-  return system_total_stake
+  return system_stake
 })
