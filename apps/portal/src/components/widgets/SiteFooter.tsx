9b89eff2d83dfc659f681b0b8b75e0acb859dc00
alecdwm <alec@owls.io>
2024-12-24 01:16:00 +0000

    fix: footer link colour

--- apps/portal/src/components/widgets/SiteFooter.tsx
+++ apps/portal/src/components/widgets/SiteFooter.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react'
+
 import Discord from '@/assets/icons/discord.svg?react'
 import GitHub from '@/assets/icons/github.svg?react'
 import XSocial from '@/assets/icons/x-social.svg?react'
@@ -7,25 +9,30 @@ export const SiteFooter = ({ className }: { className?: string }) => {
   return (
     <footer className={cn('pb-8', className)}>
       <div className="flex items-center justify-center gap-8">
-        <a href="https://x.com/wearetalisman" target="_blank" rel="noreferrer noopener">
+        <FooterLink href="https://x.com/wearetalisman">
           <XSocial width="1em" />
-        </a>
-        <a href="https://discord.gg/talisman" target="_blank" rel="noreferrer noopener">
+        </FooterLink>
+        <FooterLink href="https://discord.gg/talisman">
           <Discord width="1em" />
-        </a>
-        <a href="https://github.com/talismansociety" target="_blank" rel="noreferrer noopener">
+        </FooterLink>
+        <FooterLink href="https://github.com/talismansociety">
           <GitHub width="1em" />
-        </a>
-        <a href="https://docs.talisman.xyz" target="_blank" rel="noreferrer noopener">
-          Docs
-        </a>
-        <a href="https://docs.talisman.xyz/talisman/about/terms-of-use" target="_blank" rel="noreferrer noopener">
-          Terms
-        </a>
-        <a href="https://docs.talisman.xyz/talisman/about/privacy-policy" target="_blank" rel="noreferrer noopener">
-          Privacy
-        </a>
+        </FooterLink>
+        <FooterLink href="https://docs.talisman.xyz">Docs</FooterLink>
+        <FooterLink href="https://docs.talisman.xyz/talisman/about/terms-of-use">Terms</FooterLink>
+        <FooterLink href="https://docs.talisman.xyz/talisman/about/privacy-policy">Privacy</FooterLink>
       </div>
     </footer>
   )
 }
+
+const FooterLink = ({ href, children }: { href: string; children: ReactNode }) => (
+  <a
+    className="text-foreground/60 hover:text-foreground focus:text-foreground"
+    href={href}
+    target="_blank"
+    rel="noreferrer noopener"
+  >
+    {children}
+  </a>
+)
