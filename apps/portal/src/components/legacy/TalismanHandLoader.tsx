1425d0d3b5c7641e83eca1c27b1edcea79bf03d3
Alec WM <alec@owls.io>
2025-01-02 14:33:18 +1000

    feat: new spinny hand (#1241)

--- apps/portal/src/components/legacy/TalismanHandLoader.tsx
+++ apps/portal/src/components/legacy/TalismanHandLoader.tsx
@@ -1,8 +1,8 @@
 import { type ImgHTMLAttributes } from 'react'
 
-import TalismanHandLoading from '@/assets/spin_red.gif'
+import SpinnyHandFast from '@/assets/spinnyhand-fast.gif'
 
 /** @deprecated */
 export const TalismanHandLoader = (props: ImgHTMLAttributes<HTMLImageElement>) => {
-  return <img src={TalismanHandLoading} alt="Loading..." width="128px" height="128px" {...props} />
+  return <img src={SpinnyHandFast} alt="Loading..." width="128px" height="128px" {...props} />
 }
