b94f91329d3b13eb01b7530b780638c79fc62370
alecdwm <alec@owls.io>
2025-01-05 04:40:22 +0000

    fix: extra text spacing in site logo

--- apps/portal/src/components/widgets/SiteLogo.tsx
+++ apps/portal/src/components/widgets/SiteLogo.tsx
@@ -5,10 +5,10 @@ export const SiteLogo = ({ responsive }: { responsive?: boolean }) => (
   <div className="flex select-none items-center gap-2">
     <Logo />
     <div className="text-foreground flex items-center gap-1">
-      <div className={cn('font-surtexpanded text-3xl', responsive ? 'hidden lg:block' : '')}>Talisman</div>
+      <div className={cn('font-surtexpanded text-3xl leading-none', responsive ? 'hidden lg:block' : '')}>Talisman</div>
       <div
         className={cn(
-          'font-surtexpanded text-3xl',
+          'font-surtexpanded text-3xl leading-none',
           responsive ? 'lg:text-foreground/60 hidden md:block' : 'text-foreground/60'
         )}
       >
