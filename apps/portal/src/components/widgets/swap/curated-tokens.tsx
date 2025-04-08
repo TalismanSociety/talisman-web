94d028c96704e1f601995e10904f8652509586cd
Alec WM <2741569+alecdwm@users.noreply.github.com>
2025-04-08 15:09:27 +1000

    chore: updated curated tokens (#1296)

--- apps/portal/src/components/widgets/swap/curated-tokens.tsx
+++ apps/portal/src/components/widgets/swap/curated-tokens.tsx
@@ -1,32 +1,39 @@
 export const popularTokens: string[] = [
-  '146-evm-native',
-  'btc-native',
-  'polkadot-substrate-native',
-  'bittensor-substrate-native',
-  '1-evm-native',
-  '1-evm-erc20-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
-  '1-evm-erc20-0xdac17f958d2ee523a2206206994597c13d831ec7',
-  '1-evm-erc20-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-  '42161-evm-native',
-  '42161-evm-erc20-0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f',
-  '42161-evm-erc20-0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9',
-  '42161-evm-erc20-0xaf88d065e77c8cc2239327c5edb3a432268e5831',
-  '169-evm-native',
-  '169-evm-erc20-0x95cef13441be50d20ca4558cc0a27b601ac544e5',
+  'btc-native', // BTC on Bitcoin (destination only)
+  '146-evm-native', // S on Sonic
+  'polkadot-substrate-native', // DOT on Polkadot
+  'bittensor-substrate-native', // TAO on Bittensor
+  '1-evm-native', // ETH on Eth
+  '1-evm-erc20-0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC on Eth
+  '1-evm-erc20-0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT on Eth
+  '1-evm-erc20-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC on Eth
+  '1-evm-erc20-0x77e06c9eccf2e797fd462a92b6d7642ef85b0a44', // WTAO on Eth
+  '10-evm-native', // ETH on OP
+  '42161-evm-native', // ETH on Arb
+  '42161-evm-erc20-0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f', // WBTC on Arb
+  '42161-evm-erc20-0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9', // USDT on Arb
+  '42161-evm-erc20-0xaf88d065e77c8cc2239327c5edb3a432268e5831', // USDC on Arb
+  '169-evm-native', // ETH on Manta Pacific
+  '169-evm-erc20-0x95cef13441be50d20ca4558cc0a27b601ac544e5', // MANTA on Manta Pacific
+  'polkadot-asset-hub-substrate-assets-1984-usdt', // USDT on AssetHub
+  'polkadot-asset-hub-substrate-assets-1337-usdc', // USDC on AssetHub
 ]
 
 export const talismanTokens: string[] = [
-  '146-evm-native',
-  'astar-substrate-native',
-  '1-evm-erc20-0xb59490ab09a0f526cc7305822ac65f2ab12f9723',
-  'aleph-zero-substrate-native',
-  'kusama-substrate-native',
-  '1-evm-erc20-0xba41ddf06b7ffd89d1267b5a93bfef2424eb2003',
-  'acala-substrate-native',
-  '1-evm-erc20-0x2da719db753dfa10a62e140f436e1d67f2ddb0d6',
-  '1284-evm-native',
-  '1285-evm-native',
-  'avail-substrate-native',
-  '1-evm-erc20-0xaa7a9ca87d3694b5755f213b5d04094b8d0f0a6f',
-  '1-evm-erc20-0xc221b7e65ffc80de234bbb6667abdd46593d34f0',
+  '146-evm-native', // S on Sonic
+  'analog-timechain-substrate-native', // ANLOG on Analog
+  'astar-substrate-native', // ASTR on Astar
+  '1-evm-erc20-0xb59490ab09a0f526cc7305822ac65f2ab12f9723', // Old LIT on Eth
+  'aleph-zero-substrate-native', // AZERO on Aleph Zero
+  'kusama-substrate-native', // KSM on Kusama
+  '1-evm-erc20-0xba41ddf06b7ffd89d1267b5a93bfef2424eb2003', // MYTH on Eth
+  'acala-substrate-native', // ACA on Acala
+  '1-evm-erc20-0x2da719db753dfa10a62e140f436e1d67f2ddb0d6', // CERE on Eth
+  '1284-evm-native', // GLMR on Moonbeam
+  '1285-evm-native', // MOVR on Moonriver
+  'avail-substrate-native', // AVAIL on Avail
+  '1-evm-erc20-0xaa7a9ca87d3694b5755f213b5d04094b8d0f0a6f', // TRAC on Eth
+  '1-evm-erc20-0xc221b7e65ffc80de234bbb6667abdd46593d34f0', // WCFG on Eth
+  'moonbeam-substrate-native', // GLMR on Moonbeam
+  'moonriver-substrate-native', // MOVR on Moonriver
 ]
