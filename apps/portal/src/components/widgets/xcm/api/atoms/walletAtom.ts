fc96a2868f28e993e068424696d723fe705ef4c0
alecdwm <2741569+alecdwm@users.noreply.github.com>
2025-05-21 01:58:12 +0000

    chore: upgrade xcm sdk

--- apps/portal/src/components/widgets/xcm/api/atoms/walletAtom.ts
+++ apps/portal/src/components/widgets/xcm/api/atoms/walletAtom.ts
@@ -3,14 +3,10 @@ import { Wallet } from '@galacticcouncil/xcm-sdk'
 import { atom } from 'jotai'
 
 import { configServiceAtom } from './configServiceAtom'
-import { poolServiceAtom } from './poolServiceAtom'
 
 export const walletAtom = atom(async get => {
   const configService = get(configServiceAtom)
   if (!configService) return
 
-  const poolService = await get(poolServiceAtom)
-  if (!poolService) return
-
-  return new Wallet({ configService, poolService, transferValidations })
+  return new Wallet({ configService, transferValidations })
 })
