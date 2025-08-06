912088b993ab91ce6eb1e43885c250e1d62f82a6
Alec WM <2741569+alecdwm@users.noreply.github.com>
2025-08-06 18:49:35 +1000

    fix: staking crash (#1365)

--- apps/portal/src/components/widgets/staking/providers/hooks/subtensor/useSubtensorProviders.ts
+++ apps/portal/src/components/widgets/staking/providers/hooks/subtensor/useSubtensorProviders.ts
@@ -13,24 +13,26 @@ const useSubtensorProviders = () => {
    * Has to be added here instead of /chains/config.ts due to the way chainsState selector is implemented,
    *  which doesn't support adding two configs for the same chain genesisHash
    */
-  const chains = [rootStaking!, { ...rootStaking!, hasDTaoStaking: true }]
+  const chains = [rootStaking, rootStaking ? { ...rootStaking, hasDTaoStaking: true } : undefined]
 
-  const subtensorProviders: Provider[] = chains.map(chain => {
-    const actionLink = `?action=stake&type=subtensor&chain=${chain.id ?? ''}`
-    return {
-      symbol: chain.hasDTaoStaking ? DTAO_SYMBOL : chain.nativeToken?.symbol ?? '',
-      logo: chain.hasDTaoStaking ? DTAO_LOGO : chain.nativeToken?.logo ?? '',
-      chainName: chain.name ?? '',
-      chainId: chain.id,
-      type: chain.hasDTaoStaking ? 'Subnet Staking' : 'Delegation',
-      typeId: 'delegationSubtensor',
-      provider: chain.name,
-      actionLink: chain.hasDTaoStaking ? actionLink.concat('&hasDTaoStaking=true') : actionLink,
-      nativeToken: chain.nativeToken,
-      genesisHash: chain.genesisHash,
-      hasDTaoStaking: chain.hasDTaoStaking,
-    }
-  })
+  const subtensorProviders: Provider[] = chains
+    .filter(chain => chain !== undefined)
+    .map(chain => {
+      const actionLink = `?action=stake&type=subtensor&chain=${chain.id ?? ''}`
+      return {
+        symbol: chain.hasDTaoStaking ? DTAO_SYMBOL : chain.nativeToken?.symbol ?? '',
+        logo: chain.hasDTaoStaking ? DTAO_LOGO : chain.nativeToken?.logo ?? '',
+        chainName: chain.name ?? '',
+        chainId: chain.id,
+        type: chain.hasDTaoStaking ? 'Subnet Staking' : 'Delegation',
+        typeId: 'delegationSubtensor',
+        provider: chain.name,
+        actionLink: chain.hasDTaoStaking ? actionLink.concat('&hasDTaoStaking=true') : actionLink,
+        nativeToken: chain.nativeToken,
+        genesisHash: chain.genesisHash!,
+        hasDTaoStaking: chain.hasDTaoStaking,
+      }
+    })
 
   return subtensorProviders
 }
