b244095d37ef1d81ed86a57bcd61f47ec378faee
Will Urban <mica@talisman.xyz>
2025-10-03 13:10:44 +0700

    fix: return number not str

--- apps/portal/src/components/widgets/staking/providers/hooks/seek/useGetSeekStakeMpr.ts
+++ apps/portal/src/components/widgets/staking/providers/hooks/seek/useGetSeekStakeMpr.ts
@@ -2,7 +2,7 @@ import useGetSeekPoolInfo from './useGetSeekPoolInfo'
 
 const useGetSeekStakeMpr = () => {
   const { data } = useGetSeekPoolInfo()
-  if (!data) return '0'
+  if (!data) return 0
   const [totalStaked, rewardRate] = data || [0n, 0n]
 
   const SECONDS_IN_MONTH = 30 * 24 * 60 * 60 // 2,592,000 seconds
