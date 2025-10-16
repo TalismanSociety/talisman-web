9287bb18906d5f4d7f79fc6076b54d54d232fa93
Will Urban <mica@talisman.xyz>
2025-10-16 10:03:19 +0700

    chore: add nav link

--- apps/portal/src/routes/portfolio/banners/SeekPreSaleBanner.tsx
+++ apps/portal/src/routes/portfolio/banners/SeekPreSaleBanner.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import SeekPreSaleBg from '@/assets/seek-pre-sale-bg.svg?react'
 
 const storageKey = 'hide-seek-pre-sale-info-banner'
+const preSaleLink = 'https://docs.talisman.xyz/talisman/seek/launchpad-pre-sale'
 
 export const SeekPreSaleBanner = () => {
   const [isVisible, setIsVisible] = useState<boolean>(true)
@@ -20,7 +21,10 @@ export const SeekPreSaleBanner = () => {
 
   if (!isVisible) return null
   return (
-    <div className="cursor-pointer rounded-[10px] bg-gradient-to-r from-[#2E3128] to-[#1B1B1B] to-[274%] p-[1px] text-[14px]">
+    <div
+      className="cursor-pointer rounded-[10px] bg-gradient-to-r from-[#2E3128] to-[#1B1B1B] to-[274%] p-[1px] text-[14px]"
+      onClick={() => window.open(preSaleLink, '_blank')}
+    >
       <div className="relative flex h-[64px] rounded-[10px] bg-gradient-to-b from-[rgb(27,27,27)] to-[#3F3F0C] to-[287%] p-2">
         <div className="align-center flex items-center gap-2 pl-6">
           <div className="flex flex-col gap-1">
