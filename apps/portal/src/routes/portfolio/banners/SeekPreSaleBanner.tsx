5f24d42381e2edfbcd28ac16918d73a658095873
Will Urban <mica@talisman.xyz>
2025-10-16 09:43:39 +0700

    feat: seek presale banner

---
+++ apps/portal/src/routes/portfolio/banners/SeekPreSaleBanner.tsx
@@ -0,0 +1,41 @@
+import { X } from '@talismn/web-icons'
+import { useEffect, useState } from 'react'
+
+import SeekPreSaleBg from '@/assets/seek-pre-sale-bg.svg?react'
+
+const storageKey = 'hide-seek-pre-sale-info-banner'
+
+export const SeekPreSaleBanner = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(true)
+
+  useEffect(() => {
+    const hidden = localStorage.getItem(storageKey) === 'true'
+    setIsVisible(!hidden)
+  }, [])
+
+  const handleClose = () => {
+    localStorage.setItem(storageKey, 'true')
+    setIsVisible(false)
+  }
+
+  if (!isVisible) return null
+  return (
+    <div className="cursor-pointer rounded-[10px] bg-gradient-to-r from-[#2E3128] to-[#1B1B1B] to-[274%] p-[1px] text-[14px]">
+      <div className="relative flex h-[64px] rounded-[10px] bg-gradient-to-b from-[rgb(27,27,27)] to-[#3F3F0C] to-[287%] p-2">
+        <div className="align-center flex items-center gap-2 pl-6">
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center">
+              <div className="font-semibold">SEEK Pre-Sale Ends Soon</div>
+            </div>
+            <div className="text-[12px] text-gray-400">Closes Oct 20. Don’t miss out! Click to Learn More. </div>
+          </div>
+          <div className="absolute right-8 top-0">
+            <SeekPreSaleBg />
+            {/* <SeekInfoBenefitsBg /> */}
+          </div>
+        </div>
+        <X className="ml-auto w-6 cursor-pointer font-semibold text-white" onClick={() => handleClose()} />
+      </div>
+    </div>
+  )
+}
