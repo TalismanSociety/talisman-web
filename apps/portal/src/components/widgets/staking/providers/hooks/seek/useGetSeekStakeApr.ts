10f910dc3aee4e0d9c32ef0b503dd6db20de68d3
Will Urban <mica@talisman.xyz>
2025-11-30 12:51:10 +0700

    fix: handle apr calculation for fresh contracts

--- apps/portal/src/components/widgets/staking/providers/hooks/seek/useGetSeekStakeApr.ts
+++ apps/portal/src/components/widgets/staking/providers/hooks/seek/useGetSeekStakeApr.ts
@@ -5,6 +5,11 @@ const useGetSeekStakeApr = () => {
   if (!data) return 0
   const [totalStaked, rewardRate] = data || [0n, 0n]
 
+  // Freshly deployed contracts have 0 staked, reward rate cannot be calculated (infinity).
+  if (!totalStaked) {
+    return 0
+  }
+
   const SECONDS_IN_YEAR = BigInt(365.25 * 24 * 60 * 60) // 31,536,000 seconds
   const MULTIPLIER = 10_000n // use to preserve decimal precision when working with bigints
   const multipliedApr = (rewardRate * SECONDS_IN_YEAR * MULTIPLIER) / totalStaked
