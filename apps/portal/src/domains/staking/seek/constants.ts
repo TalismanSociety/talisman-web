2a6f18ad9a41d5fa78d685bfd6357603a08812ba
Will Urban <mica@talisman.xyz>
2025-11-30 12:51:32 +0700

    chore: updated seek staking contract address

--- apps/portal/src/domains/staking/seek/constants.ts
+++ apps/portal/src/domains/staking/seek/constants.ts
@@ -1,5 +1,5 @@
 export const DECIMALS = 18
-export const SEEK_SINGLE_POOL_STAKING_ADDRESS = '0x6Dc508ee93163632941632025611375309c8E3c2' as const
+export const SEEK_SINGLE_POOL_STAKING_ADDRESS = '0xbc7e311abdf3b219be72011d772136e5c712f7fc' as const
 export const SEEK_TOKEN_ADDRESS = '0x2a69b0383759572081c09f0a68d3a8a955751dde' as const
 export const SEEK_TICKER = 'SEEK'
 export const CHAIN_ID = 137 // Polygon
