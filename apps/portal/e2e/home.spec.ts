5b8b5fd5ae5e3ebd1d54f3ff1e56e10b6b530ba2
alecdwm <2741569+alecdwm@users.noreply.github.com>
2025-12-04 02:56:37 +0000

    fix: tests

--- apps/portal/e2e/home.spec.ts
+++ apps/portal/e2e/home.spec.ts
@@ -1,13 +1,13 @@
 import { expect, test } from '@playwright/test'
 
-test('should redirect to portfolio page', async ({ page }) => {
+test('should redirect to staking page', async ({ page }) => {
   await page.goto('/')
 
-  await expect(page).toHaveURL(/.*\/portfolio/)
+  await expect(page).toHaveURL(/.*\/staking/)
 })
 
 test('should redirect invalid link to portfolio page', async ({ page }) => {
   await page.goto('/definitely-not-a-valid-link')
 
-  await expect(page).toHaveURL(/.*\/portfolio/)
+  await expect(page).toHaveURL(/.*\/staking/)
 })
